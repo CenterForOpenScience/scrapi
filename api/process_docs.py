816c21160eaab7a2198565e5ca4ebe0d1cc4263c
fabian <fabian@fabianism.us>
2014-06-25 14:44:04 -0400

    Base implementation of doc processor

--- api/process_docs.py
+++ api/process_docs.py
@@ -2,12 +2,14 @@
 
 
 def process_raw(doc, source, id, filetype):
-    """ 
+    """
         Takes a document (in the form of text, and saves it to disk
         with the specified name and the designated filetype in the
         specified source directory
     """
-    raise NotImplementedError
+    filepath = 'raw' + '/' + str(source) + str(id) + '.' + str(filetype)
+    with open(filepath, 'w') as f:
+        f.write(doc)
 
 
 def process(doc):
