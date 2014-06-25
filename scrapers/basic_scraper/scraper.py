bc730fa8a9b0f243fbf51c9e880cc8cd19f825b6
fabian <fabian@fabianism.us>
2014-06-25 15:59:25 -0400

    Basic xml parsing for dummy scraper

--- scrapers/basic_scraper/scraper.py
+++ scrapers/basic_scraper/scraper.py
@@ -2,24 +2,29 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-from lxml import etree
-from api import process_docs
+sys.path.insert(1, '/home/fabian/cos/scrapi/')
+
+import xmltodict
+
+import api.process_docs as process_docs
+
 
 def retrieve():
-    with open('10.1371%2Fjournal.pbio.1001356.xml') as f:
+    with open('10.1371%2Fjournal.pbio.1001356.xml', 'r') as f:
         text = f.read()
 
     process_docs.process_raw(text, 'basic_scraper', '10.1371%2Fjournal.pbio.1001356', 'xml')
     parse(text)
 
+
 def parse(text):
-    doc = etree.fromstring(text)
-    str_list = doc.findall('str')
-    for item in str_list:
-        print str(item.attrib) + '\n\t' + item.text.strip()
-    arr_list = doc.findall('arr')
-    for item in arr_list:
-        str_arr_list = item.findall('str')
-        for str_item in str_arr_list:
-            print str(item.attrib) + '\n\t' + str_item.text.strip()
+    doc = xmltodict.parse(text)
+    formatted_doc = {
+        'title': doc['doc']['str'][4]['#text'],
+        'description': doc['doc']['arr'][1]['str'],
+        'contributors': doc['doc']['arr'][0]['str'],
+        'id': doc['doc']['str'][0]['#text'],
+    }
+
+    process_docs.process(formatted_doc)
 retrieve()
