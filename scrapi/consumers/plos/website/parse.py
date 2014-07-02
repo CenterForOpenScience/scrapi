8aa437fe46ceb96c16ee1884d45762b36a26c614
Faye Huynh <fh9cp@virginia.edu>
2014-07-02 14:54:09 -0400

    Full PLoS consumer and parser

--- scrapi/consumers/plos/website/parse.py
+++ scrapi/consumers/plos/website/parse.py
@@ -7,40 +7,83 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def parse(result, timestamp):
-    result_soup = BeautifulSoup(result)
-
+def get_authors(soup):
     authors = []
-    for author in result_soup.find_all("span", class_="person"):
+    for author in soup.find_all("span", class_="person"):
         author = author.getText("|", strip=True)
         author = str(author).replace(',','')
         if "|mail|" or "|mail" in author:
             author = str(author).replace('|mail|','')
             author = str(author).replace('|mail','')
         authors.append(author)
+    return authors
 
+
+def get_email(soup):
+    for email in soup.find_all(class_="author_inner"):
+        for emailAddress in email.find_all('a'):
+            return emailAddress.string
+
+
+def get_authors_json(soup, users, email_address):
+    author_list = []
+    if get_email(soup) is None:
+        for user in users:
+            author_dict = {}
+            author_dict["full_name"] = user
+            author_dict["email"] = None
+            author_list.append(author_dict)
+    else:
+        main_email = email_address.find_parent("li")
+        main_email_user = get_authors(main_email)
+        for user in users:
+            if main_email_user[0] == user:
+                author_dict = {}
+                author_dict["full_name"] = user
+                author_dict["email"] = email_address
+                author_list.append(author_dict)
+            else:
+                author_dict = {}
+                author_dict["full_name"] = user
+                author_dict["email"] = None
+                author_list.append(author_dict)
+    return author_list
+
+
+def get_keywords(soup):
     keywords = []
-    for keyword in result_soup.find_all(class_="flagText"):
+    for keyword in soup.find_all(class_="flagText"):
         keyword = keyword.string
         keywords.append(str(keyword))
+    return keywords
 
+
+def get_figures(soup):
     figures = []
-    for figure in result_soup.find_all(class_="img"):
+    for figure in soup.find_all(class_="img"):
         for link in figure.find_all('a'):
             figures.append("http://www.plosone.org" + link.get('href'))
+    return figures
 
-    abstract = result_soup.find(class_="abstract").getText()
+
+def get_abstract(soup):
+    abstract = soup.find(class_="abstract").getText()
     abstract = " ".join(abstract.split())
+    return abstract
+
+
+def parse(result, timestamp):
+    result_soup = BeautifulSoup(result)
 
     payload = {
         "doc":
             json.dumps({
                 'title': result_soup.title.string,
-                'contributors': authors,
+                'contributors': get_authors_json(result_soup, get_authors(result_soup), get_email(result_soup)),
                 'properties': {
-                    'abstract': abstract,
-                    'tags': keywords,
-                    'figures': figures,
+                    'abstract': get_abstract(result_soup),
+                    'tags': get_keywords(result_soup),
+                    'figures': get_figures(result_soup),
                     'PDF': result_soup.find("meta", {"name":"citation_pdf_url"})['content']
                 },
                 'meta': {},
@@ -49,4 +92,4 @@ def parse(result, timestamp):
             }),
         'timestamp': timestamp
     }
-    return requests.post('http://0.0.0.0:1337/process', params=payload)
+    return requests.get('http://0.0.0.0:1337/process', params=payload)
