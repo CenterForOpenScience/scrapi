c50462e14cbddc0da777c3b364ec9d986c07eaec
Faye Huynh <fh9cp@virginia.edu>
2014-07-02 11:41:07 -0400

    Implemented PLoS full scraper

--- scrapi/consumers/website/parse.py
+++ scrapi/consumers/website/parse.py
@@ -2,24 +2,51 @@ __author__ = 'faye'
 import requests
 import json
 import sys
+from bs4 import BeautifulSoup
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
 def parse(result, timestamp):
+    result_soup = BeautifulSoup(result)
+
+    authors = []
+    for author in result_soup.find_all("span", class_="person"):
+        author = author.getText("|", strip=True)
+        author = str(author).replace(',','')
+        if "|mail|" or "|mail" in author:
+            author = str(author).replace('|mail|','')
+            author = str(author).replace('|mail','')
+        authors.append(author)
+
+    keywords = []
+    for keyword in result_soup.find_all(class_="flagText"):
+        keyword = keyword.string
+        keywords.append(str(keyword))
+
+    figures = []
+    for figure in result_soup.find_all(class_="img"):
+        for link in figure.find_all('a'):
+            figures.append("http://www.plosone.org" + link.get('href'))
+
+    abstract = result_soup.find(class_="abstract").getText()
+    abstract = " ".join(abstract.split())
 
     payload = {
         "doc":
             json.dumps({
-                'title': result["str"][4]["#text"],
-                'contributors': result["arr"][0]["str"],
+                'title': result_soup.title.string,
+                'contributors': authors,
                 'properties': {
-                    'description': result["arr"][1]["str"],
+                    'abstract': abstract,
+                    'tags': keywords,
+                    'figures': figures,
+                    'PDF': result_soup.find("meta", {"name":"citation_pdf_url"})['content']
                 },
                 'meta': {},
-                'id': result["str"][0]["#text"],
+                'id': result_soup.find("meta", {"name":"citation_doi"})['content'],
                 'source': "PLoS"
             }),
         'timestamp': timestamp
     }
-    return requests.get('http://0.0.0.0:1337/process', params=payload)
+    return requests.post('http://0.0.0.0:1337/process', params=payload)
