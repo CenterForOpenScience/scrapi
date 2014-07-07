19c76d8964ba4bc1d9d21deebee781b18468a14f
fabian <fabian@fabianism.us>
2014-07-07 10:44:11 -0400

    invoke elasticsearch now starts elasticsearch server

--- tasks.py
+++ tasks.py
@@ -14,3 +14,23 @@ def schedule():
 @task
 def reset_search():
     run("curl -XPOST 'http://localhost:9200/_shutdown'")
+    import platform
+    if platform.linux_distribution()[0] == 'Ubuntu':
+        run("sudo service elasticsearch restart")
+    elif platform.system() == 'Darwin':  # Mac OSX
+        run('elasticsearch')
+
+
+@task
+def elasticsearch():
+    '''Start a local elasticsearch server
+
+    NOTE: Requires that elasticsearch is installed. See README for instructions
+    '''
+    import platform
+    if platform.linux_distribution()[0] == 'Ubuntu':
+        run("sudo service elasticsearch restart")
+    elif platform.system() == 'Darwin':  # Mac OSX
+        run('elasticsearch')
+    else:
+        print("Your system is not recognized, you will have to start elasticsearch manually")
