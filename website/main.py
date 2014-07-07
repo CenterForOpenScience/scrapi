ca5e7ef03770463775a07e7d94aa809a01cb3f4b
Faye Huynh <fh9cp@virginia.edu>
2014-07-07 16:44:26 -0400

    GET and POST requests can now process lists

--- website/main.py
+++ website/main.py
@@ -16,17 +16,31 @@ app = Flask(__name__)
 @app.route('/process_raw', methods=['GET', 'POST'])
 def process_raw():
     if request.method == 'POST':
-        doc = request.form.get('doc')
-        source = request.form.get('source')
-        doc_id = request.form.get('doc_id')
-        filetype = request.form.get('filetype')
-    else:
-        doc = request.args.get('doc')
-        source = request.args.get('source')
-        doc_id = request.args.get('doc_id')
-        filetype = request.args.get('filetype')
+        docs = request.form['doc']
+        doc_list_item = docs.split("ASDFJKL")
+        doc_ids = request.form['doc_id']
+        doc_ids_item = doc_ids.split("ASDFJKL")
+        for x in range(0,len(doc_list_item)):
+            doc = doc_list_item[x]
+            source = request.form.get('source')
+            doc_id = doc_ids_item[x]
+            filetype = request.form.get('filetype')
 
-    return Response(process_docs.process_raw(doc, source, doc_id, filetype))
+            Response(process_docs.process_raw(doc, source, doc_id, filetype))
+
+    else:
+        docs = request.args['doc']
+        doc_list_item = docs.split("ASDFJKL")
+        doc_ids = request.args['doc_id']
+        doc_ids_item = doc_ids.split("ASDFJKL")
+        for x in range(0,len(doc_list_item)):
+            doc = doc_list_item[x]
+            source = request.args.get('source')
+            doc_id = doc_ids_item[x]
+            filetype = request.args.get('filetype')
+
+            Response(process_docs.process_raw(doc, source, doc_id, filetype))
+    return "Processed!"
 
 
 @app.route('/process', methods=['GET', 'POST'])
@@ -47,7 +61,7 @@ def search_search():
     query = request.args.get('q')
     start = request.args.get('from')
     size = request.args.get('size')
-    return Response(json.dumps(search.search(query, start, size), indent=4, sort_keys=True), mimetype='application/json')
+    return Response(json.dumps(search.search('scrapi', query, start, size), indent=4, sort_keys=True), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(
