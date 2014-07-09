bbe5ff4f3cbf33386d9938534611c0cbbd1077e1
fabian <fabian@fabianism.us>
2014-07-09 10:15:20 -0400

    removed scrapers folder

--- scrapers/basic_scraper/scraper_plos.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'faye'
-import requests
-
-myReq = requests.get('http://api.plos.org/search?q=abstract:%22why%22&api_key=7AbMQ-D-3hKy5hJ1FPY2')
-
-fileName = str(articleID)+".html"
-f = open("plos_articles/"+fileName, 'w')
-f.write(myReq.text.encode('utf-8'))
-f.close()
