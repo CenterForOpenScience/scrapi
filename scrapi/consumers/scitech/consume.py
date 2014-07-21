39884bd96c3e2f1465b52f8ea124410def9ba66b
fabian <fabian@fabianism.us>
2014-07-21 14:05:42 -0400

    Updated SciTech scraper for new scrAPI architecture

--- scrapi/consumers/scitech/consume.py
+++ scrapi/consumers/scitech/consume.py
@@ -15,6 +15,7 @@ parse_date_stamp = datetime.date.today().strftime('%Y-%m-%d')
 def consume(start_date=param_date_stamp, end_date=None, **kwargs):
     """A function for querying the SciTech Connect database for raw XML. The XML is chunked into smaller pieces, each representing data
     about an article/report. If there are multiple pages of results, this function iterates through all the pages."""
+
     base_url = 'http://www.osti.gov/scitech/scitechxml'
     parameters = kwargs
     parameters['EntryDateFrom'] = start_date
@@ -22,19 +23,18 @@ def consume(start_date=param_date_stamp, end_date=None, **kwargs):
     parameters['page'] = 0
     morepages = 'true'
     xml_list = []
-    elements_url ='http://purl.org/dc/elements/1.1/'
+    elements_url = 'http://purl.org/dc/elements/1.1/'
     while morepages == 'true':
         xml = requests.get(base_url, params=parameters).text
         xml_root = etree.XML(xml.encode('utf-8'))
         for record in xml_root.find('records'):
             payload = {
-                'doc': etree.tostring(record,encoding='ASCII'),
+                'doc': etree.tostring(record, encoding='ASCII'),
                 'source': 'SciTech',
                 'doc_id': record.find(str(etree.QName(elements_url, 'ostiId'))).text,
                 'filetype': 'xml'
             }
             xml_list.append(payload['doc'])
-            requests.get('http://0.0.0.0:1337/process_raw', params=payload)
         parameters['page'] += 1
         morepages = xml_root.find('records').attrib['morepages']
     return xml_list
@@ -42,6 +42,6 @@ def consume(start_date=param_date_stamp, end_date=None, **kwargs):
 
 def xml_to_text(xml_list=None, date_time=parse_date_stamp):
     """A function to help with debugging. Saves consumed XML as a file."""
-    with open('SciTech_raw_'+date_time+'.xml', 'w') as raw_xml:
+    with open('SciTech_raw_' + date_time + '.xml', 'w') as raw_xml:
         for record in xml_list:
             raw_xml.write(record)
--- scrapi/consumers/scitech/normalize.py
+++ scrapi/consumers/scitech/normalize.py
@@ -1,5 +1,4 @@
 from lxml import etree
-import requests
 import json
 import datetime
 import sys
@@ -21,14 +20,15 @@ sys.setdefaultencoding('utf-8')
 parse_date_stamp = datetime.date.today().strftime('%Y-%m-%d')
 
 
-def parse(xml, timestamp, date_stamp=parse_date_stamp):
-    """A function for parsing the list of XML objects returned by the consume function. 
+def normalize(xml, timestamp, date_stamp=parse_date_stamp):
+    """A function for parsing the list of XML objects returned by the consume function.
     Returns a list of Json objects in a format that can be recognized by the OSF scrapi."""
+
     terms_url = 'http://purl.org/dc/terms/'
     elements_url = 'http://purl.org/dc/elements/1.1/'
     record = etree.XML(xml)
 
-    contributor_list = record.find(str(etree.QName(elements_url,'creator'))).text.split(';')
+    contributor_list = record.find(str(etree.QName(elements_url, 'creator'))).text.split(';')
     # for now, scitech does not grab emails, but it could soon?
     contributors = []
     for name in contributor_list:
@@ -46,33 +46,28 @@ def parse(xml, timestamp, date_stamp=parse_date_stamp):
         tags = re.split(',(?!\s\&)|;', tags)
         tags = [tag.strip() for tag in tags]
 
-    json_scrapi = {
+    return {
         'title': record.find(str(etree.QName(elements_url, 'title'))).text,
         'contributors': contributors,
         'properties': {
-            'doi': record.find(str(etree.QName(elements_url,'doi'))).text,
-            'description': record.find(str(etree.QName(elements_url,'description'))).text,
-            'article_type': record.find(str(etree.QName(elements_url,'type'))).text,
-            'url': record.find(str(etree.QName(terms_url,'identifier-purl'))).text,
-            'date_entered': record.find(str(etree.QName(elements_url,'dateEntry'))).text,
-            'research_org': record.find(str(etree.QName(terms_url,'publisherResearch'))).text,
+            'doi': record.find(str(etree.QName(elements_url, 'doi'))).text,
+            'description': record.find(str(etree.QName(elements_url, 'description'))).text,
+            'article_type': record.find(str(etree.QName(elements_url, 'type'))).text,
+            'url': record.find(str(etree.QName(terms_url, 'identifier-purl'))).text,
+            'date_entered': record.find(str(etree.QName(elements_url, 'dateEntry'))).text,
+            'research_org': record.find(str(etree.QName(terms_url, 'publisherResearch'))).text,
             'research_sponsor': record.find(str(etree.QName(terms_url, 'publisherSponsor'))).text,
             'tags': tags,
             'date_retrieved': date_stamp,
             'date_published': record.find(str(etree.QName(elements_url, 'date'))).text
         },
-        'meta':{},
+        'meta': {},
         'id': record.find(str(etree.QName(elements_url, 'ostiId'))).text,
-        'source':'SciTech'
+        'source': 'SciTech'
     }
-    payload = {
-        'doc': json.dumps(json_scrapi),
-        'timestamp': timestamp
-    }
-    return requests.get('http://0.0.0.0:1337/process', params=payload) 
 
 
 def json_to_text(json_scrapi, date_time=parse_date_stamp):
     """A function to help with debugging. Saves Json produced by parsing XML as a file."""
-    with open('SciTech_parsed_'+date_time+'.json', 'w') as json_txt:
+    with open('SciTech_parsed_' + date_time + '.json', 'w') as json_txt:
         json.dump(json_scrapi, json_txt, indent=4, sort_keys=True)
