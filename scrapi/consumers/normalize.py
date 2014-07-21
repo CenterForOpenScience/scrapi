6b4f9249a678454821b93fcf4db310f4526feecf
fabian <fabian@fabianism.us>
2014-07-21 15:02:05 -0400

    Merge conflict in normalize.py

--- scrapi/consumers/normalize.py
+++ scrapi/consumers/normalize.py
@@ -8,17 +8,15 @@ sys.setdefaultencoding('utf-8')
 
 def normalize(result, timestamp):
     result = xmltodict.parse(result)
-    payload = {
-        "doc": {
-            'title': result["str"][4]["#text"],
-            'contributors': result["arr"][0]["str"],
-            'properties': {
-		'abstract': result["arr"][1]["str"]
-            },
-            'meta': {},
-            'id': result["str"][0]["#text"],
-            'source': "PLoS",
-            'timestamp': str(timestamp)
-        }
+
+    return {
+        'title': result["str"][4]["#text"],
+        'contributors': result["arr"][0]["str"],
+        'properties': {
+		    'abstract': result["arr"][1]["str"]
+        },
+        'meta': {},
+        'id': result["str"][0]["#text"],
+        'source': "PLoS",
+        'timestamp': str(timestamp)
     }
-    return payload
