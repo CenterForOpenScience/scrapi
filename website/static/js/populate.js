6c7c901f8427bc9a283ae70d4a85d5cd1637e18b
Faye Huynh <fh9cp@virginia.edu>
2014-07-21 17:44:52 -0400

    Front end interface for SHARE

---
+++ website/static/js/populate.js
@@ -0,0 +1,40 @@
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+var AppViewModel = new function()
+{
+	var self = this;
+
+	self.keyword = ko.observable("");
+	self.response = ko.observable(null);
+
+    self.populate_results = function()
+    {
+    	$.get("http://localhost:1337/api/search?q="+self.keyword(), function(data){
+    		var item = ""
+    		var returnToJson = $.parseJSON(data);
+    		console.log(returnToJson);
+    		for(var i = 0; i < returnToJson.length; i++){
+    			item += '<div class="jsonObject"><h2>';
+    			item += returnToJson[i]["title"];
+    			item += "</h2>";
+    			item += "<p>";
+    			for (var j = 0; j < returnToJson[i]["contributors"].length-1; j++){
+    				item += returnToJson[i]["contributors"][j]["full_name"]+", ";
+    			}
+    			item += returnToJson[i]["contributors"][returnToJson[i]["contributors"].length-1]["full_name"];
+    			item += "</p>";
+    			item +="<p>";
+    			item += '<b>ID: </b>'+returnToJson[i]["id"];
+    			item +="</p>";
+    			item +="<p>";
+    			item += '<b>Source: </b>'+returnToJson[i]["source"]
+    			item +="</p>";
+    			item +="</div>"
+    		}
+    		
+    		self.response(item);
+    	});
+    }
+}
+
+// Activates knockout.js
+ko.applyBindings(AppViewModel);
