6586eecb2635c9109fc8f1af443fb29d15be2bfd
erinspace <erin.braswell@gmail.com>
2014-07-22 15:59:08 -0400

    consume function should now work properly

--- scrapi/consumers/upenn/normalize.py
+++ scrapi/consumers/upenn/normalize.py
@@ -24,42 +24,25 @@ def normalize(result, timestamp):
 
 
 
-def normalize(nct_id, timestamp):
+def normalize(result, timestamp):
     """Takes a clinicaltrials.gov nct_id and returns json in a format containing info about the trial in a format
     that can be imported by scrAPI."""
-    
-    files = set([f.rstrip('-before').rstrip('-after') for f in glob('files/{0}/*.xml'.format(nct_id))])
-    files = sorted(files, key=lambda v: time.mktime(time.strptime(v.split('/')[-1].rstrip('.xml').split('_')[-1], '%Y%m%d')))
+  
+    result = xmltodict.parse(result)
 
-
-    if len(files) == 0:
-        return None
-    
-    trial = parse('xml/{0}.xml'.format(nct_id))
-
-    versions = {}
-    for f in files:
-        version = f.split('/')[-1].rstrip('.xml').split('_')[-1]
-        v, locations = xml_to_json(f)
-        v['clinical_study']['location'] = l2c(locations)
-        v['clinical_study']['references'] = add_pubmed_to_references(v)
-        v['clinical_study']['keyword'] = ([v['clinical_study'].get('keyword')] or [])+trial['keywords']
-        add_pubmed_to_references(v)
-        versions[version] = v
-
-
-        payload = {
+    payload = {
         "doc": {
-            'title': trial["title"],
-            'contributors': trial["contributors"],
+            'title': result["title"],
+            'contributors': result["contributors"],
             'properties': {
-                'abstract': trial["description"]
+                'abstract': result["description"]
             },
             'meta': {},
-            'id': trial["id"],
+            'id': result["id"],
             'source': "ClinicalTrials.gov",
             'timestamp': str(timestamp)
         }
     }
+
         
     return payload
