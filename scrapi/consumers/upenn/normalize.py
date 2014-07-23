390c67ee6efe6e2c89052d519bf4f7cef06b6fb5
erinspace <erin.braswell@gmail.com>
2014-07-23 17:37:21 -0400

    changed format of normalize and added tests

--- scrapi/consumers/upenn/normalize.py
+++ scrapi/consumers/upenn/normalize.py
@@ -1,35 +1,46 @@
-from lxml import etree
+# from lxml import etree
+from datetime import datetime
+import xmltodict
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
-def parse(result, timestamp):
-    """Used to extract information from an XML document from clinicaltrials.gov 
-    and to return Json for scrAPI """
+def normalize(result, timestamp):
 
-    payload = {}
-    root = etree.parse(result).getroot()
-    doc = {}
+    result = xmltodict.parse(result)
 
-    if root.find('official_title') == None:
-        doc['title'] = root.find('brief_title').text
-    else:
-        doc['title'] = root.find('official_title').text
-
-    doc['contributors'] = []
-    for entry in root.findall('overall_official'):
-        official_dict = {}
-        official_dict['full_name'] = entry.find('last_name').text
-        official_dict['email'] = None
-        doc['contributors'].append(official_dict)
-
-    properties = {}
-    properties['abstract'] = root.find('brief_summary')[0].text
-    doc['properties'] = properties
-
-    doc['meta'] = {}
-    doc['id'] = root.find('id_info').find('nct_id').text
-    doc['timestamp'] = str(timestamp)
-    doc['source'] = 'ClinicalTrials.gov'
-    
-    payload['doc'] = doc
+    payload = {
+        "doc": {
+            'title': result['root']['clinical_study']['brief_title'],
+            'contributors': result['root']['clinical_study']['overall_official']['last_name']['#text'],
+            'properties': {
+                'abstract': result['root']['clinical_study']['brief_summary']
+            },
+            'meta': {},
+            'id': result['root']['clinical_study']['id_info']['nct_id']['#text'],
+            'source': "ClinicalTrials.gov",
+            'timestamp': str(timestamp)
+        }
+    }
 
     return payload
+
+
+def test_dict():
+    with open('output.txt', 'r') as f:
+        result = f.read()
+
+    normed_file = normalize(result, datetime.now())
+
+    assert isinstance(normed_file, dict)
+
+def test_contributors():
+    with open('output.txt', 'r') as f:
+        result = f.read()
+
+    normed_file = normalize(result, datetime.now())
+
+    print normed_file['doc']['contributors']
+
+    assert normed_file['doc']['contributors']
