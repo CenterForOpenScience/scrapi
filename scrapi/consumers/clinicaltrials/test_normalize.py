8b6c53786790ba152c2a534e2eb8ef0d85534635
erinspace <erin.braswell@gmail.com>
2014-07-25 11:52:53 -0400

    Added a new file that hopefully conforms to new architecture

--- scrapi/consumers/clinicaltrials/test_normalize.py
+++ scrapi/consumers/clinicaltrials/test_normalize.py
@@ -10,56 +10,56 @@ try:
 
 except IndexError: 
     print "There are no more new files..."
-    normed_file = []
+    normed_file = {}
 
 def test_first_dict():
     try:
         assert isinstance(normed_file, dict)
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 def test_contributors_exists():    
     try:
         assert normed_file['doc']['contributors']
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 def test_title():
     try:
         assert isinstance(normed_file['doc']['title'], basestring)
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 def test_contributors_is_string():
     try:    
         assert isinstance(normed_file['doc']['contributors'], basestring)
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 def test_abstract_is_string():
     try:
         assert isinstance(normed_file['doc']['properties']['abstract'], basestring)
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 def test_id_is_string():
     try:
         assert isinstance(normed_file['doc']['id'], basestring)
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 def test_source():
     try:
         assert normed_file['doc']['source'] == 'ClinicalTrials.gov'
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 def test_timestamp():
     try:
@@ -68,7 +68,7 @@ def test_timestamp():
         assert isinstance(timestamp, datetime)
     except NameError:
         print "no new files..."
-        assert normed_file == []
+        assert normed_file == {}
 
 
 
