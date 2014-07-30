ee68b0cb503739b439b1f9b42adc19022b7e1877
erinspace <erin.braswell@gmail.com>
2014-07-30 12:44:30 -0400

    version of the consumer that should process docs from the past day

--- scrapi/consumers/ucescholership/consumer.py
+++ scrapi/consumers/ucescholership/consumer.py
@@ -2,13 +2,15 @@
 ## at the University of California
 
 # import requests
-from lxml import etree, html
+from lxml import etree
+from xml.etree import ElementTree
 from datetime import date, timedelta
 from scrapi_tools.consumer import BaseConsumer, RawFile, NormalizedFile
-
+import requests
 
 TODAY = date.today()
 YESTERDAY = TODAY - timedelta(1)
+
 
 class EScholarshipConsumer(BaseConsumer):
 
@@ -20,49 +22,36 @@ class EScholarshipConsumer(BaseConsumer):
 
         base_url = 'http://www.escholarship.org/uc/oai?verb=ListRecords&metadataPrefix=oai_dc&from='
         url = base_url + str(YESTERDAY)
-        # response = requests.get(url)
-        # xml_text = response.content
+        data = requests.get(url)
+        doc =  etree.XML(data.content)
 
-        doc = html.parse(url)
+        namespaces = {'dc': 'http://purl.org/dc/elements/1.1/', 
+                    'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
+                    'ns0': 'http://www.openarchives.org/OAI/2.0/'}
 
-        namespaces = {'dc': 'http://purl.org/dc/elements/1.1/'}
-
-        # records = doc.findall('OAI-PMH/ListRecords/record')
-        records = doc.findall('//record')
-        titles = doc.findall('//title', namespaces=namespaces)
+        records = doc.xpath('//oai_dc:record', namespaces=namespaces)
 
         print 'num records: ' + str(len(records))
-        print 'num titles: ' + str(len(titles))
+        # print 'num titles: ' + str(len(titles))
 
+        xml_list = []
         for record in records:
-            print type(record.text)
-            print str(record.find('title', namespaces=namespaces))
-            print record.text
+            doc_id = record.xpath('ns0:header/ns0:identifier', 
+                                    namespaces=namespaces)[0].text
+            record = ElementTree.tostring(record)
+            record = '<?xml version="1.0" encoding="UTF-8"?>\n' + record
+            xml_list.append(RawFile({
+                        'doc': record,
+                        'source': 'eScholarship',
+                        'doc_id': doc_id,
+                        'filetype': 'xml'
+                    }))
 
-        # for title in titles:
-        #     print title.text
-
-        # for record in records:
-        #     print record.text
-
-        # xml_list = []
-
-        # xml_list.append(RawFile({
-        #                 'doc': content.text,
-        #                 'source': 'eScholarship',
-        #                 'doc_id': doc_id,
-        #                 'filetype': 'xml',
-        #             }))
-
-## OAI-PMH
-## ListRecords
-## record
-
+        return xml_list
 
     def normalize(self):
         pass
 
-EScholarshipConsumer().consume()
 
 # if __name__ == '__main__':
 #     consumer = EScholarshipConsumer()
