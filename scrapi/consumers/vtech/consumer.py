3841e0d459991834aef646c748f8d8cc73aa9b8f
erinspace <erin.braswell@gmail.com>
2014-08-01 17:06:01 -0400

    Now officially ported to new architecture

--- scrapi/consumers/vtech/consumer.py
+++ scrapi/consumers/vtech/consumer.py
@@ -1,17 +1,10 @@
 # Refactored consumer for VTechWorks
 
 from sickle import Sickle 
-from sickle.models import Record
-from lxml import etree
+# from sickle.models import Record
 import xmltodict
 import dicttoxml
-import time
-import os
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 from datetime import date, timedelta
-import json
 
 from scrapi_tools import lint
 from scrapi_tools.document import RawDocument, NormalizedDocument
@@ -20,25 +13,18 @@ NAME = 'VTechWorks'
 
 def consume():
     doc_list = []
-    today = str(date.today())
     yesterday = str(date.today() - timedelta(4))
     sickle = Sickle('http://vtechworks.lib.vt.edu/oai/request')
     records = sickle.ListRecords(
         **{ 'metadataPrefix': 'oai_dc',
         'from': yesterday,
-        'ignore_deleted': 'True'
-        }
-        )
+        'ignore_deleted': 'True'})
 
     # TODO: This assumes that an empty records collection throws a `KeyError`. This should be verified.
     try:
         for record in records:
-            #tree = etree.fromstring(record.raw)
-            #print(etree.tostring(tree))
             doc = xmltodict.parse(record.raw)
-                            #f.write(json.dumps(doc, indent=2))
-            # printing the titles, to show that it's all working
-            # print(doc['record']['metadata']['oai_dc:dc']['dc:title'])
+
             try:
                 doc_list.append(doc['record']) # should grab the header AND metadata, skip XML version
             except KeyError:
@@ -46,15 +32,15 @@ def consume():
     except KeyError:
             print "No new files/updates!"
 
-    with open(os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir)) + '/version', 'r') as f:
-        version = f.readline()
-
     return_list = []
-    test_list = []
     for x in range(0, len(doc_list)):
         doc_xml = doc_list[x]
         doc_id = doc_xml['header']['identifier']
-        return_list.append((dicttoxml.dicttoxml(doc_list[x]), 'VTechWorks', doc_id, 'xml', version))
+        return_list.append(RawDocument({
+            'doc': dicttoxml.dicttoxml(doc_list[x]), 
+            'source': NAME, 
+            'doc_id': doc_id, 
+            'filetype': 'xml'}))
 
 # UNCOMMENT TO RUN TESTS IN NORMALIZE.PY
 #        test_list.append(dicttoxml.dicttoxml(doc_list[x]))
@@ -66,8 +52,11 @@ def consume():
 
 
 def getcontributors(result):
-    thelist = result['root']['metadata']['oai_dc:dc']['dc:contributor']['item']
-    thelistcomprehended = [ item['#text'] for item in thelist ]
+    try:
+        thelist = result['root']['metadata']['oai_dc:dc']['dc:contributor']['item']
+        thelistcomprehended = [ item['#text'] for item in thelist ]
+    except KeyError:
+        thelistcomprehended = ['no contributors']
     return thelistcomprehended
 
 def getabstract(result):
@@ -78,19 +67,24 @@ def getid(result):
     theid = result['root']['header']['identifier']['#text']
     return theid
 
-def normalize(result, timestamp):
-    result2 = xmltodict.parse(result)
+def normalize(raw_doc, timestamp):
+    result = raw_doc.get('doc')
+    result = xmltodict.parse(result)
     payload = {
-        "doc": {
-            'title': result2['root']['metadata']['oai_dc:dc']['key'][5]['#text'],
-            'contributors': [result2['root']['metadata']['oai_dc:dc']['key'][6]['#text']] + getcontributors(result2),
-            'properties': {
-                'abstract': getabstract(result2)
-            },
-            'meta': {},
-            'id': getid(result2),
-            'source': "VTechWorks",
-            'timestamp': str(timestamp)
-        }
+        'title': result['root']['metadata']['oai_dc:dc']['key'][5]['#text'],
+        'contributors': [result['root']['metadata']['oai_dc:dc']['key'][6]['#text']] + getcontributors(result),
+        'properties': {
+            'abstract': getabstract(result)
+        },
+        'meta': {},
+        'id': getid(result),
+        'source': NAME,
+        'timestamp': str(timestamp)
     }
-    return payload
+
+    print payload
+    return NormalizedDocument(payload)
+
+
+if __name__ == '__main__':
+    print(lint(consume, normalize))
