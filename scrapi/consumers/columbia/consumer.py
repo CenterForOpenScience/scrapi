820ca6dc67e9188b4c9970e777d427e6fe901b9a
erinspace <erin.braswell@gmail.com>
2014-08-18 17:24:19 -0400

    just getting a bit started on the bare bones outine

---
+++ scrapi/consumers/columbia/consumer.py
@@ -0,0 +1,13 @@
+# start to a consumer for Columbia's academic commons for the SHARE project
+
+import requests
+from datetime import date, timedelta
+from lxml import etree 
+from scrapi_tools import lint
+from scrapi_tools.document import RawDocument, NormalizedDocument
+
+def consume():
+    pass
+
+def normalize():
+    pass
