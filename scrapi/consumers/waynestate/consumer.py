d7b81d64c7b21010259b5f1b9a6112d589789e70
erinspace <erin.braswell@gmail.com>
2014-09-03 12:34:40 -0400

    changed a variable name

--- scrapi/consumers/waynestate/consumer.py
+++ scrapi/consumers/waynestate/consumer.py
@@ -1,7 +1,6 @@
 ''' Consumer for Wayne State University -- Digital Commons '''
 
 from lxml import etree
-from xml.etree import ElementTree
 from datetime import date, timedelta
 import requests
 from scrapi_tools import lint
@@ -10,25 +9,25 @@ from scrapi_tools.document import RawDocument, NormalizedDocument
 TODAY = date.today()
 NAME = "DigitalCommonsWayneState"
 
-def consume(days_back=1):
+def consume(days_back=5):
     start_date = TODAY - timedelta(days_back)
     base_url = 'http://digitalcommons.wayne.edu/do/oai/?verb=ListRecords&metadataPrefix=oai_dc&from='
     url = base_url + str(start_date) + 'T00:00:00Z'
     print url
     data = requests.get(url)
     doc =  etree.XML(data.content)
-
+    # import pdb; pdb.set_trace()
     namespaces = {'dc': 'http://purl.org/dc/elements/1.1/', 
                 'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
                 'ns0': 'http://www.openarchives.org/OAI/2.0/'}
 
 
     records = doc.xpath('//oai_dc:record', namespaces=namespaces)
-
+    # pdb.set_trace()
     xml_list = []
     for record in records:
         doc_id = record.xpath('ns0:header/ns0:identifier', namespaces=namespaces)[0].text
-        record = ElementTree.tostring(record)
+        record = etree.tostring(record)
         record = '<?xml version="1.0" encoding="UTF-8"?>\n' + record
         xml_list.append(RawDocument({
                     'doc': record,
@@ -42,40 +41,44 @@ def consume(days_back=1):
     ## TODO: fix if there are no records found... what would the XML look like?
 
 def normalize(raw_doc, timestamp):
-    raw_doc = raw_doc.get('doc')
-    doc = etree.XML(raw_doc)
+    doc = raw_doc.get('doc')
+
+    record = etree.XML(doc)
 
     namespaces = {'dc': 'http://purl.org/dc/elements/1.1/', 
                 'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
                 'ns0': 'http://www.openarchives.org/OAI/2.0/'}
 
-    contributors = doc.findall('ns0:metadata/oai_dc:dc/dc:creator', namespaces=namespaces)
+    # contributors #
+    contributors = record.findall('ns0:metadata/oai_dc:dc/dc:creator', namespaces=namespaces)
     contributor_list = []
     for contributor in contributors:
         contributor_list.append({'full_name': contributor.text, 'email':''})
-    title = doc.xpath('//dc:title/node()', namespaces=namespaces)
+    
+    # title
+    title = record.xpath('//dc:title/node()', namespaces=namespaces)[0]
 
-    service_id = doc.xpath('ns0:header/ns0:identifier', namespaces=namespaces)[0].text
+    # ids
+    service_id = record.xpath('ns0:header/ns0:identifier', namespaces=namespaces)[0].text
 
-    all_urls = doc.xpath('//dc:identifier/node()', namespaces=namespaces)
+    all_urls = record.xpath('//dc:identifier/node()', namespaces=namespaces)
     for identifier in all_urls:
         if 'cgi' not in identifier and 'http://digitalcommons.wayne.edu' in identifier:
             url = identifier
 
     ids = {'url': url, 'service_id': service_id, 'doi': ''}
 
-    ## TODO: make this an actual absttract maybe by going to the source...
     try: 
-        description = doc.xpath('ns0:metadata/oai_dc:dc/dc:description/node()', namespaces=namespaces)[0]
+        description = record.xpath('ns0:metadata/oai_dc:dc/dc:description/node()', namespaces=namespaces)[0]
     except IndexError:
-        description = "No abstract available"
+        description = "No description available"
 
-    date_created = doc.xpath('ns0:metadata/oai_dc:dc/dc:date', namespaces=namespaces)[0].text
+    date_created = record.xpath('ns0:metadata/oai_dc:dc/dc:date', namespaces=namespaces)[0].text
 
-    tags = doc.xpath('//dc:subject/node()', namespaces=namespaces)
+    tags = record.xpath('//dc:subject/node()', namespaces=namespaces)
 
     normalized_dict = {
-            'title': title[0],
+            'title': title,
             'contributors': contributor_list,
             'properties': {},
             'description': description,
@@ -87,6 +90,7 @@ def normalize(raw_doc, timestamp):
             'timestamp': str(timestamp)
     }
 
+    print normalized_dict
     return NormalizedDocument(normalized_dict)
         
 
