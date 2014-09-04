3cb5aee70e12266fce446f9deef64f0394909858
erinspace <erin.braswell@gmail.com>
2014-09-04 10:18:26 -0400

    added extra information to properties field

--- scrapi/consumers/dataone/consumer.py
+++ scrapi/consumers/dataone/consumer.py
@@ -40,15 +40,15 @@ def get_response(rows):
 def normalize(raw_doc, timestamp):
     raw_doc = raw_doc.get('doc')
     doc = etree.XML(raw_doc)
-    contributors = doc.xpath("arr[@name='origin']/str/node()") or  doc.xpath("str[@name='author']/node()") or ['No contributors.']
-    submitters = doc.xpath("str[@name='submitter']/node()")
+    contributors = (doc.xpath("str[@name='author']/node()") + doc.xpath("arr[@name='origin']/str/node()"))  or ['No contributors.']
     email = ''
-    for submitter in submitters:
-        if '@' in submitter:
-            email = submitter
+    submitters = doc.xpath("str[@name='submitter']/node()")
     contributor_list = []
     for contributor in contributors:
         contributor_list.append({'full_name': contributor, 'email': email})
+        for submitter in submitters:
+            if '@' in submitter:
+                contributor_list[0]['email'] = submitter
     #TODO: sometimes email info is in submitter or rights holder fields...
     
     try:
@@ -74,10 +74,51 @@ def normalize(raw_doc, timestamp):
 
     date_created = doc.xpath("date[@name='dateUploaded']/node()")[0]
 
+    ## properties ##
+    properties = { 
+        'author': (doc.xpath("str[@name='author']/node()") or [''])[0],
+        'authorGivenName': (doc.xpath("str[@name='authorGivenName']/node()") or [''])[0],
+        'authorSurName': (doc.xpath("str[@name='authorSurName']/node()") or [''])[0],
+        'authoritativeMN' : (doc.xpath("str[@name='authoritativeMN']/node()") or [''])[0],
+        'checksum' : (doc.xpath("str[@name='checksum']/node()") or [''])[0],
+        'checksumAlgorithm' : (doc.xpath("str[@name='checksumAlgorithm']/node()") or [''])[0],
+        'dataUrl': (doc.xpath("str[@name='dataUrl']/node()") or [''])[0],
+        'datasource': (doc.xpath("str[@name='datasource']/node()") or [''])[0],
+
+        'dateModified': (doc.xpath("date[@name='dateModified']/node()") or [''])[0],
+        'datePublished': (doc.xpath("date[@name='datePublished']/node()") or [''])[0],
+        'dateUploaded': (doc.xpath("date[@name='dateUploaded']/node()") or [''])[0],
+        'pubDate': (doc.xpath("date[@name='pubDate']/node()") or [''])[0],
+        'updateDate': (doc.xpath("date[@name='updateDate']/node()") or [''])[0],
+
+        'fileID': (doc.xpath("str[@name='fileID']/node()") or [''])[0],
+        'formatId': (doc.xpath("str[@name='formatId']/node()") or [''])[0],
+        'formatType': (doc.xpath("str[@name='formatType']/node()") or [''])[0],
+
+        'identifier': (doc.xpath("str[@name='identifier']/node()") or [''])[0],
+
+        'investigator': doc.xpath("arr[@name='investigator']/str/node()"),
+        'origin': doc.xpath("arr[@name='origin']/str/node()"),
+
+        'isPublic': (doc.xpath("bool[@name='isPublic']/node()") or [''])[0],
+        'readPermission': doc.xpath("arr[@name='readPermission']/str/node()"),
+        'replicaMN': doc.xpath("arr[@name='replicaMN']/str/node()"),
+        'replicaVerifiedDate': doc.xpath("arr[@name='replicaVerifiedDate']/date/node()"),
+        'replicationAllowed': (doc.xpath("bool[@name='replicationAllowed']/node()") or [''])[0],
+
+        'rightsHolder': (doc.xpath("str[@name='rightsHolder']/node()") or [''])[0],
+
+        'scientificName': doc.xpath("arr[@name='scientificName']/str/node()"),
+        'site': doc.xpath("arr[@name='site']/str/node()"),
+        'size': (doc.xpath("long[@name='size']/node()") or [''])[0],
+        'sku': (doc.xpath("str[@name='sku']/node()") or [''])[0],
+    }
+
+    # updateDate
     normalized_dict = {
             'title': title,
             'contributors': contributor_list,
-            'properties': {},
+            'properties': properties,
             'description': description,
             'meta': {},
             'id': ids,
