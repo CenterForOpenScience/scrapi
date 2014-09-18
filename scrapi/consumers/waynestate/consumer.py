4b4637ad481afaa5b9124731330471f3501c7eb0
erinspace <erin.braswell@gmail.com>
2014-09-18 12:15:21 -0400

    Added support for a resumption token
    Previously, if the request returned more than 100 results, they wouldn't be harvested. Consumer now looks for a resumption token and grabs all of the results from a query.

--- scrapi/consumers/waynestate/consumer.py
+++ scrapi/consumers/waynestate/consumer.py
@@ -9,23 +9,21 @@ from scrapi_tools.document import RawDocument, NormalizedDocument
 TODAY = date.today()
 NAME = "DigitalCommonsWayneState"
 
+NAMESPACES = {'dc': 'http://purl.org/dc/elements/1.1/', 
+            'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
+            'ns0': 'http://www.openarchives.org/OAI/2.0/'}
+
 def consume(days_back=5):
     start_date = TODAY - timedelta(days_back)
     base_url = 'http://digitalcommons.wayne.edu/do/oai/?verb=ListRecords&metadataPrefix=oai_dc&from='
     url = base_url + str(start_date) + 'T00:00:00Z'
-    data = requests.get(url)
-    doc =  etree.XML(data.content)
-    # import pdb; pdb.set_trace()
-    namespaces = {'dc': 'http://purl.org/dc/elements/1.1/', 
-                'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
-                'ns0': 'http://www.openarchives.org/OAI/2.0/'}
+    print url
 
+    records = get_records(url)
 
-    records = doc.xpath('//oai_dc:record', namespaces=namespaces)
-    # pdb.set_trace()
     xml_list = []
     for record in records:
-        doc_id = record.xpath('ns0:header/ns0:identifier', namespaces=namespaces)[0].text
+        doc_id = record.xpath('ns0:header/ns0:identifier', namespaces=NAMESPACES)[0].text
         record = etree.tostring(record)
         record = '<?xml version="1.0" encoding="UTF-8"?>\n' + record
         xml_list.append(RawDocument({
@@ -37,6 +35,20 @@ def consume(days_back=5):
 
     return xml_list
 
+def get_records(url):
+    data = requests.get(url)
+    doc = etree.XML(data.content)
+    records = doc.xpath('//ns0:record', namespaces=NAMESPACES)
+    token = doc.xpath('//ns0:resumptionToken/node()', namespaces=NAMESPACES)
+
+    if len(token) == 1:
+        time.sleep(0.5)
+        base_url = 'http://digitalcommons.wayne.edu/do/oai/?verb=ListRecords&resumptionToken=' 
+        url = base_url + token[0]
+        records += get_records(url)
+
+    return records
+
     ## TODO: fix if there are no records found... what would the XML look like?
 
 def normalize(raw_doc, timestamp):
@@ -44,23 +56,19 @@ def normalize(raw_doc, timestamp):
 
     record = etree.XML(doc)
 
-    namespaces = {'dc': 'http://purl.org/dc/elements/1.1/', 
-                'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-                'ns0': 'http://www.openarchives.org/OAI/2.0/'}
-
     # contributors #
-    contributors = record.findall('ns0:metadata/oai_dc:dc/dc:creator', namespaces=namespaces)
+    contributors = record.findall('ns0:metadata/oai_dc:dc/dc:creator', namespaces=NAMESPACES)
     contributor_list = []
     for contributor in contributors:
         contributor_list.append({'full_name': contributor.text, 'email':''})
     
     # title
-    title = record.xpath('//dc:title/node()', namespaces=namespaces)[0]
+    title = record.xpath('//dc:title/node()', namespaces=NAMESPACES)[0]
 
     # ids
-    service_id = record.xpath('ns0:header/ns0:identifier', namespaces=namespaces)[0].text
+    service_id = record.xpath('ns0:header/ns0:identifier', namespaces=NAMESPACES)[0].text
 
-    all_urls = record.xpath('//dc:identifier/node()', namespaces=namespaces)
+    all_urls = record.xpath('//dc:identifier/node()', namespaces=NAMESPACES)
     for identifier in all_urls:
         if 'cgi' not in identifier and 'http://digitalcommons.wayne.edu' in identifier:
             url = identifier
@@ -68,13 +76,13 @@ def normalize(raw_doc, timestamp):
     ids = {'url': url, 'service_id': service_id, 'doi': ''}
 
     try: 
-        description = record.xpath('ns0:metadata/oai_dc:dc/dc:description/node()', namespaces=namespaces)[0]
+        description = record.xpath('ns0:metadata/oai_dc:dc/dc:description/node()', namespaces=NAMESPACES)[0]
     except IndexError:
         description = "No description available"
 
-    date_created = record.xpath('ns0:metadata/oai_dc:dc/dc:date', namespaces=namespaces)[0].text
+    date_created = record.xpath('ns0:metadata/oai_dc:dc/dc:date', namespaces=NAMESPACES)[0].text
 
-    tags = record.xpath('//dc:subject/node()', namespaces=namespaces)
+    tags = record.xpath('//dc:subject/node()', namespaces=NAMESPACES)
 
     normalized_dict = {
             'title': title,
@@ -89,7 +97,6 @@ def normalize(raw_doc, timestamp):
             'timestamp': str(timestamp)
     }
 
-    print normalized_dict
     return NormalizedDocument(normalized_dict)
         
 
