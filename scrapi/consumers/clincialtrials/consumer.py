73af33d2dc8181a943f8a19cc057117ff8364621
erinspace <erin.braswell@gmail.com>
2014-09-23 16:16:50 -0400

    added many extra properties

--- scrapi/consumers/clincialtrials/consumer.py
+++ scrapi/consumers/clincialtrials/consumer.py
@@ -11,7 +11,7 @@ from scrapi_tools.document import RawDocument, NormalizedDocument
 TODAY = date.today()
 NAME = "ClinicalTrials"
 
-def consume(days_back=1):
+def consume(days_back=4):
     """ First, get a list of all recently updated study urls,
     then get the xml one by one and save it into a list 
     of docs including other information """
@@ -41,6 +41,7 @@ def consume(days_back=1):
     if int(count) > 0:
         # get a new url with all results in it
         url = url + '&count=' + count
+        print url
         total_requests = requests.get(url)
         response = xmltodict.parse(total_requests.text)
 
@@ -49,9 +50,8 @@ def consume(days_back=1):
         for study in response['search_results']['clinical_study']:
             study_urls.append(study['url'] + '?displayxml=true')
 
-        # studies_processed = 0
         # grab each of those urls for full content
-        for study_url in study_urls:
+        for study_url in study_urls[:10]:
             content = requests.get(study_url)
             try:
                 xml_doc = xmltodict.parse(content.text)
@@ -85,6 +85,7 @@ def normalize(raw_doc, timestamp):
 
     xml_doc = etree.XML(raw_doc)
 
+    # Title
     try: 
         title = result['clinical_study']['official_title']
     except KeyError:
@@ -94,9 +95,11 @@ def normalize(raw_doc, timestamp):
             title = 'No title available'
             pass
 
+    # contributors
     contributor_list = xml_doc.xpath('//overall_official/last_name/node()') or xml_doc.xpath('//lead_sponsor/agency/node()') or ['No contributors']
     contributors = [{'full_name': contributor_list[0], 'email': ''}]
 
+    # abstract
     try:
         abstract = result['clinical_study']['brief_summary'].get('textblock')
     except KeyError:
@@ -105,23 +108,48 @@ def normalize(raw_doc, timestamp):
         except KeyError:
             abstract = 'No abstract available'
 
+    # IDs
     try: 
         nct_id = result['clinical_study']['id_info']['nct_id']
     except KeyError:
         nct_id = 'Secondary ID: ' + result['clinical_study']['id_info'].get('secondary_id')
-
     url = result['clinical_study']['required_header'].get('url')
-
     ids = {'service_id': nct_id, 'doi': '', 'url': url}
 
+    # date created
     date_created = result['clinical_study'].get('firstreceived_date')
 
+    # tags/keywords
     keywords = xml_doc.xpath('//keyword/node()')
+
+    lead_sponsor  = {
+            'agency': (xml_doc.xpath('//lead_sponsor/agency/node()') or [''])[0],
+            'agency_class': (xml_doc.xpath('//lead_sponsor/agency_class/node()') or [''])[0]
+        }
+
+    primary_outcome = {
+            'measure': (xml_doc.xpath('//primary_outcome/measure/node()') or [''])[0],
+            'time_frame': (xml_doc.xpath('//primary_outcome/time_frame/node()') or [''])[0],
+            'safety_issue': (xml_doc.xpath('//primary_outcome/safety_issue/node()') or [''])[0]
+        }
+
+    ## extra properties ##
+    properties = {
+        'sponsors': lead_sponsor,
+        'oversight_authority': xml_doc.xpath('//oversigh_info/authority/node()'),
+        'study_design': (xml_doc.xpath('//study_design/node') or [''])[0],
+        'primary_outcome': primary_outcome,
+        'source': (xml_doc.xpath('//source/node()') or [''])[0],
+        'condition': (xml_doc.xpath('//condition/node()') or [''])[0], 
+        'last_changed': (xml_doc.xpath('//lastchanged_date/node()') or [''])[0],
+        'status': (xml_doc.xpath('//status/node()') or [''])[0],
+        'location_countries': xml_doc.xpath('//location_countries/country/node()')
+    }
 
     normalized_dict = {
             'title': title,
             'contributors': contributors,
-            'properties': {},
+            'properties': properties,
             'description': abstract,
             'meta': {},
             'id': ids,
@@ -131,6 +159,7 @@ def normalize(raw_doc, timestamp):
             'timestamp': str(timestamp)
     }
 
+    print normalized_dict['properties']
     return NormalizedDocument(normalized_dict)
 
 
