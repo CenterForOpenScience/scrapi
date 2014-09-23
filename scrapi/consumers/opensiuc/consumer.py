4cd24d0a605595eee8bb3a78ba453f1a5dc8272d
erinspace <erin.braswell@gmail.com>
2014-09-23 11:50:47 -0400

    consumer now prints the sets that were accepted and rejected just so we can get an idea of how many of each we're getting

--- scrapi/consumers/opensiuc/consumer.py
+++ scrapi/consumers/opensiuc/consumer.py
@@ -16,11 +16,20 @@ NAMESPACES = {'dc': 'http://purl.org/dc/elements/1.1/',
             'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
             'ns0': 'http://www.openarchives.org/OAI/2.0/'}
 
-def consume(days_back=5):
+# load the list of approved series_names as a file - second option
+with open('approved_sets.txt') as series_names:
+    series_name_list = [word.replace('\n', '') for word in series_names]
+    
+def consume(days_back=1):
     start_date = TODAY - timedelta(days_back)
     base_url = OAI_DC_BASE + '?verb=ListRecords&metadataPrefix=oai_dc&from='
     url = base_url + str(start_date) + 'T00:00:00Z'
     print url
+
+    num_approved_sets = 0
+    num_rejected_sets = 0
+    approved_sets = []
+    rejected_sets = []
 
     records = get_records(url)
 
@@ -28,16 +37,31 @@ def consume(days_back=5):
     for record in records:
         set_spec = record.xpath('ns0:header/ns0:setSpec/node()', namespaces=NAMESPACES)[0]
         doc_id = record.xpath('ns0:header/ns0:identifier/node()', namespaces=NAMESPACES)[0]
+
+        set_spec = record.xpath('ns0:header/ns0:setSpec/node()', namespaces=NAMESPACES)[0]
+
         record_string = etree.tostring(record)
         record_string = '<?xml version="1.0" encoding="UTF-8"?>\n' + record_string
 
-        # if set_spec.replace('publication:', '') in series_name_list:
+        if set_spec.replace('publication:', '') in series_name_list:
+            approved_sets.append(set_spec)
+            num_approved_sets += 1
+        else:
+            rejected_sets.append(set_spec)
+            num_rejected_sets += 1
+
         xml_list.append(RawDocument({
                     'doc': record_string,
                     'source': NAME,
                     'doc_id': doc_id,
                     'filetype': 'xml'
                 }))
+
+
+    print "There were {} approved sets".format(num_approved_sets)
+    print "These were the approved sets: {}".format(set(approved_sets))
+    print "There were {} rejected sets".format(num_rejected_sets)
+    print "These were the rejected sets: {}".format(set(rejected_sets))
 
     return xml_list
 
@@ -59,20 +83,12 @@ def get_records(url):
 
 def normalize(raw_doc, timestamp):
     doc = raw_doc.get('doc')
-
     record = etree.XML(doc)
-
-    # # save the list of approved series names as a big list - first option
-    # series_name_list = ['acb_frp', 'agtc', 'anthrofrp', 'bio_fuel', 'biomed_eng_frp', 'biomedcentral', 'biosci_frp', 'business_frp', 'ce_eng_frp', 'chemfrp', 'cjfrp', 'cmmg', 'coe_aos', 'coe_khs', 'coe_tbf', 'coe_ted', 'commfrp', 'commsci_frp', 'compscifrp', 'cpcs_pubs', 'csdt', 'ec_eng_frp', 'englishfrp', 'geofrp', 'gerontology', 'humbiol_preprints', 'iehs', 'im_eng_frp', 'immunology_frp', 'libsp', 'mathfrp', 'med_anesthesiology', 'med_biochem', 'med_cardio', 'med_cher', 'med_dermatology', 'med_dho', 'med_did', 'med_dpacs', 'med_edm', 'med_em', 'med_intmed', 'med_neurology', 'med_neurosurgery', 'med_obgyn', 'med_ohn_surgery', 'med_oncology', 'med_opthalmology', 'med_ortho_surgery', 'med_path', 'med_pbn', 'med_pediatrics', 'med_pmr', 'med_radiology', 'med_ro', 'med_surgery', 'med_urology', 'mott_pubs', 'musicfrp', 'nfsfrp', 'nursingfrp', 'pet', 'pharm_appsci', 'pharm_healthcare', 'pharm_practice', 'pharm_science', 'pharma_frp', 'philofrp', 'phy_astro_frp', 'physio_frp', 'prb', 'provost_pub', 'psychfrp', 'skillman', 'slisfrp', 'soc_work_pubs', 'socfrp', 'urbstud_frp', 'antipodes', 'criticism', 'discourse', 'framework', 'humbiol', 'jewishfilm', 'jmasm', 'marvels', 'mpq', 'narrative', 'storytelling']
     
-    # # # load the list of approved series_names as a file - second option
-    # # with open('series_names.txt') as series_names:
-    # #     series_name_list = [word.replace('\n', '') for word in series_names]
-    # set_spec = record.xpath('ns0:header/ns0:setSpec/node()', namespaces=NAMESPACES)[0]
+    set_spec = record.xpath('ns0:header/ns0:setSpec/node()', namespaces=NAMESPACES)[0]
 
-    # if set_spec.replace('publication:', '') not in series_name_list:
-    #     print 'Series not in approved list, not normalizing...'
-    #     return None
+    if set_spec.replace('publication:', '') not in series_name_list:
+        return None
 
     # contributors #
     contributors = record.xpath('//dc:creator', namespaces=NAMESPACES)
@@ -133,3 +149,4 @@ def normalize(raw_doc, timestamp):
 
 if __name__ == '__main__':
     print(lint(consume, normalize))
+    
