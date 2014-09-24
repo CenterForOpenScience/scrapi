1958215d5c50b3a53e10c19886974cd5590290ef
erinspace <erin.braswell@gmail.com>
2014-09-24 11:23:46 -0400

    moved namespaces to the top to get rid of some redundancy

--- scrapi/consumers/ucescholership/consumer.py
+++ scrapi/consumers/ucescholership/consumer.py
@@ -14,23 +14,23 @@ from scrapi_tools.document import RawDocument, NormalizedDocument
 TODAY = date.today()
 NAME = "eScholarship"
 
-def consume(days_back=1):
+NAMESPACES = {'dc': 'http://purl.org/dc/elements/1.1/', 
+            'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
+            'ns0': 'http://www.openarchives.org/OAI/2.0/'}
+
+def consume(days_back=10):
     start_date = TODAY - timedelta(days_back)
     base_url = 'http://www.escholarship.org/uc/oai?verb=ListRecords&metadataPrefix=oai_dc&from='
     url = base_url + str(start_date)
     data = requests.get(url)
     doc =  etree.XML(data.content)
 
-    namespaces = {'dc': 'http://purl.org/dc/elements/1.1/', 
-                'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
-                'ns0': 'http://www.openarchives.org/OAI/2.0/'}
-
-    records = doc.xpath('//oai_dc:record', namespaces=namespaces)
+    records = doc.xpath('//oai_dc:record', namespaces=NAMESPACES)
 
     xml_list = []
     for record in records:
         doc_id = record.xpath('ns0:header/ns0:identifier', 
-                                namespaces=namespaces)[0].text
+                                namespaces=NAMESPACES)[0].text
         record = ElementTree.tostring(record)
         record = '<?xml version="1.0" encoding="UTF-8"?>\n' + record
         xml_list.append(RawDocument({
@@ -48,35 +48,29 @@ def normalize(raw_doc, timestamp):
     raw_doc = raw_doc.get('doc')
     doc = etree.XML(raw_doc)
 
-    namespaces = {'dc': 'http://purl.org/dc/elements/1.1/', 
-                'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-                'ns0': 'http://www.openarchives.org/OAI/2.0/'}
-
-    contributors = doc.findall('ns0:metadata/oai_dc:dc/dc:creator', namespaces=namespaces)
+    contributors = doc.findall('ns0:metadata/oai_dc:dc/dc:creator', namespaces=NAMESPACES)
     contributor_list = []
     for contributor in contributors:
         contributor_list.append({'full_name': contributor.text, 'email': ''})
-    title = doc.findall('ns0:metadata/oai_dc:dc/dc:title', namespaces=namespaces)
+    title = (doc.xpath('//dc:title/node()', namespaces=NAMESPACES) or [''])[0]
 
     service_id = doc.xpath('ns0:header/ns0:identifier/node()', 
-                                    namespaces=namespaces)[0]
-    identifiers = doc.xpath('//dc:identifier/node()', namespaces=namespaces)
+                                    namespaces=NAMESPACES)[0]
+    identifiers = doc.xpath('//dc:identifier/node()', namespaces=NAMESPACES)
     for item in identifiers:
         if 'escholarship.org' in item:
             url = item
 
     ids = {'url': url, 'service_id': service_id, 'doi': 'doi'}
 
-    ## Using this for the abstract for now...
-    ## TODO: make this an actual absttract maybe by going to the source...
-    description = doc.xpath('ns0:metadata/oai_dc:dc/dc:description/node()', namespaces=namespaces) or ['']
+    description = doc.xpath('ns0:metadata/oai_dc:dc/dc:description/node()', namespaces=NAMESPACES) or ['']
 
-    date_created = doc.xpath('//dc:date', namespaces=namespaces)[0].text
+    date_created = doc.xpath('//dc:date', namespaces=NAMESPACES)[0].text
 
-    tags = doc.xpath('//dc:subject/node()', namespaces=namespaces)
+    tags = doc.xpath('//dc:subject/node()', namespaces=NAMESPACES)
 
     normalized_dict = {
-            'title': title[0].text,
+            'title': title,
             'contributors': contributor_list,
             'properties': {},
             'description': description[0],
