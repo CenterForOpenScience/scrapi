28a54fb0242527dc24b25f62033abc672d436b46
erinspace <erin.braswell@gmail.com>
2014-09-24 11:11:57 -0400

    List of approved sets now read from a text file

--- scrapi/consumers/waynestate/consumer.py
+++ scrapi/consumers/waynestate/consumer.py
@@ -15,7 +15,7 @@ NAMESPACES = {'dc': 'http://purl.org/dc/elements/1.1/',
             'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
             'ns0': 'http://www.openarchives.org/OAI/2.0/'}
 
-def consume(days_back=1):
+def consume(days_back=5):
     start_date = TODAY - timedelta(days_back)
     base_url = 'http://digitalcommons.wayne.edu/do/oai/?verb=ListRecords&metadataPrefix=oai_dc&from='
     url = base_url + str(start_date) + 'T00:00:00Z'
@@ -60,11 +60,11 @@ def normalize(raw_doc, timestamp):
     record = etree.XML(doc)
 
     # save the list of approved series names as a big list - first option
-    series_name_list = ['acb_frp', 'agtc', 'anthrofrp', 'bio_fuel', 'biomed_eng_frp', 'biomedcentral', 'biosci_frp', 'business_frp', 'ce_eng_frp', 'chemfrp', 'cjfrp', 'cmmg', 'coe_aos', 'coe_khs', 'coe_tbf', 'coe_ted', 'commfrp', 'commsci_frp', 'compscifrp', 'cpcs_pubs', 'csdt', 'ec_eng_frp', 'englishfrp', 'geofrp', 'gerontology', 'humbiol_preprints', 'iehs', 'im_eng_frp', 'immunology_frp', 'libsp', 'mathfrp', 'med_anesthesiology', 'med_biochem', 'med_cardio', 'med_cher', 'med_dermatology', 'med_dho', 'med_did', 'med_dpacs', 'med_edm', 'med_em', 'med_intmed', 'med_neurology', 'med_neurosurgery', 'med_obgyn', 'med_ohn_surgery', 'med_oncology', 'med_opthalmology', 'med_ortho_surgery', 'med_path', 'med_pbn', 'med_pediatrics', 'med_pmr', 'med_radiology', 'med_ro', 'med_surgery', 'med_urology', 'mott_pubs', 'musicfrp', 'nfsfrp', 'nursingfrp', 'pet', 'pharm_appsci', 'pharm_healthcare', 'pharm_practice', 'pharm_science', 'pharma_frp', 'philofrp', 'phy_astro_frp', 'physio_frp', 'prb', 'provost_pub', 'psychfrp', 'skillman', 'slisfrp', 'soc_work_pubs', 'socfrp', 'urbstud_frp', 'antipodes', 'criticism', 'discourse', 'framework', 'humbiol', 'jewishfilm', 'jmasm', 'marvels', 'mpq', 'narrative', 'storytelling']
+    # series_name_list = ['acb_frp', 'agtc', 'anthrofrp', 'bio_fuel', 'biomed_eng_frp', 'biomedcentral', 'biosci_frp', 'business_frp', 'ce_eng_frp', 'chemfrp', 'cjfrp', 'cmmg', 'coe_aos', 'coe_khs', 'coe_tbf', 'coe_ted', 'commfrp', 'commsci_frp', 'compscifrp', 'cpcs_pubs', 'csdt', 'ec_eng_frp', 'englishfrp', 'geofrp', 'gerontology', 'humbiol_preprints', 'iehs', 'im_eng_frp', 'immunology_frp', 'libsp', 'mathfrp', 'med_anesthesiology', 'med_biochem', 'med_cardio', 'med_cher', 'med_dermatology', 'med_dho', 'med_did', 'med_dpacs', 'med_edm', 'med_em', 'med_intmed', 'med_neurology', 'med_neurosurgery', 'med_obgyn', 'med_ohn_surgery', 'med_oncology', 'med_opthalmology', 'med_ortho_surgery', 'med_path', 'med_pbn', 'med_pediatrics', 'med_pmr', 'med_radiology', 'med_ro', 'med_surgery', 'med_urology', 'mott_pubs', 'musicfrp', 'nfsfrp', 'nursingfrp', 'pet', 'pharm_appsci', 'pharm_healthcare', 'pharm_practice', 'pharm_science', 'pharma_frp', 'philofrp', 'phy_astro_frp', 'physio_frp', 'prb', 'provost_pub', 'psychfrp', 'skillman', 'slisfrp', 'soc_work_pubs', 'socfrp', 'urbstud_frp', 'antipodes', 'criticism', 'discourse', 'framework', 'humbiol', 'jewishfilm', 'jmasm', 'marvels', 'mpq', 'narrative', 'storytelling']
     
     # # load the list of approved series_names as a file - second option
-    # with open('series_names.txt') as series_names:
-    #     series_name_list = [word.replace('\n', '') for word in series_names]
+    with open('series_names.txt') as series_names:
+        series_name_list = [word.replace('\n', '') for word in series_names]
     set_spec = record.xpath('ns0:header/ns0:setSpec/node()', namespaces=NAMESPACES)[0]
 
     if set_spec.replace('publication:', '') not in series_name_list:
