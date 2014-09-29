f3cf68d7469da6519c9d987422e218643deea7de
csheldonhess <coral@sheldon-hess.org>
2014-09-29 19:08:43 -0400

    Make some updates to consume.py, more forthcoming

--- scrapi/consumers/mit/consumer.py
+++ scrapi/consumers/mit/consumer.py
@@ -1,29 +1,32 @@
 # Consumer for MIT DSpace
 
 import requests
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+from dateutil.parser import *
 import time
 from lxml import etree 
 from scrapi_tools import lint
 from scrapi_tools.document import RawDocument, NormalizedDocument
+from nameparser import HumanName
 
 NAME = 'mit'
 TODAY = date.today()
 NAMESPACES = {'dc': 'http://purl.org/dc/elements/1.1/', 
             'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
             'ns0': 'http://www.openarchives.org/OAI/2.0/'}
+OAI_DC_BASE_URL = 'http://dspace.mit.edu/oai/request?verb=ListRecords&metadataPrefix=oai_dc&from='
 
-def consume(days_back=5):
+def consume(days_back=1):
 
     start_date = str(date.today() - timedelta(days_back))
-    base_url = 'http://dspace.mit.edu/oai/request?verb=ListRecords&metadataPrefix=oai_dc&from='
     start_date = TODAY - timedelta(days_back)
     # YYYY-MM-DD hh:mm:ss
-    url = base_url + str(start_date)
+    url = OAI_DC_BASE_URL + str(start_date)
 
     records = get_records(url)
 
     xml_list = []
+    json_list = []
     for record in records:
         doc_id = record.xpath('ns0:header/ns0:identifier', namespaces=NAMESPACES)[0].text
         record = etree.tostring(record)
@@ -34,7 +37,6 @@ def consume(days_back=5):
                     'doc_id': doc_id,
                     'filetype': 'xml'
                 }))
-
     return xml_list
 
 def get_records(url):
@@ -63,12 +65,23 @@ def getcontributors(result):
 
     contributor_list = []
     for person in all_contributors:
-        contributor_list.append({'full_name': person, 'email': ''})
-
+        name = HumanName(person)
+        contributor = {
+            "prefix": name.title,
+            "given": name.first,
+            "middle": name.middle,
+            "family": name.last,
+            "suffix": name.suffix,
+            "email": "",
+            "ORCID": "",
+            }
+        contributor_list.append(contributor)
     return contributor_list
 
 def gettags(result):
     tags = result.xpath('//dc:subject/node()', namespaces=NAMESPACES) or []
+    for tag in tags:
+        tag = tag.lower()
     return tags
 
 def getabstract(result):
@@ -78,6 +91,7 @@ def getabstract(result):
 def getids(result):
     service_id = result.xpath('ns0:header/ns0:identifier/node()', namespaces=NAMESPACES)[0]
     identifiers = result.xpath('//dc:identifier/node()', namespaces=NAMESPACES)
+    identifiers += result.xpath('//dc:relation/node()', namespaces=NAMESPACES)
     url = ''
     doi = ''
     for item in identifiers:
@@ -93,27 +107,27 @@ def getids(result):
 
     return {'service_id': service_id, 'url': url, 'doi': doi}
 
-def get_earliest_date(result):
+def get_date_created(result):
     dates = result.xpath('//dc:date/node()', namespaces=NAMESPACES)
     date_list = []
+    DEFAULT = datetime(2010, 01, 01)
     for item in dates:
         try:
-            a_date = time.strptime(str(item)[:10], '%Y-%m-%d')
+            a_date = parse(str(item)[:10], yearfirst=True).isoformat()
         except ValueError:
             try:
-                a_date = time.strptime(str(item)[:10], '%Y')
+                a_date = parse(str(item)[:10], yearfirst=True).isoformat()
             except ValueError:
                 try:
-                    a_date = time.strptime(str(item)[:10], '%m/%d/%Y')
+                    a_date = parse(str(item)[:10], yearfirst=True).isoformat()
                 except ValueError:
                     try:
-                        a_date = time.strptime(str(item)[:10], '%Y-%d-%m')
+                        a_date = parse(str(item)[:10], yearfirst=True).isoformat()
                     except ValueError:
-                        a_date = time.strptime(str(item)[:10], '%Y-%m')
+                        #import pdb; pdb.set_trace()
+                        a_date = parse(str(item)[:10], yearfirst=True,  default=DEFAULT).isoformat()
         date_list.append(a_date)
-    min_date =  min(date_list) 
-    min_date = time.strftime('%Y-%m-%d', min_date)
-
+    min_date = min(date_list)
     return min_date
 
 def normalize(raw_doc, timestamp):
@@ -126,28 +140,45 @@ def normalize(raw_doc, timestamp):
 
     title = result.xpath('//dc:title/node()', namespaces=NAMESPACES)[0]
     rights = result.xpath('//dc:rights/node()', namespaces=NAMESPACES) or ['']
-    identifiers = result.xpath('//dc:identifier/node()', namespaces=NAMESPACES) or ['']
+    ids = result.xpath('//dc:identifier/node()', namespaces=NAMESPACES) or ['']
+    ids += result.xpath('//dc:relation/node()', namespaces=NAMESPACES)
+    identifiers = []
+    for identifier in ids:
+        if 'http://' not in identifier:
+            identifiers.append(identifier)
+    source = (result.xpath('//dc:source/node()', namespaces=NAMESPACES) or [''])[0]
+    language = (result.xpath('//dc:language/node()', namespaces=NAMESPACES) or [''])[0]
     publisher = (result.xpath('//dc:publisher/node()', namespaces=NAMESPACES) or [''])[0]
     dcformat = result.xpath('//dc:format/node()', namespaces=NAMESPACES) or []
+    dateupdated = result.xpath('//ns0:header/ns0:datestamp/node()', namespaces=NAMESPACES)[0]
+    dateupdated = parse(dateupdated).isoformat()
 
     payload = {
-        'title': title,
-        'contributors': getcontributors(result),
-        'properties': {
-            'rights': rights[0],
-            'identifiers': identifiers,
-            'publisher': publisher,
-            'format': dcformat,
+        "title": title,
+        "contributors": getcontributors(result),
+        "properties": {
+            "permissions": {
+                "copyrightStatement": rights[0],
+            },
+            "identifiers": identifiers,
+            "publisherInfo": {
+                "publisher": publisher,
+            },
+            "format": dcformat,
+            "source": source,
+            "language": language,
         },
-        'description': getabstract(result),
-        'tags': gettags(result),
-        'meta': {},
-        'id': getids(result),
-        'source': NAME,
-        'date_created': get_earliest_date(result),
-        'timestamp': str(timestamp)
+        "description": getabstract(result),
+        "tags": gettags(result),
+        "id": getids(result),
+        "source": NAME,
+        "dateUpdated": dateupdated,
+        "date_created": get_date_created(result),
+        "timestamp": str(timestamp),
     }
 
+    import json
+    print(json.dumps(payload, indent=4))
     return NormalizedDocument(payload)
 
 if __name__ == '__main__':
