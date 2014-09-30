f6860c5988ca0fcff236c69792cfcca15f09b27d
erinspace <erin.braswell@gmail.com>
2014-09-30 18:51:12 -0400

    added support for updates

--- scrapi/consumers/consumer.py
+++ scrapi/consumers/consumer.py
@@ -1,18 +1,19 @@
-__author__ = 'faye'
-from scrapi.linter import lint
-from scrapi.linter.document import RawDocument, NormalizedDocument
-import requests
-import xmltodict
-from lxml import etree
-import json
+from __future__ import unicode_literals
+
 import time
-from nameparser import HumanName
 from datetime import date, timedelta
-import sys
+
+import requests
+
+from lxml import etree
+
 from dateutil.parser import *
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+from nameparser import HumanName
+
+from scrapi.linter import lint
+from scrapi.linter.document import RawDocument, NormalizedDocument
+
 import settings
 
 TODAY = str(date.today()) + "T00:00:00Z"
@@ -30,7 +31,6 @@ def consume(days_back=1):
     base_url += '[{}%20TO%20{}]'.format(START_DATE, TODAY)
     plos_request = requests.get(base_url, params=payload)
     xml_response = etree.XML(plos_request.content)
-    response = xmltodict.parse(plos_request.text)
     num_results = int(xml_response.xpath('//result/@numFound')[0])
     num_results = 5
 
@@ -73,10 +73,10 @@ def consume(days_back=1):
 
 
 def get_ids(raw_doc, record):
-    doi = record.xpath('//str[@name="id"]/node()')[0],
+    doi = record.xpath('//str[@name="id"]/node()')[0]
     ids = {
         'doi': doi,
-        'serviceID': raw_doc.get('doc_id'),
+        'serviceID': raw_doc.get('docID'),
         'url': 'http://dx.doi.org/{}'.format(doi)
     }
     return ids
@@ -103,7 +103,7 @@ def get_properties(record):
     properties = {
         'journal': (record.xpath('//str[@name="journal"]/node()') or [''])[0],
         'eissn': (record.xpath('//str[@name="eissn"]/node()') or [''])[0],
-        'article_type': (record.xpath('//str[@name="article_type"]/node()') or [''])[0],
+        'articleType': (record.xpath('//str[@name="article_type"]/node()') or [''])[0],
         'score': (record.xpath('//float[@name="score"]/node()') or [''])[0],
     }
     return properties
@@ -116,7 +116,7 @@ def get_date_created(record):
 # PLoS doesn't seem to return date updated, so just putting 
 # date consumed here... 
 def get_date_updated(timestamp):
-    return timestamp
+    return str(timestamp)
 
 # No tags... 
 def get_tags(record):
@@ -128,17 +128,27 @@ def normalize(raw_doc, timestamp):
     normalized_dict = {
         'title': record.xpath('//str[@name="title_display"]/node()')[0],
         'contributors': get_contributors(record),
-        'description': record.xpath('//arr[@name="abstract"]/str/node()')[0],
+        'description': (record.xpath('//arr[@name="abstract"]/str/node()') or [''])[0],
         'properties': get_properties(record),
         'id': get_ids(raw_doc, record),
         'source': NAME,
-        'timestamp': timestamp,
+        'timestamp': str(timestamp),
         'dateCreated': get_date_created(record),
         'dateUpdated': get_date_updated(timestamp),
         'tags': get_tags(record),
     }
 
-    print json.dumps(normalized_dict['title'], indent=4)
+    if normalized_dict['properties']['articleType'] == 'Correction':
+        normalized_dict['title'] = normalized_dict['title'].replace('Correction: ', '')
+        normalized_dict['contributors'] = {
+            'prefix': '',
+            'given': '',
+            'middle': '',
+            'family': '',
+            'suffix': '',
+            'email': '',
+            'ORCID': ''
+        }
     return NormalizedDocument(normalized_dict)
 
 if __name__ == '__main__':
