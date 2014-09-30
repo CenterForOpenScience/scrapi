fb312dd956d0212d53153b134c73951b34de50d2
Chris Seto <chriskseto@gmail.com>
2014-09-30 13:44:13 -0400

    Increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='scrAPI Utils',
-    version='0.4.3',
+    version='0.4.4',
     author='Chris Seto',
     author_email='Chris@seto.xyz',
     packages=['scrapi.linter'],
