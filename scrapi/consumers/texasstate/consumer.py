d5c6c15ee51e7334a38ca956380f8e36e75c2bcf
VP Nagraj <vpnagraj@virginia.edu>
2014-10-01 16:26:48 -0400

    updated consumer.py and fixed date created, date updated, json camelcasing, creators in addition to contributors

--- scrapi/consumers/texasstate/consumer.py
+++ scrapi/consumers/texasstate/consumer.py
@@ -1,17 +1,21 @@
 # Consumer for Texas State University
 
 import requests
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+from dateutil.parser import *
 import time
-from lxml import etree 
-from scrapi_tools import lint
-from scrapi_tools.document import RawDocument, NormalizedDocument
+from lxml import etree
+from scrapi.linter import lint
+from scrapi.linter.document import RawDocument, NormalizedDocument
+from nameparser import HumanName
+import json
 
-NAME = 'texas state'
+NAME = 'DSpace at Texas State University'
 TODAY = date.today()
 NAMESPACES = {'dc': 'http://purl.org/dc/elements/1.1/', 
             'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
             'ns0': 'http://www.openarchives.org/OAI/2.0/'}
+DEFAULT = datetime(1970, 01, 01)
 
 def consume(days_back=250):
     # days back is set so high because texas state consumer had nothing for the last six months plus when consumer was built
@@ -31,7 +35,7 @@ def consume(days_back=250):
         xml_list.append(RawDocument({
                     'doc': record,
                     'source': NAME,
-                    'doc_id': doc_id,
+                    'docID': doc_id,
                     'filetype': 'xml'
                 }))
 
@@ -48,57 +52,95 @@ def get_records(url):
 
 def getcontributors(result):
     contributors = result.xpath('//dc:contributor/node()', namespaces=NAMESPACES) or ['']
+    creators = result.xpath('//dc:creator/node()', namespaces=NAMESPACES) or ['']
+    # contributor_list = []
+    # for person in contributors:
+    #     contributor_list.append({'full_name': person, 'email': ''})
+    # return contributor_list
+
+    all_contributors = contributors + creators
+
     contributor_list = []
-    for person in contributors:
-        contributor_list.append({'full_name': person, 'email': ''})
+    for person in all_contributors:
+        name = HumanName(person)
+        contributor = {
+            'prefix': name.title,
+            'given': name.first,
+            'middle': name.middle,
+            'family': name.last,
+            'suffix': name.suffix,
+            'email': '',
+            'ORCID': '',
+            }
+        contributor_list.append(contributor)
     return contributor_list
 
 def gettags(result):
     tags = result.xpath('//dc:subject/node()', namespaces=NAMESPACES) or []
-    return tags
+    return [tag.lower() for tag in tags]
 
-def getabstract(result):
-    abstract = result.xpath('//dc:description/node()', namespaces=NAMESPACES) or ['No abstract']
-    return abstract[0]
-
-def getids(result):
-    service_id = result.xpath('ns0:header/ns0:identifier/node()', namespaces=NAMESPACES)[0]
+def get_ids(result, doc):
+    serviceID = doc.get('docID')
     identifiers = result.xpath('//dc:identifier/node()', namespaces=NAMESPACES)
     url = ''
     doi = ''
     for item in identifiers:
         if 'hdl.handle.net' in item:
             url = item
-        if 'dx.doi.org' in item:
+        if 'doi' in item or 'DOI' in item:
             doi = item
+            doi = doi.replace('doi:', '')
+            doi = doi.replace('DOI:', '')
+            doi = doi.replace('http://dx.doi.org/', '')
+            doi = doi.strip(' ')
 
-    if url == '':
-         raise Exception('Warning: No url provided!')
+    #if url == '':
+    #   raise Exception('Warning: No url provided!')
 
-    return {'service_id': service_id, 'url': url, 'doi': doi}
+    return {'serviceID': serviceID, 'url': url, 'doi': doi}
 
-def get_earliest_date(result):
-    dates = result.xpath('//dc:date/node()', namespaces=NAMESPACES)
-    date_list = []
-    for item in dates:
-        try:
-            a_date = time.strptime(str(item)[:10], '%Y-%m-%d')
-        except ValueError:
-            try:
-                a_date = time.strptime(str(item)[:10], '%Y')
-            except ValueError:
-                try:
-                    a_date = time.strptime(str(item)[:10], '%m/%d/%Y')
-                except ValueError:
-                    try:
-                        a_date = time.strptime(str(item)[:10], '%Y-%d-%m')
-                    except ValueError:
-                        a_date = time.strptime(str(item)[:10], '%Y-%m')
-        date_list.append(a_date)
-    min_date =  min(date_list) 
-    min_date = time.strftime('%Y-%m-%d', min_date)
+def get_properties(result):
+    result_type = (result.xpath('//dc:type/node()', namespaces=NAMESPACES) or [''])[0]
+    rights = result.xpath('//dc:rights/node()', namespaces=NAMESPACES) or ['']
+    if len(rights) > 1:
+        copyright = ' '.join(rights)
+    else:
+        copyright = rights
+    publisher = (result.xpath('//dc:publisher/node()', namespaces=NAMESPACES) or [''])[0]
+    relation = (result.xpath('//dc:relation/node()', namespaces=NAMESPACES) or [''])[0]
+    language = (result.xpath('//dc:language/node()', namespaces=NAMESPACES) or [''])[0]
+    dates = result.xpath('//dc:date/node()', namespaces=NAMESPACES) or ['']
+    props = {
+        'type': result_type,
+        'dates': dates,
+        'language': language,
+        'relation': relation,
+        'publisherInfo': {
+            'publisher': publisher,
+        },
+        'permissions': {
+            'copyrightStatement': copyright,
+        },
+    }
+    return props
 
-    return min_date
+def get_date_created(result):
+    dates = result.xpath('//dc:date/node()', namespaces=NAMESPACES) or ['']
+    return dates[0]
+    # date_list = []
+    # for item in dates:
+    #     try: 
+    #         a_date = parse(str(item)[:10], yearfirst=True,  default=DEFAULT).isoformat()
+    #     except ValueError: 
+    #         import pdb; pdb.set_trace()
+    #     date_list.append(a_date)
+    # min_date = date_list[0]
+    # return min_date
+
+def get_date_updated(result):
+    dateupdated = result.xpath('//ns0:header/ns0:datestamp/node()', namespaces=NAMESPACES)[0]
+    date_updated = parse(dateupdated).isoformat()
+    return date_updated
 
 def normalize(raw_doc, timestamp):
     result = raw_doc.get('doc')
@@ -109,30 +151,22 @@ def normalize(raw_doc, timestamp):
         return None
 
     title = result.xpath('//dc:title/node()', namespaces=NAMESPACES)[0]
-    result_type = result.xpath('//dc:type/node()', namespaces=NAMESPACES) or ['']
-    rights = result.xpath('//dc:rights/node()', namespaces=NAMESPACES) or ['']
-    identifiers = result.xpath('//dc:identifier/node()', namespaces=NAMESPACES) or ['']
-    publisher = result.xpath('//dc:publisher/node()', namespaces=NAMESPACES) or ['']
+    description = (result.xpath('//dc:description/node()', namespaces=NAMESPACES) or [''])[0]
 
     payload = {
         'title': title,
         'contributors': getcontributors(result),
-        'properties': {
-            'type': result_type[0],
-            'rights': rights[0],
-            'identifiers': identifiers,
-            'publisher': publisher[0]
-        },
-        'description': getabstract(result),
+        'properties': get_properties(result),
+        'description': description,
         'tags': gettags(result),
-        'meta': {},
-        'id': getids(result),
+        'id': get_ids(result,raw_doc),
         'source': NAME,
-        'date_created': get_earliest_date(result),
-        'timestamp': str(timestamp)
+        'dateCreated': get_date_created(result),
+        'dateUpdated': get_date_updated(result),
+        'timestamp': str(timestamp),
 
     }
-    print payload
+    #print payload
     return NormalizedDocument(payload)
     ## TODO catch namespace exception
 
