1ad01538c07009617f2b7b33279bd8f81aae84ee
erinspace <erin.braswell@gmail.com>
2014-09-30 22:06:41 -0400

    added more helper functions

--- scrapi/consumers/doepages/consumer.py
+++ scrapi/consumers/doepages/consumer.py
@@ -5,9 +5,14 @@ import requests
 from lxml import etree
 from datetime import date, timedelta, datetime
 
-from scrapi_tools import lint
-from scrapi_tools.document import RawDocument, NormalizedDocument
+import json
 
+from nameparser import HumanName
+
+from dateutil.parser import *
+
+from scrapi.linter import lint
+from scrapi.linter.document import RawDocument, NormalizedDocument
 
 NAME = 'doepages'
 TODAY = date.today()
@@ -16,40 +21,40 @@ NAMESPACES = {'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
             'dc': 'http://purl.org/dc/elements/1.1/',
             'dcq': 'http://purl.org/dc/terms/'}
 
-def consume(days_back=30):
+def consume(days_back=15):
     start_date = TODAY - timedelta(days_back)
-    base_url = 'http://www.osti.gov/pages/pagesxml?nrows=3000&EntryDateFrom='
-    url = base_url + start_date.strftime('%m/%d/%Y')
+    base_url = 'http://www.osti.gov/pages/pagesxml?nrows={0}&EntryDateFrom={1}'
+    url = base_url.format('1', start_date.strftime('%m/%d/%Y'))
+    initial_data = requests.get(url)
+    initial_doc = etree.XML(initial_data.content)
 
+    num_results = int(initial_doc.xpath('//records/@count', namespaces=NAMESPACES)[0])
+
+    url = base_url.format(num_results, start_date.strftime('%m/%d/%Y'))
+    print url
     data = requests.get(url)
     doc = etree.XML(data.content)
-
-    # NOTE: only grabs the first 3000 results
-    num_results = doc.xpath('//records[@count]', namespaces=NAMESPACES)[0].attrib['count']
-    if int(num_results) > 3000:
-        raise Exception('More results than have been consumed!! There are {} results.'.format(num_results))
 
     records = doc.xpath('records/record')
 
     xml_list = []
     for record in records:
-        doc_id = record.xpath('dc:ostiId', 
-                                namespaces=NAMESPACES)[0].text
+        doc_id = record.xpath('dc:ostiId/node()', namespaces=NAMESPACES)[0]
         record = etree.tostring(record)
         record = '<?xml version="1.0" encoding="UTF-8"?>\n' + record
         xml_list.append(RawDocument({
                     'doc': record,
                     'source': NAME,
-                    'doc_id': doc_id,
+                    'docID': doc_id,
                     'filetype': 'xml'
                 }))
 
     return xml_list
 
-def get_ids(doc):
+def get_ids(doc, raw_doc):
     ids = {}
     ids['doi'] = (doc.xpath('//dc:doi/node()', namespaces=NAMESPACES) or [''])[0]
-    ids['service_id'] = (doc.xpath('//dc:ostiId/node()', namespaces=NAMESPACES) or [''])[0]
+    ids['serviceID'] = raw_doc.get('docID')
     url = (doc.xpath('//dcq:identifier-citation/node()', namespaces=NAMESPACES) or [''])[0]
     if url == '':
         url = 'http://dx.doi.org/' + ids['doi']
@@ -59,34 +64,70 @@ def get_ids(doc):
 
     return ids
 
-def normalize(raw_doc, timestamp):
-    raw_doc = raw_doc.get('doc')
-    doc = etree.XML(raw_doc)
-
-    ## contributors ##
+def get_contributors(doc):
     contributor_list = []
     full_contributors = doc.xpath('//dc:creator/node()', namespaces=NAMESPACES)[0].split(';')
-    for contributor in full_contributors:
-        contributor_list.append({'full_name': contributor, 'email': ''})
+    for person in full_contributors:
+        name = HumanName(person)
+        contributor = {
+            'prefix': name.title,
+            'given': name.first,
+            'middle': name.middle,
+            'family': name.last,
+            'suffix': name.suffix,
+            'email': '',
+            'ORCID': ''
+        }
+        contributor_list.append(contributor)
+
+    return contributor_list
+def get_properties(doc):
+    properties = {
+        'publisher': (doc.xpath('//dcq:publisher/node()', namespaces=NAMESPACES) or [''])[0],
+        'publisherSponsor': (doc.xpath('//dcq:publisherSponsor/node()', namespaces=NAMESPACES) or [''])[0],
+        'language': (doc.xpath('//dc:language/node()', namespaces=NAMESPACES) or [''])[0],
+        'type': (doc.xpath('//dc:type/node()', namespaces=NAMESPACES) or [''])[0]
+    }
+    return properties
+
+   # "publisherInfo": {
+   #      "publisher": 
+   #      "publisherSponsor": 
+   #      "publisherAvailability": 
+   #      "publisherResearch": 
+   #      "publisherCountry": 
+   #  },  
+
+def get_date_created(doc):
+    date_created = doc.xpath('//dc:date/node()', namespaces=NAMESPACES)[0]
+    return parse(date_created).isoformat()
+
+def get_date_updated(doc):
+    date_updated = doc.xpath('//dc:dateEntry/node()', namespaces=NAMESPACES)[0]
+    return parse(date_updated).isoformat()
+
+def get_tags(doc):
+    tags = doc.xpath('//dc:subject/node()', namespaces=NAMESPACES) or []
+    return [tag.lower() for tag in tags]
+
+def normalize(raw_doc, timestamp):
+    raw_doc_string = raw_doc.get('doc')
+    doc = etree.XML(raw_doc_string)
+
     normalized_dict = {
         'title': doc.xpath('//dc:title/node()', namespaces=NAMESPACES)[0],
-        'contributors': contributor_list,
-        'properties': {
-                'publisher': (doc.xpath('//dcq:publisher/node()', namespaces=NAMESPACES) or [''])[0],
-                'publisher_sponsor': (doc.xpath('//dcq:publisherSponsor/node()', namespaces=NAMESPACES) or [''])[0],
-                'language': (doc.xpath('//dc:language/node()', namespaces=NAMESPACES) or [''])[0],
-                'type': (doc.xpath('//dc:type/node()', namespaces=NAMESPACES) or [''])[0]
+        'contributors': get_contributors(doc),
 
-        },
         'description': (doc.xpath('//dc:description/node()', namespaces=NAMESPACES) or [''])[0],
-        'meta': {},
-        'id': get_ids(doc),
+        'id': get_ids(doc, raw_doc),
         'source': NAME,
-        'tags': doc.xpath('//dc:subject/node()', namespaces=NAMESPACES) or [],
-        'date_created': doc.xpath('//dc:date/node()', namespaces=NAMESPACES)[0],
+        'tags': get_tags(doc),
+        'dateCreated': get_date_created(doc),
+        'dateUpdated': get_date_updated(doc),
         'timestamp': str(timestamp)
     }
 
+    print json.dumps(normalized_dict['id'], indent=4)
     return NormalizedDocument(normalized_dict)
 
 
