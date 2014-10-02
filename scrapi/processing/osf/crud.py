88e25469f2d30dfd4e060a769d71bdb9fbeffb70
Chris Seto <chriskseto@gmail.com>
2014-10-02 10:48:51 -0400

    Update a bunch of crud methods

--- scrapi/processing/osf/crud.py
+++ scrapi/processing/osf/crud.py
@@ -1,4 +1,7 @@
+from __future__ import unicode_literals
+
 import json
+from copy import deepcopy
 
 import requests
 
@@ -13,13 +16,30 @@ POST_HEADERS = {
 def create_resource(normalized, hashlist):
     bundle = {
         'systemData': {
-            'uuid': hashlist,
-            'is_project': True
+            'isProject': True
         },
         'permissions': ['read']
     }
 
-    return _create_node(normalized, bundle)['id']
+    return _create_node(normalized, bundle, hashlist)['id']
+
+
+def update_resource(normalized, resource):
+    current = _get_metadata(resource)
+    # TODO Remove information that would not be included on project ie source
+    # and service IDs
+    if not is_claimed(resource):
+        pass
+
+    # TODO Update actual osf project if unclaimed
+    if current['collisionCategory'] > normalized['collisionCategory']:
+        new = deepcopy(current.attributes)
+        new.update(normalized)
+    else:
+        new = deepcopy(normalized.attributes)
+        new.update(current)
+
+    return _post_metadata(resource, new)
 
 
 def create_report(normalized, parent, hashlist):
@@ -27,43 +47,22 @@ def create_report(normalized, parent, hashlist):
         'title': '{}: {}'.format(normalized['source'], normalized['title']),
         'parent': parent,
         'category': 'report',
-        'systemData': {
-            'uuid': hashlist
-        },
     }
 
-    return _create_node(normalized, bundle)['id']
+    return _create_node(normalized, bundle, hashlist)['id']
 
 
-def _create_node(normalized, additional):
-    contributors = [
-        {
-            'name': '{given} {middle} {family}'.format(**x),
-            'email': x.get('email')
-        }
-        for x in normalized['contributors']
-    ]
+def update_report(normalized, report):
+    current = _get_metadata(report)
 
-    bundle = {
-        'title': normalized['title'],
-        'description': normalized.get('description'),
-        'contributors': contributors,
-        'tags': normalized.get('tags'),
-        'metadata': normalized.attributes,
-    }
+    if current['collisionCategory'] > normalized['collisionCategory']:
+        new = current.attributes
+        new.update(normalized)
+    else:
+        new = normalized.attributes
+        new.update(current)
 
-    bundle.update(additional)
-
-    kwargs = {
-        'auth': settings.OSF_AUTH,
-        'data': json.dumps(bundle),
-        'headers': POST_HEADERS
-    }
-    return requests.post(settings.OSF_NEW_PROJECT, **kwargs).json()
-
-
-def _get_metadata(id):
-    pass
+    return _post_metadata(report, new)
 
 
 def is_event(normalized):
@@ -78,14 +77,57 @@ def is_claimed(resource):
     pass
 
 
-def update_resource(normalized, resource):
-    pass
+def get_collision_cat(source):
+    return settings.MANIFESTS[source]['collisionCategory']
 
 
-def update_report(normalized, report):
+def clean_report(normalized):
+    new = deepcopy(normalized)
+    del new['source']
+    del new['id']
+    return new
+
+
+def _create_node(normalized, additional, hashlist):
+    contributors = [
+        {
+            'name': '{given} {middle} {family}'.format(**x),
+            'email': x.get('email')
+        }
+        for x in normalized['contributors']
+    ]
+
+    bundle = {
+        'title': normalized['title'],
+        'description': normalized.get('description'),
+        'contributors': contributors,
+        'tags': normalized.get('tags'),
+        'metadata': deepcopy(normalized.attributes),
+    }
+
+    bundle.update(additional)
+
+    bundle['metadata']['uuid'] = hashlist
+
     kwargs = {
-        'data': json.dumps(normalized.attributes),
+        'auth': settings.OSF_AUTH,
+        'data': json.dumps(bundle),
         'headers': POST_HEADERS
     }
-    url = '{}{}/'.format(settings.OSF_APP_URL, report)
+    return requests.post(settings.OSF_NEW_PROJECT, **kwargs).json()
+
+
+def _get_metadata(id):
+    url = '{}{}/'.format(settings.OSF_APP_URL, id)
+    return requests.get(url, auth=settings.OSF_AUTH).json()
+
+
+def _post_metadata(id, data):
+    kwargs = {
+        'data': json.dumps(data),
+        'headers': POST_HEADERS,
+        'auth': settings.OSF_AUTH
+    }
+    url = '{}{}/'.format(settings.OSF_APP_URL, id)
+
     requests.post(url, **kwargs)
