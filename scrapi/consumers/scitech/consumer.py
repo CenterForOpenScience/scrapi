8b72f8e99bb2fd7ce2b7c241f2eadb929154700a
erinspace <erin.braswell@gmail.com>
2014-10-01 20:13:24 -0400

    add some more helper functions

--- scrapi/consumers/scitech/consumer.py
+++ scrapi/consumers/scitech/consumer.py
@@ -1,9 +1,16 @@
-from scrapi_tools import lint
-from scrapi_tools.document import RawDocument, NormalizedDocument
-from lxml import etree
+
+import re
 import requests
 import datetime
-import re
+
+from lxml import etree
+
+from nameparser import HumanName
+
+from dateutil.parser import *
+
+from scrapi.linter import lint
+from scrapi.linter.document import RawDocument, NormalizedDocument
 
 TODAY = datetime.date.today()
 YESTERDAY = TODAY - datetime.timedelta(1)
@@ -33,69 +40,88 @@ def consume(days_back=1, end_date=None, **kwargs):
             xml_list.append(RawDocument({
                 'doc': etree.tostring(record, encoding='ASCII'),
                 'source': NAME,
-                'doc_id': record.find(str(etree.QName(elements_url, 'ostiId'))).text,
+                'docID': record.find(str(etree.QName(elements_url, 'ostiId'))).text,
                 'filetype': 'xml',
             }))
         parameters['page'] += 1
         morepages = xml_root.find('records').attrib['morepages']
     return xml_list
 
+def get_ids(record, raw_doc):
+    ids =  {
+        'service_id': raw_doc.get('docID'),
+        'doi': record.find(str(etree.QName(elements_url, 'doi'))).text or '',
+        'url': record.find(str(etree.QName(terms_url, 'identifier-purl'))).text or '',
+    }
+    return ids
+
+def get_properties(record):
+    properties = {
+        'article_type': record.find(str(etree.QName(elements_url, 'type'))).text or '',
+        'date_entered': record.find(str(etree.QName(elements_url, 'dateEntry'))).text or '',
+        'research_org': record.find(str(etree.QName(terms_url, 'publisherResearch'))).text or '',
+        'research_sponsor': record.find(str(etree.QName(terms_url, 'publisherSponsor'))).text or '',
+        'research_country': record.find(str(etree.QName(terms_url, 'publisherCountry'))).text or '',
+        'identifier_info': {
+            'identifier': record.find(str(etree.QName(elements_url, 'identifier'))).text or "",
+            'identifier_report': record.find(str(etree.QName(elements_url, 'identifierReport'))).text or "",
+            'identifier_contract': record.find(str(etree.QName(terms_url, 'identifierDOEcontract'))) or "",
+            'identifier_citation': record.find(str(etree.QName(terms_url, 'identifier-citation'))) or "",
+            'identifier_other': record.find(str(etree.QName(elements_url, 'identifierOther'))) or ""
+        },
+        'relation': record.find(str(etree.QName(elements_url, 'relation'))).text or "",
+        'coverage': record.find(str(etree.QName(elements_url, 'coverage'))).text or "",
+        'format': record.find(str(etree.QName(elements_url, 'format'))).text or "",
+        'language': record.find(str(etree.QName(elements_url, 'language'))).text or ""
+    }
+    return properties
+
+def get_tags(record):
+    tags = record.find(str(etree.QName(elements_url, 'subject'))).text
+    tags = re.split(',(?!\s\&)|;', tags) if tags is not None else []
+    tags = [tag.strip().lower() for tag in tags]
+    return tags
+
+def get_contributors(record):
+    contributors = record.find(str(etree.QName(elements_url, 'creator'))).text.split(';') or ['']
+    # for now, scitech does not grab emails, but it could soon?
+    contributor_list = []
+    for person in contributors:
+        person = person.strip()
+        if person[0] in ['/', ',', 'et. al']:
+            continue
+        if '[' in person:
+            person = person[:person.index('[')].strip()
+        name = HumanName(person)
+        contributor = {
+            'prefix': name.title,
+            'given': name.first,
+            'middle': name.middle,
+            'family': name.last,
+            'suffix': name.suffix,
+            'email': '',
+            'ORCID': ''
+        }
+        contributor_list.append(contributor)
+    return contributor_list
 
 def normalize(raw_doc, timestamp):
     """A function for parsing the list of XML objects returned by the 
     consume function.
     Returns a list of Json objects in a format that can be recognized 
     by the OSF scrapi."""
-    raw_doc = raw_doc.get('doc')
+    raw_doc_str = raw_doc.get('doc')
     terms_url = 'http://purl.org/dc/terms/'
     elements_url = 'http://purl.org/dc/elements/1.1/'
-    record = etree.XML(raw_doc)
+    record = etree.XML(raw_doc_str)
 
-    contributor_list = record.find(str(etree.QName(elements_url, 'creator'))).text.split(';') or ['']
-    # for now, scitech does not grab emails, but it could soon?
-    contributors = []
-    for name in contributor_list:
-        name = name.strip()
-        if name[0] in ['/', ',', 'et. al']:
-            continue
-        if '[' in name:
-            name = name[:name.index('[')].strip()
-        contributor = {}
-        contributor['full_name'] = name
-        contributor['email'] = ''
-        contributors.append(contributor)
 
-    tags = record.find(str(etree.QName(elements_url, 'subject'))).text
-    tags = re.split(',(?!\s\&)|;', tags) if tags is not None else []
-    tags = [tag.strip() for tag in tags]
 
     normalized_dict = {
         'title': record.find(str(etree.QName(elements_url, 'title'))).text,
         'contributors': contributors,
-        'properties': {
-            'article_type': record.find(str(etree.QName(elements_url, 'type'))).text or '',
-            'date_entered': record.find(str(etree.QName(elements_url, 'dateEntry'))).text or '',
-            'research_org': record.find(str(etree.QName(terms_url, 'publisherResearch'))).text or '',
-            'research_sponsor': record.find(str(etree.QName(terms_url, 'publisherSponsor'))).text or '',
-            'research_country': record.find(str(etree.QName(terms_url, 'publisherCountry'))).text or '',
-            'identifier_info': {
-                'identifier': record.find(str(etree.QName(elements_url, 'identifier'))).text or "",
-                'identifier_report': record.find(str(etree.QName(elements_url, 'identifierReport'))).text or "",
-                'identifier_contract': record.find(str(etree.QName(terms_url, 'identifierDOEcontract'))) or "",
-                'identifier_citation': record.find(str(etree.QName(terms_url, 'identifier-citation'))) or "",
-                'identifier_other': record.find(str(etree.QName(elements_url, 'identifierOther'))) or ""
-            },
-            'relation': record.find(str(etree.QName(elements_url, 'relation'))).text or "",
-            'coverage': record.find(str(etree.QName(elements_url, 'coverage'))).text or "",
-            'format': record.find(str(etree.QName(elements_url, 'format'))).text or "",
-            'language': record.find(str(etree.QName(elements_url, 'language'))).text or ""
-        },
-        'meta': {},
-        'id': {
-            'service_id': record.find(str(etree.QName(elements_url, 'ostiId'))).text,
-            'doi': record.find(str(etree.QName(elements_url, 'doi'))).text or '',
-            'url': record.find(str(etree.QName(terms_url, 'identifier-purl'))).text or "",
-        },
+        'properties': get_properties(doc),
+        'id': get_ids(record, raw_doc),
         'source': NAME,
         'timestamp': str(timestamp),
         'date_created': record.find(str(etree.QName(elements_url, 'date'))).text,
