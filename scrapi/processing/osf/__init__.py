7591189527ad05be62a561afadf70b217d725b1f
Chris Seto <chriskseto@gmail.com>
2014-10-02 10:47:20 -0400

    Make sure to keep certain report fields out of resources

--- scrapi/processing/osf/__init__.py
+++ scrapi/processing/osf/__init__.py
@@ -11,18 +11,23 @@ class OSFProcessor(BaseProcessor):
             crud.create_event(normalized)
             return
 
-        report_hash = collision.generate_report_hash_list(normalized)
-        resource_hash = collision.generate_resource_hash_list(normalized)
+        normalized['collisionCategory'] = crud.get_collision_cat(normalized['source'])
+
+        report_norm = normalized
+        resource_norm = crud.clean_report(normalized)
+
+        report_hash = collision.generate_report_hash_list(report_norm)
+        resource_hash = collision.generate_resource_hash_list(resource_norm)
 
         report = collision.detect_collisions(report_hash)
         resource = collision.detect_collisions(resource_hash)
 
         if not resource:
-            resource = crud.create_resource(normalized, resource_hash)
+            resource = crud.create_resource(resource_norm, resource_hash)
         elif not crud.is_claimed(resource):
-            crud.update_resource(normalized, resource)
+            crud.update_resource(resource_norm, resource)
 
         if not report:
-            crud.create_report(normalized, resource, report_hash)
+            crud.create_report(report_norm, resource, report_hash)
         else:
-            crud.update_report(normalized, report)
+            crud.update_report(report_norm, report)
