c19c69926e54e8268b7587a91264a976724a8801
Chris Seto <chriskseto@gmail.com>
2014-10-03 09:52:02 -0400

    Increment version number for latest linter version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='scrAPI Utils',
-    version='0.4.7',
+    version='0.4.8',
     author='Chris Seto',
     author_email='Chris@seto.xyz',
     packages=['scrapi.linter'],
