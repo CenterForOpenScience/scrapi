29326e6edd05663e892be29a79f8599595e645d1
csheldonhess <coral@sheldon-hess.org>
2014-10-04 15:27:09 -0400

    This is not working.

--- scrapi/consumers/stcloud/consumer.py
+++ scrapi/consumers/stcloud/consumer.py
@@ -10,17 +10,19 @@ from scrapi.linter.document import RawDocument, NormalizedDocument
 from nameparser import HumanName
 import os
 
+NAME = u"stcloud"
 TODAY = date.today()
-NAME = "stcloud"
 OAI_DC_BASE_URL = 'http://repository.stcloudstate.edu/do/oai/'
 DEFAULT = datetime(1970, 01, 01)
 NAMESPACES = {'dc': 'http://purl.org/dc/elements/1.1/', 
               'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
               'ns0': 'http://www.openarchives.org/OAI/2.0/'
               }
+DEFAULT_ENCODING = 'utf-8'
+record_encoding = None
 
 
-def consume(days_back=5):
+def consume(days_back=2):
     start_date = TODAY - timedelta(days_back)
     base_url = OAI_DC_BASE_URL + '?verb=ListRecords&metadataPrefix=oai_dc&from='
     url = base_url + str(start_date) + 'T00:00:00Z'
@@ -29,20 +31,21 @@ def consume(days_back=5):
     xml_list = []
     for record in records:
         doc_id = record.xpath('ns0:header/ns0:identifier/node()', namespaces=NAMESPACES)[0]
-        record_string = etree.tostring(record)
-        record_string = '<?xml version="1.0" encoding="UTF-8"?>\n' + record_string
+        record = etree.tostring(record, encoding=(record_encoding or DEFAULT_ENCODING))
         xml_list.append(RawDocument({
-            'doc': record_string,
+            'doc': record,
             'source': NAME,
-            'docID': doc_id,
-            'filetype': 'xml'
+            'docID': copy_to_unicode(doc_id),
+            'filetype': u'xml'
         }))
+        #import pdb; pdb.set_trace()
 
     return xml_list
 
 
 def get_records(url):
     data = requests.get(url)
+    record_encoding = data.encoding
     doc = etree.XML(data.content)
     records = doc.xpath('//ns0:record', namespaces=NAMESPACES)
     token = doc.xpath('//ns0:resumptionToken/node()', namespaces=NAMESPACES)
@@ -56,6 +59,15 @@ def get_records(url):
     return records
 
 
+def copy_to_unicode(element):
+    encoding = record_encoding or DEFAULT_ENCODING
+    element = ''.join(element)
+    if isinstance(element, unicode):
+        return element
+    else:
+        return unicode(element, encoding=encoding)
+
+
 def get_contributors(record):
     all_contributors = record.xpath('//dc:creator/node()', namespaces=NAMESPACES)
     contributor_list = []
@@ -67,8 +79,8 @@ def get_contributors(record):
             'middle': name.middle,
             'family': name.last,
             'suffix': name.suffix,
-            'email': '',
-            'ORCID': '',
+            'email': u'',
+            'ORCID': u'',
         }
         contributor_list.append(contributor)
     return contributor_list
@@ -76,7 +88,7 @@ def get_contributors(record):
 
 def get_tags(record):
     tags = record.xpath('//dc:subject/node()', namespaces=NAMESPACES)
-    return [tag.lower() for tag in tags]
+    return [copy_to_unicode(tag.lower()) for tag in tags]
 
 
 def get_ids(record, doc):
@@ -88,7 +100,9 @@ def get_ids(record, doc):
             url = identifier
     if url is '':
         raise Exception('Warning: No url provided!')
-    return {'serviceID': serviceID, 'url': url, 'doi': ''}
+    return {'serviceID': serviceID,
+            'url': copy_to_unicode(url),
+            'doi': u''}
 
 
 def get_properties(record):
@@ -98,11 +112,11 @@ def get_properties(record):
     dcformat = (record.xpath('//dc:format/node()', namespaces=NAMESPACES) or [''])[0]
     props = {
         'publisherInfo': {
-            'publisher': publisher,
+            'publisher': copy_to_unicode(publisher),
         },
-        'source': source,
-        'type': dctype,
-        'format': dcformat,
+        'source': copy_to_unicode(source),
+        'type': copy_to_unicode(dctype),
+        'format': copy_to_unicode(dcformat),
     }
     return props
 
@@ -120,9 +134,12 @@ def get_date_updated(record):
 
 
 def normalize(raw_doc, timestamp):
-    doc = raw_doc.get('doc')
-
-    record = etree.XML(doc)
+    record = raw_doc.get('doc')
+    try:
+        record = etree.XML(record)
+    except etree.XMLSyntaxError:
+        print('Error in namespaces! Skipping this one...')
+        return None
 
     # load the list of approved series_names as a file - second option
     with open(os.path.join(os.path.dirname(__file__), 'approved_series.txt')) as series_names:
@@ -135,20 +152,20 @@ def normalize(raw_doc, timestamp):
     description = (record.xpath('//dc:description/node()', namespaces=NAMESPACES) or [''])[0]
 
     payload = {
-        'title': title,
+        'title': copy_to_unicode(title),
         'contributors': get_contributors(record),
         'properties': get_properties(record),
-        'description': description,
+        'description': copy_to_unicode(description),
         'tags': get_tags(record),
-        'id': get_ids(record,raw_doc),
+        'id': get_ids(record, raw_doc),
         'source': NAME,
-        'dateUpdated': get_date_updated(record),
-        'dateCreated': get_date_created(record),
-        'timestamp': str(timestamp),
+        'dateUpdated': copy_to_unicode(get_date_updated(record)),
+        'dateCreated': copy_to_unicode(get_date_created(record)),
+        'timestamp': copy_to_unicode(timestamp),
     }
 
-    #import json
-    #print(json.dumps(payload, indent=4))
+    # import json
+    # print(json.dumps(payload, indent=4))
     return NormalizedDocument(payload)
         
 
