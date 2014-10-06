fa6c2c43289eeee1c0efab45101149b49be1b5cb
Chris Seto <chriskseto@gmail.com>
2014-10-05 20:35:52 -0400

    Update dumping to osf logic

--- scrapi/processing/osf/__init__.py
+++ scrapi/processing/osf/__init__.py
@@ -7,6 +7,15 @@ class OSFProcessor(BaseProcessor):
     NAME = 'osf'
 
     def process_normalized(self, raw_doc, normalized):
+        found, _hash = collision.already_processed(raw_doc)
+
+        if found:
+            return
+
+        normalized['meta'] = {
+            'docHash': _hash
+        }
+
         if crud.is_event(normalized):
             crud.dump_metdata(normalized, {})
             return
@@ -22,16 +31,18 @@ class OSFProcessor(BaseProcessor):
         report = collision.detect_collisions(report_hash)
         resource = collision.detect_collisions(resource_hash, is_resource=True)
 
+        report_norm['meta']['uids'] = report_hash
+        resource_norm['meta']['uids'] = resource_hash
+
         if not resource:
-            resource = crud.create_resource(resource_norm, resource_hash)
+            resource = crud.create_resource(resource_norm)
         else:
             crud.dump_metadata(resource_norm, {'nid': resource})
+            crud.update_node(report, report_norm)
 
         if not report:
-            report = crud.create_report(report_norm, resource, report_hash)
+            report = crud.create_report(report_norm, resource)
         else:
             crud.dump_metadata(report_norm, {'nid': report, 'pid': resource})
-
-        crud.update_node(report, report_norm)
-        if not crud.is_claimed(resource):
-            crud.update_node(resource, resource_norm)
+            if not crud.is_claimed(resource):
+                crud.update_node(resource, resource_norm)
