61ccdf8d722487b6dafbbe8d255ca4ef22279ff2
Chris Seto <chriskseto@gmail.com>
2014-10-06 19:42:51 -0400

    Dont process None type documents

--- scrapi/tasks.py
+++ scrapi/tasks.py
@@ -99,6 +99,7 @@ def normalize(raw_doc, consumer_name):
 def process_normalized(normalized_doc, raw_doc, **kwargs):
     if not normalized_doc:
         logger.warning('Not processing document with id {}'.format(raw_doc['docID']))
+        return
 
     processing.process_normalized(raw_doc, normalized_doc, kwargs)
     # This is where the normalized doc should be dumped to disc
