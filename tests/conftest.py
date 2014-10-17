73cbdc2339bcd7a483860f240dc26ea1e15952a3
Chris Seto <chriskseto@gmail.com>
2014-10-17 12:13:31 -0400

    Add some tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,18 @@ settings.CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 
 @pytest.fixture(autouse=True)
 def consumer(monkeypatch):
+    import_mock = mock.MagicMock()
     consumer_mock = mock.MagicMock()
-    monkeypatch.setattr('scrapi.tasks.import_consumer', consumer_mock)
-    monkeypatch.setattr('scrapi.util.import_consumer', consumer_mock)
+
+    import_mock.return_value = consumer_mock
+
+    monkeypatch.setattr('scrapi.tasks.import_consumer', import_mock)
+    monkeypatch.setattr('scrapi.util.import_consumer', import_mock)
+
     return consumer_mock
+
+
+@pytest.fixture(autouse=True)
+def timestamp_patch(monkeypatch):
+    monkeypatch.setattr('scrapi.util.timestamp', lambda: 'TIME')
+    monkeypatch.setattr('scrapi.tasks.timestamp', lambda: 'TIME')
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -11,7 +11,50 @@ def dispatch(monkeypatch):
     return event_mock
 
 
-@pytest.mark.usesfixtures('consumer')
-def test_run_consumer(monkeypatch, dispatch, consumer):
+def test_run_consumer_calls(monkeypatch, dispatch):
+    mock_consume = mock.MagicMock()
+    mock_begin_norm = mock.MagicMock()
+
+    monkeypatch.setattr('scrapi.tasks.consume', mock_consume)
+    monkeypatch.setattr('scrapi.tasks.begin_normalization', mock_begin_norm)
+
     tasks.run_consumer('test')
+
     assert dispatch.called
+    assert mock_consume.si.called
+    assert mock_begin_norm.s.called
+
+    mock_begin_norm.s.assert_called_once_with('test')
+    mock_consume.si.assert_called_once_with('test', 'TIME', days_back=1)
+
+
+def test_run_consumer_daysback(monkeypatch, dispatch):
+    mock_consume = mock.MagicMock()
+    mock_begin_norm = mock.MagicMock()
+
+    monkeypatch.setattr('scrapi.tasks.consume', mock_consume)
+    monkeypatch.setattr('scrapi.tasks.begin_normalization', mock_begin_norm)
+
+    tasks.run_consumer('test', days_back=10)
+
+    assert dispatch.called
+    assert mock_consume.si.called
+    assert mock_begin_norm.s.called
+
+    mock_begin_norm.s.assert_called_once_with('test')
+    mock_consume.si.assert_called_once_with('test', 'TIME', days_back=10)
+
+
+@pytest.mark.usefixtures('consumer')
+def test_consume_runs_consume(dispatch, consumer):
+    tasks.consume('test', 'TIME')
+
+    assert consumer.consume.called
+
+
+@pytest.mark.usefixtures('consumer')
+def test_consume_days_back(dispatch, consumer):
+    tasks.consume('test', 'TIME', days_back=10)
+
+    assert consumer.consume.called
+    consumer.consume.assert_called_once_with(days_back=10)
