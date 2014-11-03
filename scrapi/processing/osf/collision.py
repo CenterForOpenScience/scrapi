107b0ad6bec70af933364f3ac5f49ec059b42ea0
Chris Seto <chriskseto@gmail.com>
2014-11-03 10:50:57 -0500

    Correct to new output format

--- scrapi/processing/osf/collision.py
+++ scrapi/processing/osf/collision.py
@@ -100,7 +100,7 @@ def _search(_filter):
 
     ret = requests.post(settings.OSF_APP_URL, **kwargs).json()
 
-    if ret['counts']['total'] > 0:
+    if ret['count'] > 0:
         return ret['results'][0]
 
     return None
