d0203d04b07bac8dd659f2038dc61f8e59c83594
erinspace <erin.braswell@gmail.com>
2014-11-14 18:12:57 -0500

    added a few more tests now

--- tests/test_push_api.py
+++ tests/test_push_api.py
@@ -1,9 +1,57 @@
 from __future__ import unicode_literals
 
+import json
 import mock
 import pytest
 
 from website import process_metadata
+
+
+API_INPUT = {
+    'source': 'Wrestling Digest',
+    'events': [
+        {
+            'title': 'Using Table Stable Carbon in Gold and STAR Isotopes',
+            'contributors': [
+                {
+                    'prefix': 'The One And Only',
+                    'given': 'DEVON',
+                    'middle': 'Get The Tables',
+                    'family': 'DUDLEY',
+                    'suffix': 'Thirsty Boy',
+                    'email': 'dudley.boyz@email.uni.edu',
+                    'ORCID': 'BubbaRayDudley'
+                }
+            ],
+            'id': {
+                'url': 'http://www.plosone.org/article',
+                'doi': '10.1371/doi.DOI!',
+                'serviceID': 'AWESOME'
+            },
+            'properties': {
+                'figures': ['http://www.plosone.org/article/image.png'],
+                'type': 'text',
+                'yep':'A property'
+            },
+            'description': 'This study seeks to understand how humans impact\
+            the dietary patterns of eight free-ranging vervet monkey\
+            (Chlorocebus pygerythrus) groups in South Africa using stable\
+            isotope analysis.',
+            'tags': [
+                'behavior',
+                'genetics'
+            ],
+            'source': 'example_pusher',
+            'dateCreated': '2012-11-30T17:05:48+00:00',
+            'dateUpdated': '2015-02-23T17:05:48+00:01',
+        }
+    ]
+}
+
+RAW_DOC = {
+    'doc': json.dumps(API_INPUT['events'][0]),
+    'docID': 'someID'
+}
 
 
 @pytest.fixture
@@ -15,25 +63,23 @@ def dispatch(monkeypatch):
 
 def test_consume_returns_list():
 
-    events = [{'source': 'dudleyz', 'id': {'serviceID': 'getthetables'}}]
-    result = process_metadata.consume(events)
+    result = process_metadata.consume(API_INPUT['events'])
     assert isinstance(result, list)
 
 
 def test_task_consume_returns_tuple():
 
-    events = [{'source': 'cmpunk', 'id': {'serviceID': 'bestintheworld'}}]
-    result = process_metadata.task_consume(events)
-
+    result = process_metadata.task_consume(API_INPUT['events'])
     assert isinstance(result, tuple)
 
 
-def test_task_consume_calls(monkeypatch, dispatch):
+def test_task_consume_calls(dispatch):
 
-    mock_consume = mock.MagicMock()
-    monkeypatch.setattr('website.process_metadata.consume', mock_consume)
-
-    events = [{'source': 'dudleyz', 'id': {'serviceID': 'getthetables'}}]
-    process_metadata.task_consume(events)
+    process_metadata.task_consume(API_INPUT['events'])
     assert dispatch.called
 
+
+def test_normalize_calls(dispatch):
+
+    process_metadata.normalize(RAW_DOC)
+    assert dispatch.called
