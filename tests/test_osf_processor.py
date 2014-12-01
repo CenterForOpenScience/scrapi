e17d8a19e15877276d7ebf3252713c78d3809fdc
erinspace <erin.braswell@gmail.com>
2014-12-01 17:17:04 -0500

    One more test to make sure the right stuff is added

--- tests/test_osf_processor.py
+++ tests/test_osf_processor.py
@@ -105,8 +105,7 @@ def test_contrib_deleted_if_resource(mock_detect_collisions, mock_already_proces
     normed = deepcopy(NORMALIZED)
     resource = deepcopy(RECORD)
 
-    httpretty.register_uri(
-        httpretty.POST, re.compile('.*'), body=json.dumps(RECORD))
+    httpretty.register_uri(httpretty.POST, re.compile('.*'), body=json.dumps(RECORD))
     httpretty.register_uri(httpretty.PUT, re.compile('.*'))
 
     mock_already_processed.return_value = False,  md5().hexdigest()
@@ -119,3 +118,29 @@ def test_contrib_deleted_if_resource(mock_detect_collisions, mock_already_proces
     osf_processor.process_normalized(RAW_DOC, normed)
 
     assert 'contributors' not in resource.keys()
+
+
+@httpretty.activate
+@mock.patch('scrapi.processing.osf.collision.already_processed')
+@mock.patch('scrapi.processing.osf.collision.detect_collisions')
+def test_report_id_added_to_cmids(mock_detect_collisions, mock_already_processed):
+
+    normed = deepcopy(NORMALIZED)
+    resource = deepcopy(RECORD)
+
+    httpretty.register_uri(httpretty.POST, re.compile('.*'), body=json.dumps(RECORD))
+    httpretty.register_uri(httpretty.PUT, re.compile('.*'))
+
+    mock_already_processed.return_value = False,  md5().hexdigest()
+
+    mock_detect_collisions.return_value = resource
+
+    resource['meta'] = {}
+    resource['attached'] = {}
+    resource['attached']['cmids'] = ['some stuff']
+
+    osf_processor = osf.OSFProcessor()
+    osf_processor.process_normalized(RAW_DOC, normed)
+
+    assert len(resource['attached']['cmids']) > 1
+    assert normed['_id'] in resource['attached']['cmids']
