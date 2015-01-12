41f4a438c37424cbbabc5b53abcbfd7b7bf1de24
erinspace <erin.braswell@gmail.com>
2015-01-12 15:35:14 -0500

    add all harvester components

---
+++ scrapi/consumers/valposcholar/__init__.py
@@ -0,0 +1,12 @@
+from scrapi.base import OAIHarvester
+
+
+valposcholar = OAIHarvester(
+    name='valposcholar',
+    base_url='http://scholar.valpo.edu/do/oai/',
+    property_list=['type', 'source'],
+    approved_sets=True
+)
+
+consume = valposcholar.consume
+normalize = valposcholar.normalize
