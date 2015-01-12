4ff948e6f0c3279a44b42581972d45161d08c38f
erinspace <erin.braswell@gmail.com>
2015-01-12 14:22:04 -0500

    change dateCollected to match consumerFinished

--- scrapi/tasks.py
+++ scrapi/tasks.py
@@ -159,7 +159,7 @@ def normalize(raw_doc, consumer_name):
     normalized['timestamps'] = raw_doc['timestamps']
     normalized['timestamps']['normalizeFinished'] = timestamp()
 
-    normalized['dateCollected'] = normalized['timestamps']['normalizeFinished']
+    normalized['dateCollected'] = normalized['timestamps']['consumeFinished']
 
     normalized['raw'] = '{url}/{archive}{source}/{doc_id}/{consumeFinished}/raw.{raw_format}'.format(
         url=settings.SCRAPI_URL,
