a712c7d3480f23cee3f1feabfb5d30611eba24bd
erinspace <erin.braswell@gmail.com>
2015-01-23 14:08:30 -0500

    update to harvester classes

--- scrapi/consumers/arxiv_oai/__init__.py
+++ scrapi/consumers/arxiv_oai/__init__.py
@@ -1,1 +1,15 @@
-from consumer import consume, normalize
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+arxiv_oai = OAIHarvester(
+    name='arxiv_oai',
+    timeout=30,
+    base_url='http://academiccommons.arxiv_oai.edu/catalog/oai',
+    property_list=['type', 'publisher', 'format', 'date',
+                   'identifier', 'language', 'setSpec']
+)
+
+consume = arxiv_oai.harvest
+normalize = arxiv_oai.normalize
