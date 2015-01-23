ddca4b0a90603f0b80c860eedf780eaba69deea9
erinspace <erin.braswell@gmail.com>
2015-01-23 11:47:49 -0500

    update to harvester based class

--- scrapi/consumers/columbia/__init__.py
+++ scrapi/consumers/columbia/__init__.py
@@ -1,1 +1,14 @@
-from consumer import consume, normalize
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+columbia = OAIHarvester(
+    name='columbia',
+    base_url='http://academiccommons.columbia.edu/catalog/oai',
+    property_list=['type', 'publisher', 'format', 'date',
+                   'identifier', 'language']
+)
+
+consume = columbia.harvest
+normalize = columbia.normalize
