6d10fd646b36d27d545a2e7132670bafc5ff9313
erinspace <erin.braswell@gmail.com>
2015-01-23 15:28:26 -0500

    move to class based harvester

--- scrapi/consumers/ucescholership/__init__.py
+++ scrapi/consumers/ucescholership/__init__.py
@@ -1,1 +1,15 @@
-from consumer import consume, normalize
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+escholarship = OAIHarvester(
+    name='escholarship',
+    base_url='http://www.escholarship.org/uc/oai',
+    property_list=['type', 'publisher', 'format', 'date',
+                   'identifier', 'language', 'setSpec', 'source', 'coverage',
+                   'relation', 'rights']
+)
+
+consume = escholarship.harvest
+normalize = escholarship.normalize
