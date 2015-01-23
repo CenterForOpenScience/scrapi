147e30187c863c632064b185c067b8bca5126ce4
erinspace <erin.braswell@gmail.com>
2015-01-22 19:21:50 -0500

    move to class based harvester

--- scrapi/consumers/stcloud/__init__.py
+++ scrapi/consumers/stcloud/__init__.py
@@ -1,1 +1,33 @@
-from consumer import consume, normalize
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+stcloud = OAIHarvester(
+    name='stcloud',
+    base_url='http://repository.stcloudstate.edu/do/oai/',
+    property_list=['type', 'source', 'publisher', 'format', 'setSpec'],
+    approved_sets=[
+        'ews_facpubs',
+        'ews_wps',
+        'hist_facwp',
+        'comm_facpubs',
+        'anth_facpubs',
+        'soc_facpubs',
+        'soc_ug_research',
+        'chem_facpubs',
+        'phys_present',
+        'lrs_facpubs',
+        'cfs_facpubs',
+        'hurl_facpubs',
+        'ed_facpubs',
+        'cpcf_gradresearch',
+        'econ_facpubs',
+        'econ_wps',
+        'econ_seminars',
+        'stcloud_ling'
+    ]
+)
+
+consume = stcloud.harvest
+normalize = stcloud.normalize
