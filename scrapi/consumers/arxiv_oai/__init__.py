b0a4315fd17cc47c7e822dd6121ff2416763804e
erinspace <erin.braswell@gmail.com>
2015-01-23 17:05:02 -0500

    update url

--- scrapi/consumers/arxiv_oai/__init__.py
+++ scrapi/consumers/arxiv_oai/__init__.py
@@ -6,7 +6,7 @@ from scrapi.base import OAIHarvester
 arxiv_oai = OAIHarvester(
     name='arxiv_oai',
     timeout=30,
-    base_url='http://academiccommons.arxiv_oai.edu/catalog/oai',
+    base_url='http://export.arxiv.org/oai2',
     property_list=['type', 'publisher', 'format', 'date',
                    'identifier', 'language', 'setSpec']
 )
