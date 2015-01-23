8b6b5ead367d007e187ba6f36b0f58cfdf48f21a
erinspace <erin.braswell@gmail.com>
2015-01-22 19:00:07 -0500

    update to class based harvester

--- scrapi/consumers/spdataverse/__init__.py
+++ scrapi/consumers/spdataverse/__init__.py
@@ -1,1 +1,14 @@
-from consumer import consume, normalize
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+spdataverse = OAIHarvester(
+    name='spdataverse',
+    base_url='http://dataverse.scholarsportal.info/dvn/OAIHandler',
+    property_list=['type', 'source', 'publisher', 'format', 'relation',
+                   'description', 'coverage', 'rights', 'setSpec']
+)
+
+consume = spdataverse.harvest
+normalize = spdataverse.normalize
