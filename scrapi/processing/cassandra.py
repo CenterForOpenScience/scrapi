c02ab2c3738ba96bb3a03bf7f5ac09f928139ef4
Fabian von Feilitzsch <fabian@fabianism.us>
2015-02-03 16:33:14 -0500

    Normalized documents now added to the cassandra database

--- scrapi/processing/cassandra.py
+++ scrapi/processing/cassandra.py
@@ -1,30 +1,58 @@
+import json
+
 from cqlengine import columns, Model, connection
 from cqlengine.management import sync_table, create_keyspace
 
 from scrapi.processing.base import BaseProcessor
 
 connection.setup(['127.0.0.1'], 'cqlengine')
+create_keyspace("cqlengine", replication_factor=1, strategy_class="SimpleStrategy")
 
 class CassandraProcessor(BaseProcessor):
     NAME = 'cassandra'
 
     def __init__(self):
-        create_keyspace("cqlengine", replication_factor=1, strategy_class="SimpleStrategy")
         sync_table(RawModel)
-
+        sync_table(NormalizedModel)
 
     def process_normalized(self, raw_doc, normalized):
-        raise NotImplementedError
+        nm = NormalizedModel.create(
+            url=normalized.get('id')['url'],
+            contributors=json.dumps(normalized.get('contributors')),
+            id=normalized.get('id'),
+            title=normalized.get('title'),
+            source=normalized.get('source'),
+            tags=normalized.get('tags'),
+            dateUpdated=normalized.get('dateUpdated'),
+            properties=json.dumps(normalized.get('properties'))
+        )
 
     def process_raw(self, raw_doc):
         rm = RawModel.create(**raw_doc.attributes)
 
 
 class RawModel(Model):
-    __table_name__ = 'raw_doc'
+    __table_name__ = 'raw'
     __keyspace__ = 'cqlengine'
-    docID = columns.Text(required=True, primary_key=True)
-    doc = columns.Bytes(required=True)
-    source = columns.Text(required=True)
-    filetype = columns.Text(required=True)
+
+    docID = columns.Text(primary_key=True)
+    doc = columns.Bytes()
+    source = columns.Text(index=True)
+    filetype = columns.Text()
     timestamps = columns.Map(columns.Text, columns.Text)
+
+
+class NormalizedModel(Model):
+    __table_name__ = 'normalized'
+    __keyspace__ = 'cqlengine'
+
+    url = columns.Text(primary_key=True)
+
+    contributors = columns.Text()
+    id = columns.Map(columns.Text, columns.Text)
+    title = columns.Text(index=True)
+    source = columns.Text(index=True)
+    description = columns.Text()
+    tags = columns.List(columns.Text())
+    dateUpdated = columns.Text()
+    properties=columns.Text()
