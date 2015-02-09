b38e6fb29fe2f21f43332307855d40ed67b0f9d0
Fabian von Feilitzsch <fabian@fabianism.us>
2015-02-09 17:55:50 -0500

    Just allow celery task to raise errors

--- migration_tasks.py
+++ migration_tasks.py
@@ -38,28 +38,24 @@ cass = CassandraProcessor()
 
 @app.task
 def process_one_to_cassandra(consumer_name, consumer, raw_path):
-    try:
-        date = parser.parse(raw_path.split('/')[-2])
+    date = parser.parse(raw_path.split('/')[-2])
 
-        timestamp = date.isoformat()
+    timestamp = date.isoformat()
 
-        raw_file = store.get_as_string(raw_path)
+    raw_file = store.get_as_string(raw_path)
 
-        raw_doc = RawDocument({
-            'doc': raw_file,
-            'timestamps': {
-                'consumeFinished': timestamp
-            },
-            'docID': b64decode(raw_path.split('/')[-3]).decode('utf-8'),
-            'source': consumer_name,
-            'filetype': consumer['fileFormat'],
-        })
+    raw_doc = RawDocument({
+        'doc': raw_file,
+        'timestamps': {
+            'consumeFinished': timestamp
+        },
+        'docID': b64decode(raw_path.split('/')[-3]).decode('utf-8'),
+        'source': consumer_name,
+        'filetype': consumer['fileFormat'],
+    })
 
-        cass.process_raw(raw_doc)
+    cass.process_raw(raw_doc)
 
-        normalized = normalize(raw_doc, consumer_name)
+    normalized = normalize(raw_doc, consumer_name)
 
-        cass.process_normalized(raw_doc, normalized)
-    except Exception as e:
-        logger.exception(e)
-        return e
+    cass.process_normalized(raw_doc, normalized)
