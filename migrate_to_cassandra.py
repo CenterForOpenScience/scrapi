8d7080a9acd335a359edd48e59452af97291ee96
Fabian von Feilitzsch <fabian@fabianism.us>
2015-02-09 15:54:29 -0500

    Cassandra migration now makes use of celery

--- migrate_to_cassandra.py
+++ migrate_to_cassandra.py
@@ -1,48 +1,23 @@
 import logging
 
-from dateutil import parser
-
-from base64 import b64decode
+from cqlengine.connection import LOG
 
 from scrapi import settings
-from scrapi.linter import RawDocument
-from scrapi.tasks import normalize
 from scrapi.util.storage import store
-from scrapi.processing.cassandra import CassandraProcessor
+
+from migration_tasks import process_one_to_cassandra
+
 
 logger = logging.getLogger(__name__)
+LOG.setLevel(logging.WARN)
 
-cass = CassandraProcessor()
 
-exceptions = []
-for consumer_name in settings.MANIFESTS.keys():
-    consumer = settings.MANIFESTS[consumer_name]
-    for raw_path in store.iter_raws(consumer_name, include_normalized=True):
-        try:
-            date = parser.parse(raw_path.split('/')[-2])
+def main():
+    for consumer_name in settings.MANIFESTS.keys():
+        consumer = settings.MANIFESTS[consumer_name]
+        for raw_path in store.iter_raws(consumer_name, include_normalized=True):
+            process_one_to_cassandra.delay(consumer_name, consumer, raw_path)
 
-            timestamp = date.isoformat()
 
-            raw_file = store.get_as_string(raw_path)
-
-            raw_doc = RawDocument({
-                'doc': raw_file,
-                'timestamps': {
-                    'consumeFinished': timestamp
-                },
-                'docID': b64decode(raw_path.split('/')[-3]).decode('utf-8'),
-                'source': consumer_name,
-                'filetype': consumer['fileFormat'],
-            })
-
-            cass.process_raw(raw_doc)
-
-            normalized = normalize(raw_doc, consumer_name)
-
-            cass.process_normalized(raw_doc, normalized)
-        except Exception as e:
-            logger.exception(e)
-            exceptions.append(e)
-
-for exception in exceptions:
-    logger.exception(exception)
+if __name__ == '__main__':
+    main()
---
+++ migration_tasks.py
@@ -0,0 +1,66 @@
+import logging
+
+from dateutil import parser
+
+from base64 import b64decode
+
+from celery import Celery
+from celery.signals import worker_process_init
+
+from cqlengine import connection
+from cqlengine.connection import cluster, session
+
+from scrapi import settings
+from scrapi.linter import RawDocument
+from scrapi.tasks import normalize
+
+from scrapi.util.storage import store
+
+from scrapi.processing.cassandra import CassandraProcessor
+
+logger = logging.getLogger(__name__)
+
+
+def cassandra_init(*args, **kwargs):
+    if cluster is not None:
+        cluster.shutdown()
+    if session is not None:
+        session.shutdown()
+    connection.setup(settings.CASSANDRA_URI, settings.CASSANDRA_KEYSPACE)
+
+worker_process_init.connect(cassandra_init)
+
+app = Celery()
+app.config_from_object(settings)
+
+cass = CassandraProcessor()
+
+
+@app.task
+def process_one_to_cassandra(consumer_name, consumer, raw_path):
+    exceptions = []
+    try:
+        date = parser.parse(raw_path.split('/')[-2])
+
+        timestamp = date.isoformat()
+
+        raw_file = store.get_as_string(raw_path)
+
+        raw_doc = RawDocument({
+            'doc': raw_file,
+            'timestamps': {
+                'consumeFinished': timestamp
+            },
+            'docID': b64decode(raw_path.split('/')[-3]).decode('utf-8'),
+            'source': consumer_name,
+            'filetype': consumer['fileFormat'],
+        })
+
+        cass.process_raw(raw_doc)
+
+        normalized = normalize(raw_doc, consumer_name)
+
+        cass.process_normalized(raw_doc, normalized)
+    except Exception as e:
+        exceptions.append(e)
+        raise e
