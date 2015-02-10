01d985e1e0f5e2646dbac5bb7796cc50a9384c5a
Fabian von Feilitzsch <fabian@fabianism.us>
2015-02-10 14:37:15 -0500

    add travis configuration for elasticsearch

---
+++ scrapi/settings/travis-dist.py
@@ -0,0 +1,33 @@
+DEBUG = False
+
+BROKER_URL = 'amqp://guest@localhost'
+
+STORAGE_METHOD = 'disk'
+ARCHIVE_DIRECTORY = 'archive/'
+RECORD_DIRECTORY = 'records'
+
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+STORE_HTTP_TRANSACTIONS = False
+
+NORMALIZED_PROCESSING = ['storage', 'elasticsearch']
+RAW_PROCESSING = ['storage', 'elasticsearch']
+
+SENTRY_DNS = None
+
+USE_FLUENTD = False
+FLUENTD_ARGS = ('scrapi',)
+
+# OUTPUT SETTINGS
+OSF_ENABLED = False
+
+VERIFY_SSL = True
+OSF_PREFIX = 'http://localhost:5000'
+
+APP_ID = 'some id'
+
+API_KEY_LABEL = 'some label'
+API_KEY = 'some api key'
+
+ELASTIC_URI = 'localhost:9200'
+ELASTIC_TIMEOUT = 10
