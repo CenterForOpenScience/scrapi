6485310842dd9273f7d30ae3df1270bf8bea3cb0
Fabian von Feilitzsch <fabian@fabianism.us>
2015-02-10 14:29:56 -0500

    add elasticsearch processor for scrapi

---
+++ scrapi/processing/elastic_search.py
@@ -0,0 +1,33 @@
+import logging
+
+from elasticsearch import Elasticsearch
+
+from scrapi import settings
+from scrapi.processing.base import BaseProcessor
+
+es = Elasticsearch(
+    settings.ELASTIC_URI,
+    request_timeout=settings.ELASTIC_TIMEOUT
+)
+
+logging.getLogger('elasticsearch').setLevel(logging.WARN)
+logging.getLogger('elasticsearch.trace').setLevel(logging.WARN)
+logging.getLogger('urllib3').setLevel(logging.WARN)
+logging.getLogger('requests').setLevel(logging.WARN)
+es.cluster.health(wait_for_status='yellow')
+
+
+class ElasticsearchProcessor(BaseProcessor):
+    NAME = 'elasticsearch'
+
+    def process_raw(*args, **kwargs):
+        pass
+
+    def process_normalized(self, raw_doc, normalized):
+        es.index(
+            body=normalized.attributes,
+            refresh=True,
+            index='share',
+            doc_type=normalized['source'],
+            id=normalized['id']['serviceID'],
+        )
