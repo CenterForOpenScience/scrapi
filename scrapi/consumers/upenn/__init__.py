2faf568f7a0912f7ef54e43b57dda6cbd64fd5cc
erinspace <erin.braswell@gmail.com>
2015-02-11 10:11:56 -0500

    class based and preparing for scrapi merge

--- scrapi/consumers/upenn/__init__.py
+++ scrapi/consumers/upenn/__init__.py
@@ -1,1 +1,13 @@
-from consumer import consume, normalize
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+upenn = OAIHarvester(
+    name='upenn',
+    timeout=30,
+    base_url='http://repository.upenn.edu/do/oai/'
+)
+
+consume = upenn.harvest
+normalize = upenn.normalize
