0bb967ae5da2f4b0d1c1d146df1875036b19c1ae
Chris Seto <chriskseto@gmail.com>
2015-03-04 16:47:36 -0500

    Refactor all the logging

--- scrapi/tasks.py
+++ scrapi/tasks.py
@@ -1,24 +1,14 @@
-import os
 import logging
-from dateutil import parser
-from base64 import b64decode
-from datetime import datetime
-
-import vcr
 
 import requests
-
 from celery import Celery
 
-from base64 import b64encode
-
+from scrapi import util
 from scrapi import events
 from scrapi import settings
 from scrapi import processing
 from scrapi.util import timestamp
-from scrapi.util.storage import store
 from scrapi.util import import_consumer
-from scrapi.linter.document import RawDocument
 
 
 app = Celery()
@@ -28,77 +18,32 @@ logger = logging.getLogger(__name__)
 
 
 @app.task
+@events.creates_task(events.CONSUMER_RUN)
 def run_consumer(consumer_name, days_back=1):
     logger.info('Running consumer "{}"'.format(consumer_name))
 
+    normalization = begin_normalization.s(consumer_name)
+    start_consumption = consume.si(consumer_name, timestamp(), days_back=days_back)
+
     # Form and start a celery chain
-    chain = (consume.si(consumer_name, timestamp(), days_back=days_back)
-             | begin_normalization.s(consumer_name))
-
-    chain.apply_async()
-
-    # Note: Dispatch events only after they run
-    events.dispatch(
-        events.CONSUMER_RUN,
-        events.CREATED,
-        days_back=days_back,
-        consumer=consumer_name,
-    )
+    (start_consumption | normalization).apply_async()
 
 
 @app.task
+@events.logged(events.CONSUMER_RUN)
 def consume(consumer_name, job_created, days_back=1):
-    logger.info('Consumer "{}" has begun consumption'.format(consumer_name))
-    events.dispatch(
-        events.CONSUMER_RUN,
-        events.STARTED,
-        days_back=days_back,
-        consumer=consumer_name,
-        job_created=job_created
-    )
-
-    timestamps = {
-        'consumeTaskCreated': job_created,
-        'consumeStarted': timestamp()
-    }
-
+    consume_started = timestamp()
     consumer = import_consumer(consumer_name)
 
-    try:
-        if settings.STORE_HTTP_TRANSACTIONS:
-            cassette = os.path.join(settings.RECORD_DIRECTORY,
-                                    consumer_name, timestamp() + '.yml')
-
-            logger.debug('Recording HTTP consumption request for {} to {}'
-                         .format(consumer_name, cassette))
-
-            with vcr.use_cassette(cassette, record_mode='all'):
-                result = consumer.consume(days_back=days_back)
-        else:
-            result = consumer.consume(days_back=days_back)
-    except Exception as e:
-        events.dispatch(
-            events.CONSUMER_RUN,
-            events.FAILED,
-            exception=repr(e),
-            days_back=days_back,
-            consumer=consumer_name,
-            job_created=job_created
-        )
-        raise
-
-    timestamps['consumeFinished'] = timestamp()
-
-    logger.info('Consumer "{}" has finished consumption'.format(consumer_name))
-    events.dispatch(
-        events.CONSUMER_RUN,
-        events.COMPLETED,
-        consumer=consumer_name,
-        number=len(result)
-    )
+    with util.maybe_recorded(consumer_name):
+        result = consumer.consume(days_back=days_back)
 
     # result is a list of all of the RawDocuments consumed
-    return result, timestamps
+    return result, {
+        'consumeFinished': timestamp(),
+        'consumeTaskCreated': job_created,
+        'consumeStarted': consume_started,
+    }
 
 
 @app.task
@@ -110,189 +55,52 @@ def begin_normalization((raw_docs, timestamps), consumer_name):
 
     logger.info('Normalizing {} documents for consumer "{}"'
                 .format(len(raw_docs), consumer_name))
-
     # raw is a single raw document
     for raw in raw_docs:
+        spawn_tasks(raw, timestamps, consumer_name)
+
+
+@events.creates_task(events.PROCESSING)
+@events.creates_task(events.NORMALIZATION)
+def spawn_tasks(raw, timestamps, consumer_name):
         raw['timestamps'] = timestamps
         raw['timestamps']['normalizeTaskCreated'] = timestamp()
+        chain = (normalize.si(raw, consumer_name) | process_normalized.s(raw))
 
-        logger.debug('Created the process raw task for {}/{}'
-                     .format(consumer_name, raw['docID']))
-
+        chain.apply_async()
         process_raw.delay(raw)
 
-        chain = (normalize.si(raw, consumer_name) |
-                 process_normalized.s(raw))
-
-        logger.debug('Created the process normalized task for {}/{}'
-                     .format(consumer_name, raw['docID']))
-
-        chain.apply_async()
-
-        # Note: Dispatch events only AFTER the event has actually happened
-
-        events.dispatch(events.NORMALIZATION, events.CREATED,
-                        consumer=consumer_name, **raw.attributes)
-
-        events.dispatch(events.PROCESSING, events.CREATED,
-                        consumer=consumer_name, **raw.attributes)
-
 
 @app.task
+@events.logged(events.PROCESSING, 'raw')
 def process_raw(raw_doc, **kwargs):
-    events.dispatch(events.PROCESSING, events.STARTED,
-                    _index='raw', **raw_doc.attributes)
     processing.process_raw(raw_doc, kwargs)
 
-    events.dispatch(events.PROCESSING, events.COMPLETED,
-                    _index='raw', **raw_doc.attributes)
-
 
 @app.task
+@events.logged(events.NORMALIZATION)
 def normalize(raw_doc, consumer_name):
+    normalized_started = timestamp()
     consumer = import_consumer(consumer_name)
 
-    raw_doc['timestamps']['normalizeStarted'] = timestamp()
-
-    logger.debug('Document {}/{} normalization began'.format(
-        consumer_name, raw_doc['docID']))
-
-    events.dispatch(events.NORMALIZATION, events.STARTED,
-                    consumer=consumer_name, **raw_doc.attributes)
-    try:
+    with events.logged_failure(events.NORMALIZATION):
         normalized = consumer.normalize(raw_doc)
-    except Exception as e:
-        events.dispatch(
-            events.NORMALIZATION,
-            events.FAILED,
-            consumer=consumer_name,
-            exception=repr(e),
-            **raw_doc.attributes
-        )
-        raise
 
     if not normalized:
-        events.dispatch(events.NORMALIZATION, events.SKIPPED,
-                        consumer=consumer_name, **raw_doc.attributes)
-        logger.warning
-        (
-            'Did not normalize document [{}]{}'.format
-            (
-                consumer_name,
-                raw_doc['docID']
-            )
-        )
-        return None
+        raise events.Skip('Did not normalize document with id {}'.format(raw_doc['docID']))
 
-    logger.debug('Document {}/{} normalized sucessfully'.format(
-        consumer_name, raw_doc['docID']))
+    normalized['timestamps'] = util.stamp_from_raw(raw_doc, normalizeStarted=normalized_started)
+    normalized['raw'] = util.build_raw_url(raw_doc, normalized)
 
-    events.dispatch(events.NORMALIZATION, events.COMPLETED,
-                    consumer=consumer_name, **raw_doc.attributes)
-
-    normalized['timestamps'] = raw_doc['timestamps']
-    normalized['timestamps']['normalizeFinished'] = timestamp()
-
-    normalized['dateCollected'] = normalized['timestamps']['consumeFinished']
-
-    normalized['raw'] = '{url}/{archive}{source}/{doc_id}/{consumeFinished}/raw.{raw_format}'.format(
-        url=settings.SCRAPI_URL,
-        archive=settings.ARCHIVE_DIRECTORY,
-        source=normalized['source'],
-        doc_id=b64encode(raw_doc['docID']),
-        consumeFinished=normalized['timestamps']['consumeFinished'],
-        raw_format=raw_doc['filetype']
-    )
-
-    # returns a single normalized document
-    return normalized
+    return normalized  # returns a single normalized document
 
 
 @app.task
+@events.logged(events.PROCESSING, 'normalized')
 def process_normalized(normalized_doc, raw_doc, **kwargs):
     if not normalized_doc:
-        events.dispatch(
-            events.PROCESSING,
-            events.SKIPPED,
-            **raw_doc.attributes
-        )
-        logger.warning('Not processing document with id {}'.format(raw_doc['docID']))
-        return
-
-    events.dispatch(
-        events.PROCESSING,
-        events.STARTED,
-        _index='normalized',
-        **raw_doc.attributes
-    )
-
+        raise events.Skip('Not processing document with id {}'.format(raw_doc['docID']))
     processing.process_normalized(raw_doc, normalized_doc, kwargs)
-
-    events.dispatch(
-        events.PROCESSING,
-        events.COMPLETED,
-        _index='normalized',
-        **raw_doc.attributes
-    )
-
-
-@app.task
-def check_archives(reprocess, days_back=None):
-    for consumer in settings.MANIFESTS.keys():
-        check_archive.delay(consumer, reprocess, days_back=days_back)
-
-        events.dispatch(events.CHECK_ARCHIVE, events.CREATED,
-                        consumer=consumer, reprocess=reprocess)
-
-
-@app.task
-def check_archive(consumer_name, reprocess, days_back=None):
-    events.dispatch(events.CHECK_ARCHIVE, events.STARTED, **{
-        'consumer': consumer_name,
-        'reprocess': reprocess,
-        'daysBack': str(days_back) if days_back else 'All'
-    })
-
-    consumer = settings.MANIFESTS[consumer_name]
-    extras = {
-        'overwrite': True
-    }
-    for raw_path in store.iter_raws(consumer_name, include_normalized=reprocess):
-        date = parser.parse(raw_path.split('/')[-2])
-
-        if (days_back and (datetime.now() - date).days > days_back):
-            continue
-
-        timestamp = date.isoformat()
-
-        raw_file = store.get_as_string(raw_path)
-
-        raw_doc = RawDocument({
-            'doc': raw_file,
-            'timestamps': {
-                'consumeFinished': timestamp
-            },
-            'docID': b64decode(raw_path.split('/')[-3]).decode('utf-8'),
-            'source': consumer_name,
-            'filetype': consumer['fileFormat'],
-        })
-
-        chain = (normalize.si(raw_doc, consumer_name) |
-                 process_normalized.s(raw_doc, storage=extras))
-        chain.apply_async()
-
-        events.dispatch(events.NORMALIZATION, events.CREATED, **{
-            'consumer': consumer_name,
-            'reprocess': reprocess,
-            'daysBack': str(days_back) if days_back else 'All',
-            'docID': raw_doc['docID']
-        })
-
-    events.dispatch(events.CHECK_ARCHIVE, events.COMPLETED, **{
-        'consumer': consumer_name,
-        'reprocess': reprocess,
-        'daysBack': str(days_back) if days_back else 'All'
-    })
 
 
 @app.task
@@ -300,9 +108,3 @@ def update_pubsubhubbub():
     payload = {'hub.mode': 'publish', 'hub.url': '{url}rss/'.format(url=settings.OSF_APP_URL)}
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     return requests.post('https://pubsubhubbub.appspot.com', headers=headers, params=payload)
-
-
-# TODO Fix me @fabianvf @chrisseto
-@app.task
-def tar_archive():
-    os.system('tar -czvf website/static/archive.tar.gz archive/')
