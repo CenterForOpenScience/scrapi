04f4c11ff52069475a3818de74b4cd89695cfa2c
erinspace <erin.braswell@gmail.com>
2015-03-05 10:03:56 -0500

    Update tdar harvester with custom url gatheriing

--- scrapi/harvesters/tdar/__init__.py
+++ scrapi/harvesters/tdar/__init__.py
@@ -9,10 +9,33 @@ Example API call: http://core.tdar.org/oai-pmh/oai?verb=ListRecords&metadataPref
 
 from __future__ import unicode_literals
 
+from scrapi import util
 from scrapi.base import OAIHarvester
 
 
-tdar = OAIHarvester(
+class tdarHarvester(OAIHarvester):
+
+    def get_ids(self, result, doc):
+        """
+        Gather the DOI and url from identifiers, if possible.
+        Tries to save the DOI alone without a url extension.
+        Tries to save a link to the original content at the source,
+        instead of direct to a PDF, which is usually linked with viewcontent.cgi?
+        in the url field
+        """
+        serviceID = doc.get('docID')
+
+        url = 'http://core.tdar.org/document/' + serviceID.replace('oai:tdar.org:Resource:', '')
+        print(url)
+
+        return {
+            'serviceID': serviceID,
+            'url': util.copy_to_unicode(url),
+            'doi': ''
+        }
+
+
+tdar = tdarHarvester(
     name='tdar',
     base_url='http://core.tdar.org/oai-pmh/oai',
     property_list=['type', 'date', 'setSpec', 'type', 'publisher', 'coverage']
