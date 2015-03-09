fa0d858f5d3adc107af2ebd37a59caf6e4b1b56b
Chris Seto <chriskseto@gmail.com>
2015-03-09 14:38:50 -0400

    Add and fix up tests

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,4 +1,5 @@
 import mock
+import json
 import pytest
 
 from scrapi import requests
@@ -16,87 +17,128 @@ def mock_record_transactions(monkeypatch):
     monkeypatch.setattr(requests.settings, 'RECORD_HTTP_TRANSACTIONS', True)
 
 
-def test_record_or_load_response_respects_record_false(mock_requests, monkeypatch):
-    mock_rec_or_load = mock.Mock()
-    monkeypatch.setattr(requests.settings, 'RECORD_HTTP_TRANSACTIONS', False)
-    monkeypatch.setattr(requests, 'record_or_load_response', mock_rec_or_load)
+class TestSettings(object):
+    def test_record_or_load_response_respects_record_false(self, mock_requests, monkeypatch):
+        mock_rec_or_load = mock.Mock()
+        monkeypatch.setattr(requests.settings, 'RECORD_HTTP_TRANSACTIONS', False)
+        monkeypatch.setattr(requests, 'record_or_load_response', mock_rec_or_load)
 
-    requests.get('foo')
+        requests.get('foo')
 
-    assert not mock_rec_or_load.called
-    assert mock_requests.request.called_once_with('get', 'foo')
+        assert not mock_rec_or_load.called
+        assert mock_requests.request.called_once_with('get', 'foo')
 
+    def test_record_or_load_response_respects_record_true(self, mock_requests, monkeypatch):
+        mock_rec_or_load = mock.Mock()
+        monkeypatch.setattr(requests, 'record_or_load_response', mock_rec_or_load)
 
-def test_record_or_load_response_respects_record_true(mock_requests, monkeypatch):
-    mock_rec_or_load = mock.Mock()
-    monkeypatch.setattr(requests, 'record_or_load_response', mock_rec_or_load)
+        requests.get('foo')
 
-    requests.get('foo')
-
-    assert mock_rec_or_load.called_once_with('get', 'foo')
-    assert mock_requests.request.called_once_with('get', 'foo')
-
-
-@pytest.mark.cassandra
-def test_record_or_load_loads(mock_requests, monkeypatch):
-    requests.HarvesterResponse(method='get', url='dinosaurs.sexy', content='rawr').save()
-
-    resp = requests.get('dinosaurs.sexy')
-
-    assert resp.content == 'rawr'
-    assert not mock_requests.request.called
-    assert isinstance(resp, requests.HarvesterResponse)
+        assert mock_rec_or_load.called_once_with('get', 'foo')
+        assert mock_requests.request.called_once_with('get', 'foo')
 
 
 @pytest.mark.cassandra
-def test_record_or_load_records(mock_requests, monkeypatch):
-    mock_requests.request.return_value = mock.Mock(encoding='utf-8', content='rawr', status_code=200, headers={'tota': 'dyle'})
+class TestModel(object):
+    def test_json_works(self):
+        data = {'totally': 'dyle'}
 
-    resp = requests.get('dinosaurs.sexy')
+        resp = requests.HarvesterResponse(content=json.dumps(data))
 
-    model = requests.HarvesterResponse.get(method='get', url='dinosaurs.sexy')
-
-    assert model.method == 'get'
-    assert model.content == 'rawr'
-    assert model.encoding == 'utf-8'
-    assert model.status_code == 200
-    assert model.url == 'dinosaurs.sexy'
-    assert model.headers == {'tota': 'dyle'}
-    assert model.headers_str == '{"tota": "dyle"}'
-    assert isinstance(resp, requests.HarvesterResponse)
+        assert resp.json == data
+        assert resp.content == json.dumps(data)
 
 
-def test_get_calls_get(monkeypatch):
-    mock_request = mock.Mock()
-    monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
+@pytest.mark.cassandra
+class TestCassandraIntegration(object):
+    def test_record_or_load_loads(self, mock_requests, monkeypatch):
+        requests.HarvesterResponse(ok=True, method='get', url='dinosaurs.sexy', content='rawr', headers_str="{}").save()
 
-    requests.get('test', tota='dyle')
+        resp = requests.get('dinosaurs.sexy')
 
-    assert mock_request.called_once_with('get', 'test', tota='dyle')
+        assert resp.headers == {}
+        assert resp.content == 'rawr'
+        assert not mock_requests.request.called
+        assert isinstance(resp, requests.HarvesterResponse)
+
+    def test_record_or_load_remakes(self, mock_requests, monkeypatch):
+        mock_requests.request.return_value = mock.Mock(encoding='utf-8', content='rawr', status_code=200, headers={'tota': 'dyle'})
+        requests.HarvesterResponse(ok=False, method='get', url='dinosaurs.sexy').save()
+
+        model = requests.HarvesterResponse.get(method='get', url='dinosaurs.sexy')
+
+        assert not model.ok
+        assert model.method == 'get'
+        assert model.url == 'dinosaurs.sexy'
+
+        resp = requests.get('dinosaurs.sexy')
+
+        model = requests.HarvesterResponse.get(method='get', url='dinosaurs.sexy')
+
+        assert model.method == 'get'
+        assert model.content == 'rawr'
+        assert model.encoding == 'utf-8'
+        assert model.status_code == 200
+        assert model.url == 'dinosaurs.sexy'
+        assert model.headers == {'tota': 'dyle'}
+        assert model.headers_str == '{"tota": "dyle"}'
+        assert isinstance(resp, requests.HarvesterResponse)
+
+    def test_record_or_load_records(self, mock_requests, monkeypatch):
+        mock_requests.request.return_value = mock.Mock(encoding='utf-8', content='rawr', status_code=200, headers={'tota': 'dyle'})
+
+        resp = requests.get('dinosaurs.sexy')
+
+        model = requests.HarvesterResponse.get(method='get', url='dinosaurs.sexy')
+
+        assert model.method == 'get'
+        assert model.content == 'rawr'
+        assert model.encoding == 'utf-8'
+        assert model.status_code == 200
+        assert model.url == 'dinosaurs.sexy'
+        assert model.headers == {'tota': 'dyle'}
+        assert model.headers_str == '{"tota": "dyle"}'
+        assert isinstance(resp, requests.HarvesterResponse)
+
+    def test_record_or_load_logs_not_ok(self, mock_requests, monkeypatch):
+        mock_log = mock.Mock()
+        monkeypatch.setattr(requests.events, 'log_to_sentry', mock_log)
+        mock_requests.request.return_value = mock.Mock(ok=False, encoding='utf-8', content='rawr', status_code=200, headers={'tota': 'dyle'})
+
+        requests.get('dinosaurs.sexy')
+
+        mock_log.assert_called_once_with('Got non-okay response code.', url='dinosaurs.sexy', method='get')
 
 
-def test_put_calls_put(monkeypatch):
-    mock_request = mock.Mock()
-    monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
+class TestRequestsApi(object):
+    def test_get_calls_get(self, monkeypatch):
+        mock_request = mock.Mock()
+        monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
 
-    requests.put('test', tota='dyle')
+        requests.get('test', tota='dyle')
 
-    assert mock_request.called_once_with('put', 'test', tota='dyle')
+        mock_request.assert_called_once_with('get', 'test', tota='dyle')
 
+    def test_put_calls_put(self, monkeypatch):
+        mock_request = mock.Mock()
+        monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
 
-def test_post_calls_post(monkeypatch):
-    mock_request = mock.Mock()
-    monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
+        requests.put('test', tota='dyle')
 
-    requests.post('test', tota='dyle')
+        mock_request.assert_called_once_with('put', 'test', tota='dyle')
 
-    assert mock_request.called_once_with('post', 'test', tota='dyle')
+    def test_post_calls_post(self, monkeypatch):
+        mock_request = mock.Mock()
+        monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
 
+        requests.post('test', tota='dyle')
 
-def test_delete_calls_delete(monkeypatch):
-    mock_request = mock.Mock()
-    monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
+        mock_request.assert_called_once_with('post', 'test', tota='dyle')
 
-    requests.delete('test', tota='dyle')
+    def test_delete_calls_delete(self, monkeypatch):
+        mock_request = mock.Mock()
+        monkeypatch.setattr(requests, 'record_or_load_response', mock_request)
 
-    assert mock_request.called_once_with('delete', 'test', tota='dyle')
+        requests.delete('test', tota='dyle')
+
+        mock_request.assert_called_once_with('delete', 'test', tota='dyle')
