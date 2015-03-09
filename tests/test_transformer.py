c55b11354b6e1a5511f703c32440aafcedd36d86
Fabian von Feilitzsch <fabian@fabianism.us>
2015-03-09 14:19:43 -0400

    add tests for transformer

---
+++ tests/test_transformer.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals
+
+import functools
+
+from scrapi.base import XMLHarvester
+from scrapi.linter import RawDocument
+
+from .utils import get_leaves
+from .utils import TEST_SCHEMA, TEST_NAMESPACES, TEST_XML_DOC
+
+
+class TestHarvester(XMLHarvester):
+
+    def harvest(self, days_back=1):
+        return [RawDocument({
+            'doc': str(TEST_XML_DOC),
+            'source': 'TEST',
+            'filetype': 'XML',
+            'docID': "1"
+        })] * days_back
+
+
+class TestTransformer(object):
+
+    def setup_method(self, method):
+        self.harvester = TestHarvester("TEST", TEST_SCHEMA, TEST_NAMESPACES)
+
+    def test_process_schema(self):
+        assert self.harvester.schema == TEST_SCHEMA
+        self.harvester.process_schema()
+        assert self.harvester.schema != TEST_SCHEMA
+
+        for (k, v) in get_leaves(self.harvester.schema):
+            assert type(v) == functools.partial
+
+    def test_normalize(self):
+        results = [
+            self.harvester.normalize(record) for record in self.harvester.harvest(days_back=10)
+        ]
+
+        for result in results:
+            assert result['properties']['title1'] == 'Test'
+            assert result['properties']['title2'] == 'test'
+            assert result['properties']['title3'] == 'Testtest'
+
+            for (k, v) in get_leaves(result.attributes):
+                assert type(v) != functools.partial
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from scrapi.base.schemas import BASEXMLSCHEMA, update_schema
+
 RAW_DOC = {
     'doc': str('{}'),
     'docID': 'someID',
@@ -49,3 +51,99 @@ RECORD = {
     '_id': 'yes! yes! yes!',
     'count': 0
 }
+
+
+TEST_SCHEMA = update_schema(BASEXMLSCHEMA, {
+    "properties": {
+        "title1": "//dc:title/node()",
+        "title2": ["//dc:title/node()", lambda x: x.lower()],
+        "title3": ["//dc:title/node()", "//dc:title/node()", lambda x, y: x + y.lower()]
+    }
+})
+
+
+def get_leaves(d, leaves=None):
+    if leaves is None:
+        leaves = []
+
+    for k, v in d.items():
+        if isinstance(v, dict):
+            leaves.extend(get_leaves(v, leaves))
+        else:
+            leaves.append((k, v))
+
+    return leaves
+
+
+TEST_NAMESPACES = {
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'dc': 'http://purl.org/dc/elements/1.1/',
+    'dcq': 'http://purl.org/dc/terms/'
+}
+
+
+TEST_XML_DOC = '''
+    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcq="http://purl.org/dc/terms/">
+        <records count="97" morepages="true" start="1" end="10">
+            <record rownumber="1">
+                <dc:title>Test</dc:title>
+                <dc:creator>
+                Raveh-Sadka, Tali; Thomas, Brian C; Singh, Andrea; Firek, Brian; Brooks,
+                Brandon; Castelle, Cindy J; Sharon, Itai; Baker, Robyn; Good, Misty; Morowitz,
+                Michael J; Banfield, Jillian F
+                </dc:creator>
+                <dc:subject/>
+                <dc:subjectRelated/>
+                <dc:description/>
+                <dcq:publisher>
+                    eLife Sciences Publications, Ltd.
+                </dcq:publisher>
+                <dcq:publisherAvailability/>
+                <dcq:publisherResearch>
+                    None
+                </dcq:publisherResearch>
+                <dcq:publisherSponsor>
+                    USDOE
+                </dcq:publisherSponsor>
+                <dcq:publisherCountry>
+                    Country unknown/Code not available
+                </dcq:publisherCountry>
+                <dc:date>
+                    2015-03-03
+                </dc:date>
+                <dc:language>
+                    English
+                </dc:language>
+                <dc:type>
+                    Journal Article
+                </dc:type>
+                <dcq:typeQualifier/>
+                <dc:relation>
+                    Journal Name: eLife; Journal Volume: 4
+                </dc:relation>
+                <dc:coverage/>
+                <dc:format>
+                    Medium: X
+                </dc:format>
+                <dc:identifier>
+                    OSTI ID: 1171761, Legacy ID: OSTI ID: 1171761
+                </dc:identifier>
+                <dc:identifierReport>
+                    None
+                </dc:identifierReport>
+                <dcq:identifierDOEcontract>
+                    5R01AI092531; Long term fellowship; SC0004918; ER65561; APSF-2012-10-05
+                </dcq:identifierDOEcontract>
+                <dc:identifierOther>Journal ID: ISSN 2050-084X</dc:identifierOther>
+                <dc:doi>10.7554/eLife.05477</dc:doi><dc:rights/>
+                <dc:dateEntry>2015-03-05</dc:dateEntry>
+                <dc:dateAdded>2015-03-05</dc:dateAdded>
+                <dc:ostiId>1171761</dc:ostiId>
+                <dcq:identifier-purl type=""/>
+                <dcq:identifier-citation>
+                    http://www.osti.gov/pages/biblio/1171761
+                </dcq:identifier-citation>
+            </record>
+        </records>
+    </rdf:RDF>
+'''
