f6464108e70d34a9ce68015b6ca331f8befcad7c
Chris Seto <chriskseto@gmail.com>
2015-03-11 13:20:35 -0400

    Port arxiv

---
+++ scrapi/harvesters/arxiv.py
@@ -0,0 +1,21 @@
+"""
+Harvests arXiv metadata for ingestion into the SHARE service
+
+Example API call: http://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=oai_dc&from=2014-10-10
+"""
+
+
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+class ArxivHarvester(OAIHarvester):
+    short_name = 'arxiv_oai'
+    long_name = 'arXiv'
+    timeout = 30
+    base_url = 'http://export.arxiv.org/oai2'
+    property_list = [
+        'type', 'publisher', 'format', 'date',
+        'identifier', 'language', 'setSpec', 'description'
+    ]
