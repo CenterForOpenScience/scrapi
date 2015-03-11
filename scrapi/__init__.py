700c44bb42ec51b0a34ea5248fd6454de3c72f7e
Chris Seto <chriskseto@gmail.com>
2015-03-11 11:32:57 -0400

    define registry

--- scrapi/__init__.py
+++ scrapi/__init__.py
@@ -1,1 +1,32 @@
+from celery.schedules import crontab
+
 from scrapi import linter, requests  # noqa
+
+
+class _Registry(dict):
+
+    def __init__(self):
+        super(_Registry, self).__init__()
+
+    def collect_harvesters(self):
+        pass
+
+    def __getitem__(self, key):
+        try:
+            return super(_Registry, self).__getitem__(key)
+        except KeyError:
+            raise KeyError('No harvester named "{}"'.format(key))
+
+    @property
+    def beat_schedule(self):
+        return {
+            'run_{}'.format(name): {
+                'args': [name],
+                'schedule': crontab(**inst.run_at),
+                'task': 'scrapi.tasks.run_harvester',
+            }
+            for name, inst
+            in self.items()
+        }
+
+registry = _Registry()
