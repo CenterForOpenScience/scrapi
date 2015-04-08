0cc0d4a5ddf938f176c2384503ef88bb31c91898
Fabian von Feilitzsch <fabian@fabianism.us>
2015-04-08 13:40:28 -0400

    Transform new schema to old schema, to keep share_v1 up to date

--- scrapi/processing/elasticsearch.py
+++ scrapi/processing/elasticsearch.py
@@ -7,6 +7,7 @@ from elasticsearch.exceptions import ConnectionError
 
 from scrapi import settings
 from scrapi.processing.base import BaseProcessor
+from scrapi.base.transformer import JSONTransformer
 
 
 logger = logging.getLogger(__name__)
@@ -28,6 +29,7 @@ try:
     # }
     # es.cluster.health(wait_for_status='yellow')
     es.indices.create(index=settings.ELASTIC_INDEX, body={}, ignore=400)
+    es.indices.create(index='share_v1', ignore=400)
 except ConnectionError:  # pragma: no cover
     logger.error('Could not connect to Elasticsearch, expect errors.')
     if 'elasticsearch' in settings.NORMALIZED_PROCESSING or 'elasticsearch' in settings.RAW_PROCESSING:
@@ -51,6 +53,7 @@ class ElasticsearchProcessor(BaseProcessor):
             doc_type=raw_doc['source'],
             id=raw_doc['docID'],
         )
+        self.process_normalized_v1(raw_doc, normalized)
 
     def version(self, raw, normalized):
         try:
@@ -68,3 +71,41 @@ class ElasticsearchProcessor(BaseProcessor):
             date = old_doc.get('releaseDate') or normalized['releaseDate']
 
         return date
+
+    def process_normalized_v1(self, raw_doc, normalized):
+        index = 'share_v1'
+        transformer = PreserveOldSchema()
+        data = transformer.transform(normalized.attributes)
+        es.index(
+            body=data,
+            refresh=True,
+            index=index,
+            doc_type=raw_doc['source'],
+            id=raw_doc['docID']
+        )
+
+
+class PreserveOldContributors(JSONTransformer):
+    schema = {
+        'given': '/givenName',
+        'family': '/familyName',
+        'middle': '/additionalName',
+        'email': '/email'
+    }
+
+    def process_contributors(self, contributors):
+        return [self.transform(contributor) for contributor in contributors]
+
+
+class PreserveOldSchema(JSONTransformer):
+    schema = {
+        'title': '/title',
+        'description': '/description',
+        'tags': '/otherProperties/tags',
+        'contributors': ('/contributor', PreserveOldContributors().process_contributors),
+        'dateUpdated': '/releaseDate',
+        'source': '/source',
+        'id': {
+            'url': '/directLink'
+        }
+    }
