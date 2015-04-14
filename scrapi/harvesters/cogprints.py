dba152089bbc2bf85447e756c375f59a3ed7145d
Grant R. Vousden-Dishington <GrantRVD@gmail.com>
2015-04-14 09:31:26 -0700

    Harvester for CogPrints
    Adds OAIHarvester for the Cognitive Sciences ePrint Archive

---
+++ scrapi/harvesters/cogprints.py
@@ -0,0 +1,19 @@
+"""
+Harvester for the Cognitive Sciences ePrint Archive for the SHARE project
+
+"""
+
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+class CogPrintsHarvester(OAIHarvester):
+    short_name = 'cogprints'
+    long_name = 'Cognitive Sciences ePrint Archive'
+    url = 'http://www.http://cogprints.org/'
+
+    base_url = 'http://cogprints.org/cgi/oai2'
+    property_list = [
+        'date', 'resource type', 'format', 'resource identifier', 'relation'
+    ]
