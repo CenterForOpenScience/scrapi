e5a0810a9733c8d220de25390aa3b8ddc685a8fc
erinspace <erin.braswell@gmail.com>
2015-04-20 16:03:11 -0400

    Update URL extractor to pull from PMC id in record header

--- scrapi/harvesters/pubmedcentral.py
+++ scrapi/harvesters/pubmedcentral.py
@@ -1,11 +1,7 @@
 """
-<<<<<<< HEAD:scrapi/harvesters/pubmedcentral.py
 Harvester of PubMed Central for the SHARE notification service
 
 Example API call: http://www.pubmedcentral.nih.gov/oai/oai.cgi?verb=ListRecords&metadataPrefix=oai_dc&from=2015-04-13&until=2015-04-14
-=======
-Harvester of pubmed central for the SHARE notification service
->>>>>>> 10981892c236658dd3d4d9cd6caceedf300088bb:scrapi/harvesters/pubmedcentral.py
 """
 
 
@@ -16,15 +12,9 @@ from scrapi.base import helpers
 from scrapi.base import OAIHarvester
 
 
-def oai_extract_url_pubmedcentral(identifiers):
-    identifiers = [identifiers] if not isinstance(identifiers, list) else identifiers
-    for item in identifiers:
-        try:
-            found_url = helpers.URL_REGEX.search(item).group()
-            if 'viewcontent' not in found_url and '/pubmed/' in found_url:
-                return found_url.decode('utf-8')
-        except AttributeError:
-            continue
+def oai_extract_url_pubmedcentral(header):
+    PMC_ID = header.replace('oai:pubmedcentral.nih.gov:', '')
+    return 'http://www.ncbi.nlm.nih.gov/pmc/articles/PMC' + PMC_ID
 
 
 class PubMedCentralHarvester(OAIHarvester):
@@ -36,7 +26,7 @@ class PubMedCentralHarvester(OAIHarvester):
         schemas.OAISCHEMA,
         {
             "uris": {
-                "canonicalUri": ('//dc:identifier/node()', oai_extract_url_pubmedcentral)
+                "canonicalUri": ('//ns0:header/ns0:identifier/node()', oai_extract_url_pubmedcentral)
             }
         }
     )
