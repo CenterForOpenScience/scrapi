1ee15d2895045bd85653033ca85387d2f9cd11c3
Fabian von Feilitzsch <fabian@fabianism.us>
2015-04-21 15:33:08 -0400

    Broke post-processing functions into some smaller compositions for default schemas

--- scrapi/base/schemas.py
+++ scrapi/base/schemas.py
@@ -16,12 +16,12 @@ CONSTANT = lambda x: lambda *_, **__: x
 
 
 BASEXMLSCHEMA = {
-    "description": ('//dc:description/node()', compose(lambda x: unicode(x.strip()), single_result)),
-    "contributors": ('//dc:creator/node()', compose(lambda x: default_name_parser(x.split(';')), single_result)),
-    "title": ('//dc:title/node()', compose(lambda x: unicode(x.strip()), single_result)),
-    "providerUpdatedDateTime": ('//dc:dateEntry/node()', compose(lambda x: unicode(x.strip()), single_result)),
+    "description": ('//dc:description/node()', compose(lambda x: x.strip(), single_result)),
+    "contributors": ('//dc:creator/node()', compose(default_name_parser, lambda x: x.split(';'), single_result)),
+    "title": ('//dc:title/node()', compose(lambda x: x.strip(), single_result)),
+    "providerUpdatedDateTime": ('//dc:dateEntry/node()', compose(lambda x: x.strip(), single_result)),
     "uris": {
-        "canonicalUri": ('//dcq:identifier-citation/node()', compose(lambda x: unicode(x.strip()), single_result)),
+        "canonicalUri": ('//dcq:identifier-citation/node()', compose(lambda x: x.strip(), single_result)),
     }
 }
 
@@ -30,8 +30,8 @@ OAISCHEMA = {
     "uris": {
         "canonicalUri": ('//dc:identifier/node()', oai_extract_url)
     },
-    'providerUpdatedDateTime': ('//ns0:header/ns0:datestamp/node()', lambda x: unicode(parse(x[0]).replace(tzinfo=None).isoformat())),
-    'title': ('//dc:title/node()', lambda x: x[0] if isinstance(x, list) else x),
-    'description': ('//dc:description/node()', lambda x: x[0] if isinstance(x, list) else x),
+    'providerUpdatedDateTime': ('//ns0:header/ns0:datestamp/node()', lambda x: parse(x[0]).replace(tzinfo=None).isoformat()),
+    'title': ('//dc:title/node()', single_result),
+    'description': ('//dc:description/node()', single_result),
     'tags': ('//dc:subject/node()', format_tags)
 }
