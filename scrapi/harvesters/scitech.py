e27a84385e612ab6037e443a004693ef1f6d8b3e
Fabian von Feilitzsch <fabian@fabianism.us>
2015-04-23 11:42:11 -0400

    Scitech otherProperties

--- scrapi/harvesters/scitech.py
+++ scrapi/harvesters/scitech.py
@@ -17,8 +17,8 @@ from scrapi import requests
 
 from scrapi.base import XMLHarvester
 from scrapi.linter import RawDocument
-# from scrapi.base.helpers import updated_schema
 from scrapi.base.schemas import BASEXMLSCHEMA
+from scrapi.base.helpers import updated_schema, build_properties
 
 NAME = 'scitech'
 
@@ -41,32 +41,28 @@ class SciTechHarvester(XMLHarvester):
         'dcq': 'http://purl.org/dc/terms/'
     }
 
-    schema = BASEXMLSCHEMA
-    # schema = updated_schema(BASEXMLSCHEMA, {
-    #     'otherProperties': {
-    #         'language': '//dc:language/node()',
-    #         'type': '//dc:type/node()',
-    #         'typeQualifier': '//dc:typeQualifier/node()',
-    #         'language': '//dc:language/node()',
-    #         'format': '//dc:format/node()',
-    #         'identifierOther': '//dc:identifierOther/node()',
-    #         'rights': '//dc:rights/node()',
-    #         'identifierDOEcontract': '//dcq:identifierDOEcontract/node()',
-    #         'relation': '//dc:relation/node()',
-    #         'coverage': '//dc:coverage/node()',
-    #         'identifier-purl': '//dc:identifier-purl/node()',
-    #         'identifier': '//dc:identifier/node()',
-    #         'identifierReport': '//dc:identifierReport/node()',
-    #         'publisherInfo': {
-    #             'publisher': '//dcq:publisher/node()',
-    #             'publisherCountry': '//dcq:publisherCountry/node()',
-    #             'publisherSponsor': '//dcq:publisherSponsor/node()',
-    #             'publisherAvailability': '//dcq:publisherAvailability/node()',
-    #             'publisherResearch': '//dcq:publisherResearch/node()',
-    #             'date': '//dc:date/node()'
-    #         }
-    #     }
-    # })
+    schema = updated_schema(BASEXMLSCHEMA, {
+        'otherProperties': build_properties(
+            ('language', '//dc:language/node()'),
+            ('type', '//dc:type/node()'),
+            ('typeQualifier', '//dc:typeQualifier/node()'),
+            ('format', '//dc:format/node()'),
+            ('identifierOther', '//dc:identifierOther/node()'),
+            ('rights', '//dc:rights/node()'),
+            ('identifierDOEcontract', '//dcq:identifierDOEcontract/node()'),
+            ('relation', '//dc:relation/node()'),
+            ('coverage', '//dc:coverage/node()'),
+            ('identifier-purl', '//dc:identifier-purl/node()'),
+            ('identifier', '//dc:identifier/node()'),
+            ('identifierReport', '//dc:identifierReport/node()'),
+            ('publisher', '//dcq:publisher/node()'),
+            ('publisherCountry', '//dcq:publisherCountry/node()'),
+            ('publisherSponsor', '//dcq:publisherSponsor/node()'),
+            ('publisherAvailability', '//dcq:publisherAvailability/node()'),
+            ('publisherResearch', '//dcq:publisherResearch/node()'),
+            ('date', '//dc:date/node()')
+        )
+    })
 
     def harvest(self, days_back=1):
         """A function for querying the SciTech Connect database for raw XML.
