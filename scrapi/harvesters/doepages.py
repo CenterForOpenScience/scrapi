9e12f3ed2265155e3d9c3745c4f087ae9f78dd87
Fabian von Feilitzsch <fabian@fabianism.us>
2015-04-23 11:03:45 -0400

    Doepages, new otherProperties schema

--- scrapi/harvesters/doepages.py
+++ scrapi/harvesters/doepages.py
@@ -7,8 +7,8 @@ from lxml import etree
 from scrapi import requests
 from scrapi.base import XMLHarvester
 from scrapi.linter import RawDocument
-# from scrapi.base.helpers import updated_schema
 from scrapi.base.schemas import BASEXMLSCHEMA
+from scrapi.base.helpers import updated_schema, build_properties
 
 
 class DoepagesHarvester(XMLHarvester):
@@ -63,32 +63,29 @@ class DoepagesHarvester(XMLHarvester):
 
     @property
     def schema(self):
-        return BASEXMLSCHEMA
-        # return updated_schema(
-        #     BASEXMLSCHEMA,
-        #     {
-        #         'properties': {
-        #             'language': '//dc:language/node()',
-        #             'type': '//dc:type/node()',
-        #             'typeQualifier': '//dc:typeQualifier/node()',
-        #             'language': '//dc:language/node()',
-        #             'format': '//dc:format/node()',
-        #             'identifierOther': '//dc:identifierOther/node()',
-        #             'rights': '//dc:rights/node()',
-        #             'identifierDOEcontract': '//dcq:identifierDOEcontract/node()',
-        #             'relation': '//dc:relation/node()',
-        #             'coverage': '//dc:coverage/node()',
-        #             'identifier-purl': '//dc:identifier-purl/node()',
-        #             'identifier': '//dc:identifier/node()',
-        #             'identifierReport': '//dc:identifierReport/node()',
-        #             'publisherInfo': {
-        #                 'publisher': '//dcq:publisher/node()',
-        #                 'publisherCountry': '//dcq:publisherCountry/node()',
-        #                 'publisherSponsor': '//dcq:publisherSponsor/node()',
-        #                 'publisherAvailability': '//dcq:publisherAvailability/node()',
-        #                 'publisherResearch': '//dcq:publisherResearch/node()',
-        #                 'date': '//dc:date/node()'
-        #             }
-        #         }
-        #     }
-        # )
+        return updated_schema(
+            BASEXMLSCHEMA,
+            {
+                'otherProperties': build_properties(
+                    ('language', '//dc:language/node()'),
+                    ('type', '//dc:type/node()'),
+                    ('typeQualifier', '//dc:typeQualifier/node()'),
+                    ('language', '//dc:language/node()'),
+                    ('format', '//dc:format/node()'),
+                    ('identifierOther', '//dc:identifierOther/node()'),
+                    ('rights', '//dc:rights/node()'),
+                    ('identifierDOEcontract', '//dcq:identifierDOEcontract/node()'),
+                    ('relation', '//dc:relation/node()'),
+                    ('coverage', '//dc:coverage/node()'),
+                    ('identifier-purl', '//dc:identifier-purl/node()'),
+                    ('identifier', '//dc:identifier/node()'),
+                    ('identifierReport', '//dc:identifierReport/node()'),
+                    ('publisher', '//dcq:publisher/node()'),
+                    ('publisherCountry', '//dcq:publisherCountry/node()'),
+                    ('publisherSponsor', '//dcq:publisherSponsor/node()'),
+                    ('publisherAvailability', '//dcq:publisherAvailability/node()'),
+                    ('publisherResearch', '//dcq:publisherResearch/node()'),
+                    ('date', '//dc:date/node()')
+                )
+            }
+        )
