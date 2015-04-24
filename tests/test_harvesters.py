383e1b5b79b8e321f71d85e30b5bf7fd8a6aeb9d
Fabian von Feilitzsch <fabian@fabianism.us>
2015-04-24 15:36:43 -0400

    Add tests for harvesters

--- tests/test_harvesters.py
+++ tests/test_harvesters.py
@@ -1,19 +1,23 @@
 import logging
 
+import vcr
 import pytest
+from mock import patch
 from freezegun import freeze_time
 
-from scrapi import registry
+from scrapi import registry, requests
 
 logger = logging.getLogger(__name__)
 
-@freeze_time("2015-04-23")
-@pytest.mark.parametrize('harvester_name', map(str, registry.keys()))
-def test_harvester(harvester_name):
-    harvester = registry[harvester_name]
-    try:
-        [harvester.normalize(doc) for doc in harvester.harvest()]
-        assert True
-    except Exception as e:
-        logger.exception(e)
-        assert False
+@freeze_time("2007-12-21")
+@pytest.mark.parametrize('harvester_name', sorted(map(str, registry.keys())))
+def test_harvester(monkeypatch, harvester_name, *args, **kwargs):
+    monkeypatch.setattr(requests.time, 'sleep', lambda *_, **__: None)
+    with vcr.use_cassette('tests/vcr/{}.yaml'.format(harvester_name), match_on=['host'], record_mode='once'):
+        harvester = registry[harvester_name]
+        try:
+            normalized = [harvester.normalize(doc) for doc in harvester.harvest()]
+        except Exception as e:
+            logger.exception(e)
+            assert False
+    assert len(normalized) > 0
