4d01d4a2a925508e27f9b11313a1784b141a70c3
erinspace <erin.braswell@gmail.com>
2015-05-08 09:19:24 -0400

    Fix figshare flake

--- scrapi/harvesters/figshare.py
+++ scrapi/harvesters/figshare.py
@@ -11,7 +11,7 @@ from __future__ import unicode_literals
 import json
 import logging
 from dateutil.parser import parse
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta
 
 
 from scrapi import requests
