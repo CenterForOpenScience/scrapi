4bd16a83c2cc6202edd0e2a9c3fa49df46519d59
erinspace <erin.braswell@gmail.com>
2015-05-21 15:58:12 -0400

    Add Iowa Research Online harvester

---
+++ scrapi/harvesters/iowaresearch.py
@@ -0,0 +1,18 @@
+'''
+Harvester for the Iowa Research Online for the SHARE project
+
+Example API call: http://ir.uiowa.edu/do/oai/?verb=ListRecords&metadataPrefix=oai_dc
+'''
+
+from __future__ import unicode_literals
+from scrapi.base import OAIHarvester
+
+
+class IowaresearchHarvester(OAIHarvester):
+    short_name = 'iowaresearch'
+    long_name = 'Iowa Research Online'
+    url = 'http://ir.uiowa.edu/do/oai/'
+
+    base_url = 'http://ir.uiowa.edu/do/oai/'
+    property_list = ['date', 'source', 'identifier', 'type']
+    timezone_granularity = True
