da16bec07e245e440acea629ad953e4a56085f7e
Fabian von Feilitzsch <fabian@fabianism.us>
2015-06-02 11:17:11 -0400

    Add ability to iterate over old document model and new document model for migrations

--- scripts/util.py
+++ scripts/util.py
@@ -4,25 +4,34 @@ import logging
 from cassandra.cqlengine.query import Token
 
 from scrapi.database import _manager
-from scrapi.processing.cassandra import DocumentModel
+from scrapi.processing.cassandra import DocumentModel, DocumentModelV2
 
 _manager.setup()
 logger = logging.getLogger(__name__)
 
 
-def documents(*sources):
-    q = DocumentModel.objects.timeout(500).allow_filtering().all().limit(1000)
-    querysets = (q.filter(source=source) for source in sources) if sources else [q]
-    for query in querysets:
-        page = try_forever(list, query)
-        while len(page) > 0:
-            for doc in page:
-                yield doc
-            page = try_forever(next_page, query, page)
+def ModelIteratorFactory(model, next_page):
+    def model_iterator(*sources):
+        q = model.objects.timeout(500).allow_filtering().all().limit(1000)
+        querysets = (q.filter(source=source) for source in sources) if sources else [q]
+        for query in querysets:
+            page = try_forever(list, query)
+            while len(page) > 0:
+                for doc in page:
+                    yield doc
+                page = try_forever(next_page, query, page)
+    return model_iterator
 
 
-def next_page(query, page):
+def next_page_v1(query, page):
     return list(query.filter(pk__token__gt=Token(page[-1].pk)))
+
+
+def next_page_v2(query, page):
+    return list(query.filter(docID__gt=page[-1].docID))
+
+documents_v1 = ModelIteratorFactory(DocumentModel, next_page_v1)
+documents_v2 = ModelIteratorFactory(DocumentModelV2, next_page_v2)
 
 
 def try_forever(action, *args, **kwargs):
