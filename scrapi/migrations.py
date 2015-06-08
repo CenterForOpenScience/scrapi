92822edeeca8f584d5ce0ab6efda59861497efc8
erinspace <erin.braswell@gmail.com>
2015-06-08 09:38:15 -0400

    Update migrations to contain the rename task

---
+++ scrapi/migrations.py
@@ -0,0 +1,36 @@
+import logging
+# import functools
+
+from scrapi import tasks
+from scrapi import settings
+from scrapi.linter import RawDocument
+from scrapi.processing.elasticsearch import es
+from celery import Celery
+
+app = Celery()
+app.config_from_object(settings)
+
+logger = logging.getLogger()
+
+
+@tasks.task_autoretry(default_retry_delay=30, max_retries=5)
+def rename(doc, source=None, target=None, dry=True):
+    assert source and target, "To run this migration you need both a source and a target"
+    assert source != target, "Can't rename {} to {}, names are the same".format(source, target)
+
+    raw = RawDocument({
+        'doc': doc.doc,
+        'docID': doc.docID,
+        'source': target,
+        'filetype': doc.filetype,
+        'timestamps': doc.timestamps,
+        'versions': doc.versions
+    })
+    if not dry:
+        tasks.process_raw(raw)
+        tasks.process_normalized(tasks.normalize(raw, raw['source']), raw)
+        logger.info('Processed document from {} with id {}'.format(source, raw['docID']))
+
+        es.delete(index=settings.ELASTIC_INDEX, doc_type=source, id=raw['docID'], ignore=[404])
+        es.delete(index='share_v1', doc_type=source, id=raw['docID'], ignore=[404])
+    logger.info('Deleted document from {} with id {}'.format(source, raw['docID']))
