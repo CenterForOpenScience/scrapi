f14c36c315cd8352caf495fe894d1f6ecacbeb17
erinspace <erin.braswell@gmail.com>
2015-06-08 11:29:31 -0400

    add dryad harvester and test

---
+++ scrapi/harvesters/dryad.py
@@ -0,0 +1,18 @@
+'''
+Harvester for Dryad for the SHARE project
+
+Example API call: http://www.datadryad.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc
+'''
+
+from __future__ import unicode_literals
+from scrapi.base import OAIHarvester
+
+
+class DryadHarvester(OAIHarvester):
+    short_name = 'dryad'
+    long_name = 'Dryad Data Repository'
+    url = 'http://www.datadryad.org/oai/request'
+
+    base_url = 'http://www.datadryad.org/oai/request'
+    property_list = ['rights', 'format', 'relation', 'date', 'identifier', 'type']
+    timezone_granularity = True
