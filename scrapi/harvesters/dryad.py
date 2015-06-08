1d6c17e5adc3df4de86636ef77fc0a406bf065e9
erinspace <erin.braswell@gmail.com>
2015-06-08 17:25:37 -0400

    Add custom normaluze function that will not get non existant items

--- scrapi/harvesters/dryad.py
+++ scrapi/harvesters/dryad.py
@@ -3,9 +3,14 @@ Harvester for Dryad for the SHARE project
 
 Example API call: http://www.datadryad.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc
 '''
-
 from __future__ import unicode_literals
+
+from lxml import etree
+import logging
+
 from scrapi.base import OAIHarvester
+
+logger = logging.getLogger(__name__)
 
 
 class DryadHarvester(OAIHarvester):
@@ -17,3 +22,14 @@ class DryadHarvester(OAIHarvester):
     property_list = ['rights', 'format', 'relation', 'date',
                      'identifier', 'type', 'setSpec']
     timezone_granularity = True
+
+    def normalize(self, raw_doc):
+        str_result = raw_doc.get('doc')
+        result = etree.XML(str_result)
+
+        status = (result.xpath('ns0:header/@status', namespaces=self.namespaces) or [''])[0]
+        if status.lower() == 'deleted' or status.lower() == 'item is not available':
+            logger.info('Not normalizing record with ID {}, status {}'.format(raw_doc['docID'], status))
+            return None
+
+        return super(OAIHarvester, self).normalize(raw_doc)
