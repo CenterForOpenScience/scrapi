28e96556c13e118aa38e1d88c2b411611ae71001
erinspace <erin.braswell@gmail.com>
2015-06-09 17:17:29 -0400

    Update comments and formatting

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -3,7 +3,7 @@ import pytest
 import mock
 
 import scrapi
-from scrapi.linter.document import NormalizedDocument, RawDocument
+from scrapi.linter.document import NormalizedDocument
 
 from scrapi import tasks
 
@@ -17,12 +17,12 @@ from scrapi.processing.cassandra import CassandraProcessor, DocumentModel
 from . import utils
 
 test_cass = CassandraProcessor()
-# test_elastic = ElasticsearchProcessor()
 
 harvester = utils.TestHarvester()
 
 NORMALIZED = NormalizedDocument(utils.RECORD)
 RAW = harvester.harvest()[0]
+
 
 @pytest.fixture
 def harvester():
@@ -36,9 +36,6 @@ def test_rename():
     test_cass.process_raw(RAW)
     test_cass.process_normalized(RAW, NORMALIZED)
 
-    # test_elastic.process_raw(RAW)
-    # test_elastic.process_normalized(RAW, NORMALIZED, index='test')
-
     queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
     old_source = NORMALIZED['shareProperties']['source']
 
@@ -48,7 +45,6 @@ def test_rename():
     new_record = copy.deepcopy(utils.RECORD)
 
     new_record['shareProperties']['source'] = 'wwe_news'
-    # test_info = copy.deepcopy(registry['test'])
     test_info = registry['test'].__class__()
 
     test_info.short_name = 'wwe_news'
