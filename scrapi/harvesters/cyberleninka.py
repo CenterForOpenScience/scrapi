db26b1e30c2803c665196a12dfa35c920ea35252
erinspace <erin.braswell@gmail.com>
2015-06-29 14:47:22 -0400

    Add new namespaces and force update flag

--- scrapi/harvesters/cyberleninka.py
+++ scrapi/harvesters/cyberleninka.py
@@ -12,7 +12,18 @@ class CyberleninkaHarvester(OAIHarvester):
     short_name = 'cyberleninka'
     long_name = 'CyberLeninka - Russian open access scientific library'
     url = 'http://cyberleninka.ru/oai'
+    force_request_update = True
+
+    namespaces = {
+        'dc': 'http://purl.org/dc/elements/1.1/',
+        'ns0': 'http://www.openarchives.org/OAI/2.0/',
+        'oai_dc': 'http://www.openarchives.org/OAI/2.0/',
+        'dcterms': 'http://purl.org/dc/terms/',
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'bibo': 'http://purl.org/ontology/bibo/'
+    }
 
     base_url = 'http://cyberleninka.ru/oai'
-    property_list = ['{http://purl.org/dc/terms/}isPartOf', '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}type', 'format', '{http://purl.org/ontology/bibo/}issue', '{http://purl.org/ontology/bibo/}issn', '{http://purl.org/ontology/bibo/}pages', '{http://purl.org/dc/terms/}bibliographicCitation', '{http://purl.org/ontology/bibo/}uri', 'date', 'identifier', 'type']
+    property_list = ['isPartOf', 'type', 'format', 'issue', 'issn', 'pages',
+                     'bibliographicCitation', 'uri', 'date', 'identifier', 'type']
     timezone_granularity = True
