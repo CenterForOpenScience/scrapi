990b1a6a58595d3db8ab1b71d14fbe7f47c95b23
Fabian von Feilitzsch <fabian@fabianism.us>
2015-06-30 14:52:00 -0400

    Add new base class for harvester response processing

--- scrapi/processing/base.py
+++ scrapi/processing/base.py
@@ -1,3 +1,8 @@
+import abc
+import json
+from requests.structures import CaseInsensitiveDict
+
+
 class BaseProcessor(object):
     NAME = None
 
@@ -6,3 +11,25 @@ class BaseProcessor(object):
 
     def process_normalized(self, raw_doc, normalized, **kwargs):
         pass  # pragma: no cover
+
+
+class HarvesterResponseModel(object):
+
+    class DoesNotExist(Exception):
+        pass  # pragma: no cover
+
+    def json(self):
+        return json.loads(self.content)
+
+    @property
+    def headers(self):
+        return CaseInsensitiveDict(json.loads(self.headers_str))
+
+    @property
+    def text(self):
+        return self.content.decode('utf-8')
+
+    @classmethod
+    @abc.abstractmethod
+    def get(self, url=None, method=None):
+        raise NotImplementedError
