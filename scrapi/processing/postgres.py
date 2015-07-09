21b617b76c8951f760d52748e27e11bb3acf9fce
erinspace <erin.braswell@gmail.com>
2015-07-09 09:24:40 -0400

    update model query

--- scrapi/processing/postgres.py
+++ scrapi/processing/postgres.py
@@ -22,8 +22,7 @@ class PostgresProcessor(BaseProcessor):
     @events.logged(events.PROCESSING, 'raw.postgres')
     def process_raw(self, raw_doc):
         source, docID = raw_doc['source'], raw_doc['docID']
-        # document = self._get_by_source_id(Document, source, docID) or Document(source=source, docID=docID)
-        document = Document(source=source, docID=docID)
+        document = self._get_by_source_id(Document, source, docID) or Document(source=source, docID=docID)
 
         document.raw = raw_doc.attributes
 
@@ -32,13 +31,12 @@ class PostgresProcessor(BaseProcessor):
     @events.logged(events.PROCESSING, 'normalized.postgres')
     def process_normalized(self, raw_doc, normalized):
         source, docID = raw_doc['source'], raw_doc['docID']
-        document = Document(source=source, docID=docID)
+        document = self._get_by_source_id(Document, source, docID) or Document(source=source, docID=docID)
 
         document.normalized = normalized.attributes
         document.providerUpdatedDateTime = normalized['providerUpdatedDateTime']
 
         document.save()
 
-    # def _get_by_source_id(self, model, source, docID):
-
-    #     docs = Document.objects.filter(source=source, docID=docID)
+    def _get_by_source_id(self, model, source, docID):
+        return Document.objects.filter(source=source, docID=docID)[0]
