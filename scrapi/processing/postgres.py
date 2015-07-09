3ef373143ef4ee1af918d278bc3cbc5dc034f681
erinspace <erin.braswell@gmail.com>
2015-07-09 06:47:33 -0400

    Use django ORM's model instead to avoid complications down the line

--- scrapi/processing/postgres.py
+++ scrapi/processing/postgres.py
@@ -1,32 +1,19 @@
 from __future__ import absolute_import
 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webview.settings")
+
 import logging
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, String, DateTime, Integer, Sequence
-
-from sqlalchemy.dialects.postgresql import JSON
+from api.webview.models import Document
 
 from scrapi import events
 from scrapi.processing.base import BaseProcessor
 
 logger = logging.getLogger(__name__)
 
-engine = create_engine('postgresql://localhost/scrapi', echo=True)
-session = sessionmaker(bind=engine)()
-Base = declarative_base(bind=engine)
-
 
 class PostgresProcessor(BaseProcessor):
     NAME = 'postgres'
-
-    def __init__(self, *args, **kwargs):
-        Base.metadata.create_all()
-        session.commit()
-        super(PostgresProcessor, self).__init__(*args, **kwargs)
 
     @events.logged(events.PROCESSING, 'raw.postgres')
     def process_raw(self, raw_doc):
@@ -34,8 +21,8 @@ class PostgresProcessor(BaseProcessor):
         document = self._get_by_source_id(Document, source, docID) or Document(source=source, docID=docID)
 
         document.raw = raw_doc.attributes
-        session.add(document)
-        session.commit()
+
+        document.save()
 
     @events.logged(events.PROCESSING, 'normalized.postgres')
     def process_normalized(self, raw_doc, normalized):
@@ -45,21 +32,7 @@ class PostgresProcessor(BaseProcessor):
         document.normalized = normalized.attributes
         document.providerUpdatedDateTime = normalized['providerUpdatedDateTime']
 
-        session.add(document)
-        session.commit()
+        document.save()
 
     def _get_by_source_id(self, model, source, docID):
-        return session.query(model).filter_by(source=source, docID=docID).first()
-
-
-class Document(Base):
-    __tablename__ = 'documents'
-
-    source = Column(String, primary_key=True)
-    docID = Column(String, primary_key=True)
-    shareID = Column(Integer, Sequence('shareid_seq'), primary_key=True)
-
-    providerUpdatedDateTime = Column(DateTime)
-
-    raw = Column(JSON)
-    normalized = Column(JSON)
+        return Document.objects.filter(source=source, docID=docID)
