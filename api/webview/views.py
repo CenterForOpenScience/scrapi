5474a3242656792a4c35ef5b6c69fe1349abe293
erinspace <erin.braswell@gmail.com>
2015-07-09 14:16:02 -0400

    Update view to show basic json

--- api/webview/views.py
+++ api/webview/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from webview.models import Document
+from webview.serializers import DocumentSerializer
 
-# Create your views here.
+from rest_framework import generics
+
+
+class DocumentList(generics.ListCreateAPIView):
+    """
+    List all pushed data, or push to the API
+    """
+    serializer_class = DocumentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(source=self.request.user)
+
+    def get_queryset(self):
+        """ Return queryset based on from and to kwargs
+        """
+        queryset = Document.objects.all()
+
+        return queryset
