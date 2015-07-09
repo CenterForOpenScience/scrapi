bec2f2d8f93027aa871aa11c4c141d3bd0e7df1d
erinspace <erin.braswell@gmail.com>
2015-07-09 06:25:26 -0400

    Add django models

--- api/webview/models.py
+++ api/webview/models.py
@@ -1,3 +1,17 @@
+# Create your models here.
+import collections
+
 from django.db import models
 
-# Create your models here.
+from jsonfield import JSONField
+
+
+class ShareDocument(models.Model):
+    source = models.CharField(max_length=100)
+    docID = models.IntegerField()
+    shareID = models.IntegerField()
+
+    providerUpdatedDateTime = models.DateTimeField()
+
+    raw = JSONField(load_kwargs={'object_pairs_hook': collections.OrderedDict})
+    normalized = JSONField(load_kwargs={'object_pairs_hook': collections.OrderedDict})
