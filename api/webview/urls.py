6a885e6b71932882144ab40e4a748cb604cc681a
erinspace <erin.braswell@gmail.com>
2015-07-09 23:47:47 -0400

    Update url patterns

--- api/webview/urls.py
+++ api/webview/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from webview import views
 
 urlpatterns = [
-    url(r'^documents/$', views.DocumentList.as_view())
+    url(r'^documents/$', views.DocumentList.as_view()),
+    url(r'^view_records/$', views.view_records),
 ]
