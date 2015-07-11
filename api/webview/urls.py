fe8ade44588bf09347180d1086600ba60ac04cab
erinspace <erin.braswell@gmail.com>
2015-07-11 19:49:55 -0400

    Update urls with a nae

--- api/webview/urls.py
+++ api/webview/urls.py
@@ -3,5 +3,5 @@ from webview import views
 
 urlpatterns = [
     url(r'^documents/$', views.DocumentList.as_view()),
-    url(r'^view_records/$', views.view_records),
+    url(r'^view_records/$', views.view_records, name='view_records'),
 ]
