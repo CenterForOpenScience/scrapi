f7ea43f56539beea670d6c5f36d5a734208e2401
erinspace <erin.braswell@gmail.com>
2015-07-16 14:30:11 -0400

    Remove unecessary imports

--- api/webview/views.py
+++ api/webview/views.py
@@ -1,11 +1,5 @@
-import json
-from xml.dom import minidom
-from xml.parsers.expat import ExpatError
-
 from django.http import Http404
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.views.decorators.clickjacking import xframe_options_exempt
