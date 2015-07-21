338c904eb9efc01e9c84c8ec91d810227582e1e3
erinspace <erin.braswell@gmail.com>
2015-07-21 10:09:33 -0400

    Remove test db setup from postgres processor

--- tests/test_postgres_processor.py
+++ tests/test_postgres_processor.py
@@ -5,21 +5,20 @@ from sqlalchemy.orm import sessionmaker
 
 from scrapi.linter.document import NormalizedDocument, RawDocument
 
-# Need to force cassandra to ignore set keyspace
 from scrapi.processing.postgres import PostgresProcessor, Document
 
 from . import utils
 
-
 test_db = PostgresProcessor()
 
-engine = create_engine('postgresql://localhost/scrapi', echo=True)
+engine = create_engine('postgresql://localhost/test', echo=True)
 session = sessionmaker(bind=engine)()
 
 NORMALIZED = NormalizedDocument(utils.RECORD)
 RAW = RawDocument(utils.RAW_DOC)
 
 
+@pytest.mark.postgres
 def test_process_raw():
     test_db.process_raw(RAW)
     queryset = Document(docID='someID', source=RAW['source'])
