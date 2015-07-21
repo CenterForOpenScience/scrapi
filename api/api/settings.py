b0f4cbadce87cbf1204cab92d06ecb11c2d84e5a
erinspace <erin.braswell@gmail.com>
2015-07-21 10:19:16 -0400

    Remove undeeded format

--- api/api/settings.py
+++ api/api/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'.format(DOMAIN)
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join('..', BASE_DIR, 'static')
 
