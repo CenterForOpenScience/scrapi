f54fc43d8cf35462d5818c5e195c90f5b03b31da
erinspace <erin.braswell@gmail.com>
2015-07-21 10:08:52 -0400

    Add pytest marker for postgres setup and teardown

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,10 @@ import pytest
 from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import ConnectionError
 
+from psycopg2 import connect
+from psycopg2 import DatabaseError
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
 from scrapi import settings
 from scrapi import database
 
@@ -19,6 +23,16 @@ try:
     use_es = True
 except ConnectionError:
     use_es = False
+
+try:
+    # Need to create a test database
+    postgres_con = connect(dbname='postgres', host='localhost')
+    postgres_con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = postgres_con.cursor()
+    cur.execute('CREATE DATABASE test')
+    use_pg = True
+except DatabaseError:
+    use_pg = False
 
 
 @pytest.fixture(autouse=True)
@@ -46,6 +60,10 @@ def pytest_configure(config):
         'markers',
         'elasticsearch: tests that rely on an elasticsearch connection'
     )
+    config.addinivalue_line(
+        'markers',
+        'postgres: tests that rely on a postgres connection'
+    )
 
 
 def pytest_runtest_setup(item):
@@ -59,8 +77,18 @@ def pytest_runtest_setup(item):
         if not use_es:
             pytest.skip('No connection to Elasticsearch')
 
+    marker = item.get_marker('postgres')
+    if marker is not None:
+        if not use_pg:
+            pytest.skip('No connection to Postgres')
+
 
 def pytest_runtest_teardown(item, nextitem):
     marker = item.get_marker('cassandra')
     if marker is not None:
         database._manager.clear_keyspace(force=True)
+
+    marker = item.get_marker('postgres')
+    if marker is not None:
+        cur.close()
+        postgres_con.close()
