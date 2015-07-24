16703454a9334b6667a761bd52b0a5029e5976b2
erinspace <erin.braswell@gmail.com>
2015-07-24 16:14:19 -0400

    Add pytest mark database setup for test

--- tests/test_api_views.py
+++ tests/test_api_views.py
@@ -1,6 +1,7 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.api.settings")
 
+import pytest
 import django
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
@@ -15,6 +16,7 @@ class APIViewTests(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
 
+    @pytest.mark.postgres
     def test_document_view(self):
         view = DocumentList.as_view()
         request = self.factory.get(
