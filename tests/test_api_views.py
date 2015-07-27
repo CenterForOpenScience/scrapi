874ed2634007a2055dc6f1c03278886b29c5494d
erinspace <erin.braswell@gmail.com>
2015-07-27 13:34:01 -0400

    Add api tests back to pytest to see if django can do its thing

---
+++ tests/test_api_views.py
@@ -0,0 +1,30 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.api.settings")
+
+import pytest
+import django
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+from api.webview.views import DocumentList
+
+django.setup()
+
+
+# TODO - make this work without Django.
+
+class APIViewTests(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    # @pytest.mark.postgres
+    @pytest.mark.django_db
+    def test_document_view(self):
+        view = DocumentList.as_view()
+        request = self.factory.get(
+            '/documents/'
+        )
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
