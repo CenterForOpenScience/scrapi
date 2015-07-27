aec93c014c71ed00a454d123cc4421df3b0d42b2
erinspace <erin.braswell@gmail.com>
2015-07-27 11:17:58 -0400

    add query by source and update detail pattern url

--- api/webview/urls.py
+++ api/webview/urls.py
@@ -3,5 +3,6 @@ from api.webview import views
 
 urlpatterns = [
     url(r'^documents/$', views.DocumentList.as_view()),
-    url(r'^documents/(.*)/$', views.document_detail, name='document_detail')
+    url(r'^documents/(?P<source>\w+)/$', views.DocumentsFromSource.as_view(), name='source'),
+    url(r'^documents/(?P<source>[a-z]+)/(?P<docID>(.*))/$', views.document_detail, name='document_detail')
 ]
