afb398094e207fdd338a492dbbe9fca3f041e2c7
erinspace <erin.braswell@gmail.com>
2015-07-27 13:42:17 -0400

    Make this test a django test case

--- tests/test_postgres_processor.py
+++ tests/test_postgres_processor.py
@@ -3,18 +3,21 @@ import pytest
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.api.settings")
 
-from . import utils
-from scrapi.linter.document import NormalizedDocument, RawDocument
+from django.test import TestCase
 from scrapi.processing.postgres import PostgresProcessor, Document
+
+from . import utils
+from scrapi.linter.document import RawDocument
 
 test_db = PostgresProcessor()
 
-NORMALIZED = NormalizedDocument(utils.RECORD)
+# NORMALIZED = NormalizedDocument(utils.RECORD)
 RAW = RawDocument(utils.RAW_DOC)
 
 
-@pytest.mark.django_db
-def test_process_raw():
-    test_db.process_raw(RAW)
-    queryset = Document(docID='someID', source=RAW['source'])
-    assert queryset.docID == RAW.attributes['docID']
+class DocumentTestCase(TestCase):
+    @pytest.mark.django_db
+    def test_Documents_can_speak(self):
+        test_db.process_raw(RAW)
+        queryset = Document(docID='someID', source=RAW['source'])
+        assert queryset.docID == RAW.attributes['docID']
