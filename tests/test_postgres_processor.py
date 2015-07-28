1f91a0eed0f336ac559cfbca5c4a86f313b48bb5
erinspace <erin.braswell@gmail.com>
2015-07-28 09:37:22 -0400

    Add test for process normalized

--- tests/test_postgres_processor.py
+++ tests/test_postgres_processor.py
@@ -1,4 +1,4 @@
-import pytest
+# import pytest
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.api.settings")
@@ -8,20 +8,24 @@ from django.test import TestCase
 from scrapi.processing.postgres import PostgresProcessor, Document
 
 from . import utils
-from scrapi.linter.document import RawDocument
+from scrapi.linter.document import RawDocument, NormalizedDocument
 
 django.setup()
 
 test_db = PostgresProcessor()
 
-# NORMALIZED = NormalizedDocument(utils.RECORD)
 RAW = RawDocument(utils.POSTGRES_RAW_DOC)
+NORMALIZED = NormalizedDocument(utils.RECORD)
 
 
 class DocumentTestCase(TestCase):
 
-    @pytest.mark.django_db
     def test_raw_processing(self):
         test_db.process_raw(RAW)
         queryset = Document(docID='someID', source=RAW['source'])
         assert queryset.docID == RAW.attributes['docID']
+
+    def test_normalized_processing(self):
+        test_db.process_normalized(RAW, NORMALIZED)
+        queryset = Document(docID=RAW['docID'], source=RAW['source'])
+        assert(queryset.source == NORMALIZED['shareProperties']['source'])
