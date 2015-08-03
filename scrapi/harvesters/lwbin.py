2e9b087339d07da2f959488b7094b1fd04b9e65f
Jiankun Liu <jliu3230@Jiankuns-MacBook-Air.local>
2015-08-03 11:57:12 -0400

    change append emails constraint

--- scrapi/harvesters/lwbin.py
+++ scrapi/harvesters/lwbin.py
@@ -65,8 +65,8 @@ def process_contributors(authors, emails):
 
     contributor_list = []
     append_emails = True
-    if len(authors) > len(emails):
-        append_emails = False  # if there are more authors than emails, do no append emails to authors; instead create new authors.
+    if len(authors) > 1 or len(emails) > 1:
+        append_emails = False  # if there are more than one author or eaiil, create new users
 
     for ind, person in enumerate(authors):
         name = HumanName(person)
