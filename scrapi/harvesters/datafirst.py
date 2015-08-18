f609824382614a66a94eb443d004dc92b2d8a4ca
Fabian von Feilitzsch <fabian@fabianism.us>
2015-08-18 16:06:45 -0400

    Minimal working harvester

--- scrapi/harvesters/datafirst.py
+++ scrapi/harvesters/datafirst.py
@@ -1,11 +1,29 @@
 import csv
 from datetime import timedelta, date
 
+from lxml import etree
+
 from scrapi import settings
 from scrapi import requests
-from scrapi.linter import RawDocument
 from scrapi.base import XMLHarvester
-from scrapi.base.helpers import single_result
+from scrapi.linter import RawDocument, NormalizedDocument
+from scrapi.base.helpers import (
+    compose,
+    CONSTANT,
+    single_result,
+    date_formatter,
+    default_name_parser,
+)
+
+single_string = compose(lambda x: x.title().strip(), single_result)
+
+
+def parse_contributor(author):
+    orgs = {'statistics', 'south', 'africa', 'research', 'organization'}
+    parts = set(author.lower().strip().split(' '))
+    if parts.intersection(orgs):
+        return {'name': author.strip()}
+    return default_name_parser([author])
 
 
 class DataFirstHarvester(XMLHarvester):
@@ -13,19 +31,20 @@ class DataFirstHarvester(XMLHarvester):
     short_name = 'datafirst'
     long_name = 'DataFirst, University of Cape Town'
     url = 'http://www.datafirst.uct.ac.za/'
-    file_format = 'csv'
+    file_format = 'xml'
+
+    namespaces = {'icpsr': 'http://www.icpsr.umich.edu/DDI'}
 
     BASE_URL = 'http://www.datafirst.uct.ac.za/dataportal/index.php/catalog/export/csv?ps=500&sort_by=proddate&sort_order=desc&view=s'
     BASE_DATA_URL = 'http://www.datafirst.uct.ac.za/dataportal/index.php/catalog/ddi/{}'
+    BASE_PROJECT_URL = 'http://www.datafirst.uct.ac.za/dataportal/index.php/catalog/{}'
 
     schema = {
-        'title': ('//titl/node().text', single_result),
-        'description': (),
-        'uris': {
-            'canonicalUri': (),
-
-        },
-        'contributors': ()
+        'title': ('//icpsr:titl/node()', single_string),
+        'contributors': ('//icpsr:AuthEnty/node()', lambda x: map(parse_contributor, x)),
+        'uris': CONSTANT({}),
+        'providerUpdatedDateTime': ('//icpsr:prodDate/node()', compose(date_formatter, single_result)),
+        'description': ('//icpsr:abstract/node()', single_string),
     }
 
     def harvest(self, start_date=None, end_date=None):
@@ -39,7 +58,18 @@ class DataFirstHarvester(XMLHarvester):
             'source': self.short_name,
             'docID': line[0],
             'filetype': self.file_format
-        }) for line in [reader.next()]]
+        }) for line in reader]
 
     def harvest_record(self, service_id):
-        return requests.get(self.BASE_DATA_URL.format(service_id)).content
+        return etree.tostring(etree.XML(requests.get(self.BASE_DATA_URL.format(service_id)).content))
+
+    def normalize(self, raw_doc):
+        transformed = self.transform(etree.XML(raw_doc['doc']), fail=settings.RAISE_IN_TRANSFORMER)
+        transformed['shareProperties'] = {
+            'source': self.short_name,
+            'docID': raw_doc['docID']
+        }
+        transformed['uris']['canonicalUri'] = self.BASE_PROJECT_URL.format(raw_doc['docID'])
+        transformed['uris']['descriptorUris'] = [self.BASE_PROJECT_URL.format(raw_doc['docID'])]
+
+        return NormalizedDocument(transformed, clean=True)
