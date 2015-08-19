1cbab715a647689aeda4679d7dcf4e60ff9ab5b1
Fabian von Feilitzsch <fabian@fabianism.us>
2015-08-19 14:24:20 -0400

    Add harvester response model in django ORM

--- api/webview/models.py
+++ api/webview/models.py
@@ -1,5 +1,9 @@
-from django.db import models
+import json
 
+import six
+from requests.structures import CaseInsensitiveDict
+
+from django.db import models
 from django_pgjson.fields import JsonField
 
 
@@ -11,3 +15,28 @@ class Document(models.Model):
 
     raw = JsonField()
     normalized = JsonField()
+
+
+class HarvesterResponse(models.Model):
+
+    method = models.TextField(primary_key=True)
+    url = models.TextField(primary_key=True, required=True)
+
+    # Raw request data
+    ok = models.BooleanField()
+    content = models.BinaryField()
+    encoding = models.TextField()
+    headers_str = models.TextField()
+    status_code = models.IntegerField()
+    time_made = models.DateTimeField(auto_now=True)
+
+    def json(self):
+        return json.loads(self.content)
+
+    @property
+    def headers(self):
+        return CaseInsensitiveDict(json.loads(self.headers_str))
+
+    @property
+    def text(self):
+        return six.u(self.content)
