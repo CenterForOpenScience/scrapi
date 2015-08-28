22a75ab425a5d6551cb7c29f9f2e6ab8cbf10410
Fitz Elliott <fitz@cos.io>
2015-08-28 15:06:16 -0400

    save canonical uri for NOAA-NODC records

--- scrapi/harvesters/noaa_nodc.py
+++ scrapi/harvesters/noaa_nodc.py
@@ -7,13 +7,26 @@ Example API call: http://data.nodc.noaa.gov/cgi-bin/oai-pmh?verb=GetRecord&metad
 
 from __future__ import unicode_literals
 
+from scrapi.base import helpers
 from scrapi.base import OAIHarvester
+
+
+def oai_extract_url_noaa_nodc(nodc_id):
+    return 'http://data.nodc.noaa.gov/cgi-bin/iso?id=' + nodc_id
 
 
 class NODCHarvester(OAIHarvester):
     short_name = 'noaa_nodc'
     long_name = 'National Oceanographic Data Center'
     url = 'https://www.nodc.noaa.gov/'
+
+    @property
+    def schema(self):
+        return helpers.updated_schema(self._schema, {
+            "uris": {
+                "canonicalUri": ('//ns0:header/ns0:identifier/node()', helpers.compose(oai_extract_url_noaa_nodc, helpers.single_result))
+            }
+        })
 
     base_url = 'http://data.nodc.noaa.gov/cgi-bin/oai-pmh'
     property_list = [
