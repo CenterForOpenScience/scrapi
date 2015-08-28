b5e8d16b8968d0dcaf4833c5e56f8b1a3f35d7a2
Fitz Elliott <fitz@cos.io>
2015-08-28 10:51:22 -0400

    add harvester for NOAA's NODC
    National Oceanographic Data Center

---
+++ scrapi/harvesters/noaa_nodc.py
@@ -0,0 +1,23 @@
+"""
+Harvester of National Oceanic and Atmosphere Administration's National Oceanographic Data Center
+
+Example API call: http://data.nodc.noaa.gov/cgi-bin/oai-pmh?verb=GetRecord&metadataPrefix=oai_dc&identifier=gov.noaa.nodc:0110317
+
+"""
+
+
+from __future__ import unicode_literals
+
+from scrapi.base import helpers
+from scrapi.base import OAIHarvester
+
+class NODCHarvester(OAIHarvester):
+    short_name = 'noaa_nodc'
+    long_name = 'National Oceanographic Data Center'
+    url = 'https://www.nodc.noaa.gov/'
+
+    base_url = 'http://data.nodc.noaa.gov/cgi-bin/oai-pmh'
+    property_list = [
+        'type', 'format', 'coverage', 'date', 'identifier',
+    ]
+    timezone_granularity = True
