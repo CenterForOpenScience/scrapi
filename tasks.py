c82dfabbe155d0b72c818efd0025aa11ab3399d4
Fabian von Feilitzsch <fabian@fabianism.us>
2015-09-08 15:32:20 -0400

    remove daterange

--- tasks.py
+++ tasks.py
@@ -227,6 +227,6 @@ def provider_map(delete=False):
 
 
 @task
-def autooai(shortname, baseurl=None, daterange=None, favicon=False):
+def autooai(shortname, baseurl=None, favicon=False):
     from autooai import generate_oai_harvester
-    generate_oai_harvester(shortname, baseurl, daterange, favicon)
+    generate_oai_harvester(shortname, baseurl, favicon)
