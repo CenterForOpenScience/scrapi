ea9199352209c5e4d31028a3307d05f66a48cee5
Fabian von Feilitzsch <fabian@fabianism.us>
2015-09-10 11:21:23 -0400

    Update harvester tests

--- tests/test_harvesters.py
+++ tests/test_harvesters.py
@@ -3,23 +3,25 @@ import logging
 import vcr
 import pytest
 from freezegun import freeze_time
+from dateutil.parser import parse
 
 from scrapi import base
+from scrapi import settings
 from scrapi import registry, requests
 from scrapi.base.helpers import compose
 
 logger = logging.getLogger(__name__)
 
 
-@freeze_time("2007-12-21")
 @pytest.mark.parametrize('harvester_name', filter(lambda x: x != 'test', sorted(map(str, registry.keys()))))
 def test_harvester(monkeypatch, harvester_name, *args, **kwargs):
     monkeypatch.setattr(requests.time, 'sleep', lambda *_, **__: None)
     base.settings.RAISE_IN_TRANSFORMER = True
+    base.settings.RECORD_HTTP_TRANSACTIONS = False
 
     harvester = registry[harvester_name]
 
-    with vcr.use_cassette('tests/vcr/{}.yaml'.format(harvester_name), match_on=['host'], record_mode='none'):
+    with vcr.use_cassette('tests/vcr/{}.yaml'.format(harvester_name), match_on=['path'], record_mode=settings.TEST_RECORD_MODE):
         harvested = harvester.harvest()
         assert len(harvested) > 0
 
