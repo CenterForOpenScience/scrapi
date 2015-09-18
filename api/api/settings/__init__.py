2467b7fa7fb7dfa08af652d98eb5c7d4ae0194f8
erinspace <erin.braswell@gmail.com>
2015-09-18 16:48:11 -0400

    Add cp suggestion to missing local.py file found

--- api/api/settings/__init__.py
+++ api/api/settings/__init__.py
@@ -3,4 +3,4 @@ from .defaults import *
 try:
     from .local import *
 except ImportError as error:
-    raise ImportError("No local.py settings file found.")
+    raise ImportError("No api local.py settings file found. Try running $ cp api/api/settings/local-dist.py api/api/settings/local.py")
