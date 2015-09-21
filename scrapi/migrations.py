199c480e256f200c20d531e2ca2a8c86c987e5e8
erinspace <erin.braswell@gmail.com>
2015-09-21 16:01:58 -0400

    Remove old document model things and an old function

--- scrapi/migrations.py
+++ scrapi/migrations.py
@@ -1,19 +1,11 @@
 import json
-import time
 import logging
 
-from six.moves import xrange
-from cassandra.cqlengine.query import Token
-
 from scrapi import tasks
-from scrapi import registry
 from scrapi import settings
 from scrapi.events import log_to_sentry
 from scrapi.processing import get_processor
 from scrapi.linter import RawDocument, NormalizedDocument
-from scrapi.processing.cassandra import DocumentModel, DocumentModelOld
-
-from api.webview.models import Document
 
 
 logger = logging.getLogger()
@@ -118,52 +110,6 @@ def delete(docs, sources=None, **kwargs):
         es_processor.manager.es.delete(index='share_v1', doc_type=sources, id=doc.docID, ignore=[404])
 
         logger.info('Deleted document from {} with id {}'.format(sources, doc.docID))
-
-
-@tasks.task_autoretry(default_retry_delay=30, max_retries=5)
-def document_v2_migration(doc, dry=True):
-    logger.warning(
-        'This function has been deprecated, and will be removed in future versions of scrapi'
-    )
-    if not dry:
-        try_n_times(5, DocumentModel.create, **dict(doc)).save()
-
-
-def ModelIteratorFactory(model, next_page, default_args=None):
-    def model_iterator(*sources):
-        sources = sources or default_args
-        q = model.objects.timeout(500).allow_filtering().all().limit(1000)
-        querysets = (q.filter(source=source) for source in sources) if sources else [q]
-        for query in querysets:
-            page = try_n_times(5, list, query)
-            while len(page) > 0:
-                for doc in page:
-                    yield doc
-                page = try_n_times(5, next_page, query, page)
-    return model_iterator
-
-
-def next_page_old(query, page):
-    return list(query.filter(pk__token__gt=Token(page[-1].pk)))
-
-
-def next_page_source_partition(query, page):
-    return list(query.filter(docID__gt=page[-1].docID))
-
-documents_old = ModelIteratorFactory(DocumentModelOld, next_page_old)
-documents = ModelIteratorFactory(DocumentModel, next_page_source_partition, default_args=registry.keys())
-postgres_documents = Document.objects.all()
-
-
-def try_n_times(n, action, *args, **kwargs):
-    for _ in xrange(n):
-        try:
-            return action(*args, **kwargs)
-        except Exception as e:
-            logger.exception(e)
-            time.sleep(15)
-    if e:
-        raise e
 
 
 def doc_to_normed_dict(doc):
