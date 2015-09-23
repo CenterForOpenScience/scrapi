9e2c07aaf6e837994b5deab2fa717832f788a322
erinspace <erin.braswell@gmail.com>
2015-09-23 15:30:00 -0400

    Mostly remove cassandra and postgres specific calls from tests of migrations

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -10,19 +10,21 @@ from scrapi.linter.document import NormalizedDocument
 
 from scrapi import tasks
 from scrapi import registry
+from scrapi import settings
 from scrapi.migrations import delete
 from scrapi.migrations import rename
 from scrapi.migrations import cross_db
 from scrapi.migrations import renormalize
-from scrapi.migrations import DocumentModelOld
+
+from scrapi.processing import get_processor
 
 # Need to force cassandra to ignore set keyspace
-from scrapi.processing.postgres import Document
-from scrapi.processing.cassandra import CassandraProcessor, DocumentModel
+# from scrapi.processing.postgres import Document
+# from scrapi.processing.cassandra import CassandraProcessor, DocumentModel
 
 from . import utils
 
-test_cass = CassandraProcessor()
+# test_cass = CassandraProcessor()
 
 test_harvester = utils.TestHarvester()
 
@@ -37,31 +39,34 @@ def harvester():
 
 @pytest.mark.django_db
 @pytest.mark.cassandra
-def test_rename():
+# @pytest.mark.parametrize('processor_name', ['postgres', 'cassandra'])
+@pytest.mark.parametrize('processor_name', ['postgres'])
+def test_rename(processor_name, monkeypatch):
     real_es = scrapi.processing.elasticsearch.ElasticsearchProcessor.manager.es
     scrapi.processing.elasticsearch.es = mock.MagicMock()
-    test_cass.process_raw(RAW)
-    test_cass.process_normalized(RAW, NORMALIZED)
 
-    queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
+    processor = get_processor(processor_name)
+    processor.process_raw(RAW)
+    processor.process_normalized(RAW, NORMALIZED)
+
+    queryset = processor.document_query(source=RAW['source'], docID=RAW['docID'])
+
     old_source = NORMALIZED['shareProperties']['source']
 
-    assert queryset[0].source == utils.RECORD['shareProperties']['source']
-    assert queryset[0].source == old_source
+    assert queryset.normalized.attributes['shareProperties']['source'] == utils.RECORD['shareProperties']['source']
+    assert queryset.normalized.attributes['shareProperties']['source'] == old_source
 
     new_record = copy.deepcopy(utils.RECORD)
-
     new_record['shareProperties']['source'] = 'wwe_news'
-
     test_harvester.short_name = 'wwe_news'
-
     registry['wwe_news'] = test_harvester
 
     tasks.migrate(rename, sources=[old_source], target='wwe_news', dry=False)
 
-    queryset = DocumentModel.objects(docID=RAW['docID'], source='wwe_news')
-    assert queryset[0].source == 'wwe_news'
-    assert len(queryset) == 1
+    queryset = processor.document_query(source='wwe_news', docID=RAW['docID'])
+
+    assert queryset.normalized.attributes['source'] == 'wwe_news'
+
     scrapi.processing.elasticsearch.ElasticsearchProcessor.manager.es = real_es
     test_harvester.short_name = RAW['source']
     registry['test'] = test_harvester
@@ -70,35 +75,40 @@ def test_rename():
 
 @pytest.mark.django_db
 @pytest.mark.cassandra
-def test_delete():
+@pytest.mark.parametrize('processor_name', ['postgres'])
+def test_delete(processor_name):
     real_es = scrapi.processing.elasticsearch.ElasticsearchProcessor.manager.es
     scrapi.processing.elasticsearch.es = mock.MagicMock()
-    test_cass.process_raw(RAW)
-    test_cass.process_normalized(RAW, NORMALIZED)
 
-    queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
-    assert len(queryset) == 1
+    processor = get_processor(processor_name)
+    processor.process_raw(RAW)
+    processor.process_normalized(RAW, NORMALIZED)
+
+    queryset = processor.document_query(docID=RAW['docID'], source=RAW['source'])
+    # assert len(queryset) == 1
 
     tasks.migrate(delete, sources=[RAW['source']], dry=False)
-    queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
+    queryset = processor.document_query(docID=RAW['docID'], source=RAW['source'])
     assert len(queryset) == 0
     scrapi.processing.elasticsearch.ElasticsearchProcessor.manager.es = real_es
 
 
 @pytest.mark.django_db
 @pytest.mark.cassandra
-def test_renormalize():
+@pytest.mark.parametrize('processor_name', ['postgres'])
+def test_renormalize(processor_name):
     # Set up
     real_es = scrapi.processing.elasticsearch.es
     scrapi.processing.elasticsearch.es = mock.MagicMock()
 
     # Process raw and normalized with fake docs
-    test_cass.process_raw(RAW)
-    test_cass.process_normalized(RAW, NORMALIZED)
+    processor = get_processor(processor_name)
+    processor.process_raw(RAW)
+    processor.process_normalized(RAW, NORMALIZED)
 
     # Check to see those docs were processed
-    queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
-    assert len(queryset) == 1
+    queryset = processor.document_query(docID=RAW['docID'], source=RAW['source'])
+    # assert len(queryset) == 1
 
     # Create a new doucment to be renormalized
     new_raw = copy.deepcopy(RAW)
@@ -110,61 +120,48 @@ def test_renormalize():
 
     tasks.migrate(renormalize, sources=[RAW['source']], dry=False)
 
-    queryset = DocumentModel.objects(docID='get_the_tables', source=RAW['source'])
+    queryset = processor.document_query(docID='get_the_tables', source=RAW['source'])
     assert len(queryset) == 1
     scrapi.processing.elasticsearch.es = real_es
 
 
 @pytest.mark.django_db
 @pytest.mark.cassandra
-def test_migrate_v2():
+@pytest.mark.parametrize('processor_name', ['postgres'])
+def test_cassandra_to_postgres(processor_name):
     real_es = scrapi.processing.elasticsearch.es
     scrapi.processing.elasticsearch.es = mock.MagicMock()
 
-    try:
-        RAW['doc'] = RAW['doc'].encode('utf-8')
-    except AttributeError:
-        RAW['doc'] = str(RAW['doc'])
-    DocumentModelOld.create(**RAW.attributes).save()
-    queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
-    assert len(queryset) == 0
-    tasks.migrate_to_source_partition(dry=False)
-    queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
-    assert len(queryset) == 1
-    scrapi.processing.elasticsearch.es = real_es
+    processor = get_processor(processor_name)
+    processor.process_raw(RAW)
+    processor.process_normalized(RAW, NORMALIZED)
 
+    cassandra_queryset = processor.document_query(docID=RAW['docID'], source=RAW['source'])
+    postgres_queryset = processor.document_query(source=RAW['source'], docID=RAW['docID'])
 
-@pytest.mark.django_db
-@pytest.mark.cassandra
-def test_cassandra_to_postgres():
-    real_es = scrapi.processing.elasticsearch.es
-    scrapi.processing.elasticsearch.es = mock.MagicMock()
-
-    test_cass.process_raw(RAW)
-    test_cass.process_normalized(RAW, NORMALIZED)
-
-    cassandra_queryset = DocumentModel.objects(docID=RAW['docID'], source=RAW['source'])
-    postgres_queryset = Document.objects.filter(source=RAW['source'], docID=RAW['docID'])
     assert len(cassandra_queryset) == 1
     assert len(postgres_queryset) == 0
     tasks.migrate(cross_db, target_db='postgres', dry=False, sources=['test'])
 
-    postgres_queryset = Document.objects.filter(source=RAW['source'], docID=RAW['docID'])
+    postgres_queryset = processor.document_query(source=RAW['source'], docID=RAW['docID'])
     assert len(postgres_queryset) == 1
     scrapi.processing.elasticsearch.es = real_es
 
 
 @pytest.mark.cassandra
 @pytest.mark.elasticsearch
-def test_cassandra_to_elasticsearch():
+@pytest.mark.parametrize('processor_name', ['postgres'])
+def test_cassandra_to_elasticsearch(processor_name):
 
     results = scrapi.processing.elasticsearch.ElasticsearchProcessor.manager.es.search(index='test', doc_type=RAW['source'])
 
     assert (len(results['hits']['hits']) == 0)
 
+    processor = get_processor(processor_name)
+
     # create the cassandra documents
-    test_cass.process_raw(RAW)
-    test_cass.process_normalized(RAW, NORMALIZED)
+    processor.process_raw(RAW)
+    processor.process_normalized(RAW, NORMALIZED)
 
     # run the migration
     tasks.migrate(cross_db, target_db='elasticsearch', dry=False, sources=['test'], index='test')
