589dd641e5460a5b13533e2f577f58736013f42c
alexgarciac <alexgarciac@gmail.com>
2015-10-13 15:55:15 -0500

    Changing query strategy
    Optimizing and splitting queries to avoid the use of OPTIONAL
    statements in SPARQL.

--- scrapi/harvesters/vivo.py
+++ scrapi/harvesters/vivo.py
@@ -16,6 +16,7 @@ from six.moves import xrange
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 from scrapi import settings
+from scrapi.settings.sparql_mapping import *
 from scrapi.base import JSONHarvester
 from scrapi.linter.document import RawDocument
 from scrapi.base.helpers import build_properties
@@ -23,45 +24,15 @@ from scrapi.base.helpers import build_properties
 logger = logging.getLogger(__name__)
 
 
-def process_contributors(authors):
+def process_object_uris(pmid, doi):
     ret = []
-    aux = []
-    authors_list = authors.split('|')
-    for author in authors_list:
-        author_list = author.split(';')
-        if author_list[0] in aux:
-            continue
-        else:
-            contributor = {}
-            aux.append(author_list[0])
-            contributor['sameAs'] = [author_list[0]]
-            contributor['name'] = author_list[1]
-            ret.append(contributor)
+    if pmid:
+        pmid = 'http://www.ncbi.nlm.nih.gov/pubmed/{}'.format(pmid)
+        ret.append(pmid)
+    if doi:
+        doi = 'https://dx.doi.org/{}'.format(doi)
+        ret.append(doi)
     return ret
-
-
-def process_publisher(publisher):
-    ret = {'name': publisher}
-    return ret
-
-
-def process_result(result):
-    processed_result = {}
-    for field in result:
-        processed_result[field] = result[field]['value']
-    return processed_result
-
-
-def process_object_uris(object_uris):
-    processed_uris = []
-    object_uris_array = object_uris.split('|')
-    if object_uris_array[0]:
-        doi_uri = 'https://dx.doi.org/' + object_uris_array[0]
-        processed_uris.append(doi_uri)
-    if object_uris_array[1]:
-        pmid_uri = 'http://www.ncbi.nlm.nih.gov/pubmed/' + object_uris_array[1]
-        processed_uris.append(pmid_uri)
-    return processed_uris
 
 
 class VIVOHarvester(JSONHarvester):
@@ -76,8 +47,80 @@ class VIVOHarvester(JSONHarvester):
     sparql_wrapper.method = 'GET'
 
     DEFAULT_ENCODING = 'UTF-8'
+    QUERY_TEMPLATE = """
+                       PREFIX vivo:  <http://vivoweb.org/ontology/core#>
+                       PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>
+                       PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+                       PREFIX bibo:  <http://purl.org/ontology/bibo/>
+                       PREFIX foaf:  <http://xmlns.com/foaf/0.1/>
+                       PREFIX vitro: <http://vitro.mannlib.cornell.edu/ns/vitro/0.7#>
+                       PREFIX dc: <http://purl.org/dc/terms/>
+                       PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
+                       PREFIX obo:   <http://purl.obolibrary.org/obo/>
+
+                       SELECT {}
+                       {{
+                            {}
+                        }}
+                     """
 
     record_encoding = None
+
+    def get_string(self, uri, sparql_map):
+        variable = sparql_map['name']
+        pattern = sparql_map['pattern'].format(uri)
+        sparql_query = self.QUERY_TEMPLATE.format('?' + variable, pattern)
+        self.sparql_wrapper.setQuery(sparql_query)
+        result = self.sparql_wrapper.query()
+        result = result.convert()
+        if len(result['results']['bindings']) > 0:
+            return result['results']['bindings'][0][variable]['value']
+        else:
+            return ''
+
+    def get_array(self, uri, sparql_map):
+        variable = sparql_map['fields'][0]
+        pattern = sparql_map['pattern'].format(uri)
+        sparql_query = self.QUERY_TEMPLATE.format('?' + variable, pattern)
+        self.sparql_wrapper.setQuery(sparql_query)
+        results = self.sparql_wrapper.query()
+        results = results.convert()
+        ret = []
+        for result in results['results']['bindings']:
+            ret.append(result[variable]['value'])
+        return ret
+
+    def get_dict(self, uri, sparql_map):
+        variables = ''
+        for variable in sparql_map['fields']:
+            variables += '?' + variable + ' '
+        pattern = sparql_map['pattern'].format(uri)
+        sparql_query = self.QUERY_TEMPLATE.format(variables, pattern)
+        self.sparql_wrapper.setQuery(sparql_query)
+        results = self.sparql_wrapper.query()
+        results = results.convert()
+        ret = []
+        for result in results['results']['bindings']:
+            item = {}
+            for variable in sparql_map['fields']:
+                item[variable] = result[variable]['value']
+            ret.append(item)
+        return ret
+
+    def get_records(self, uris):
+        records = []
+        for uri in uris:
+            record = {}
+            record['uri'] = uri
+            for sparql_map in SPARQL_MAPPING:
+                if sparql_map['type'] == 'string':
+                    record[sparql_map['name']] = self.get_string(uri, sparql_map)
+                if sparql_map['type'] == 'array':
+                    record[sparql_map['name']] = self.get_array(uri, sparql_map)
+                if sparql_map['type'] == 'dict':
+                    record[sparql_map['name']] = self.get_dict(uri, sparql_map)
+            records.append(record)
+        return records
 
     def get_total(self, start_date, end_date):
         query_str = """PREFIX vivo:  <http://vivoweb.org/ontology/core#>
@@ -94,116 +137,75 @@ class VIVOHarvester(JSONHarvester):
         result = result.convert()
         return int(result['results']['bindings'][0]['total']['value'])
 
-    def sparql_query(self, start_date, end_date, limit, offset):
+    def get_uris(self, start_date, end_date, limit, offset):
+        uris = []
         query_str = """ PREFIX vivo:  <http://vivoweb.org/ontology/core#>
                         PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>
-                        PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
-                        PREFIX bibo:  <http://purl.org/ontology/bibo/>
-                        PREFIX foaf:  <http://xmlns.com/foaf/0.1/>
-                        PREFIX vitro: <http://vitro.mannlib.cornell.edu/ns/vitro/0.7#>
 
-                        SELECT ?URI ?title ?abstract ?journalTitle ?date ?publisher ?startPage ?endPage ?doi (CONCAT(?doi, "|", ?PMID) as ?objectUris) ?volume ?number ?ISSN ?ISBN
-                                (group_concat(distinct ?type ; separator = "|") AS ?types)
-                                (group_concat(distinct ?subject ; separator = "|") AS ?subjects)
-                                (group_concat(distinct ?keyword ; separator = "|") AS ?keywords)
-                                (group_concat(distinct ?author ; separator = "|") AS ?authors)
+                        SELECT ?uri
                         {{
-                            ?URI vivo:dateTimeValue ?dateURI .
-                            ?dateURI vivo:dateTime ?date .
-                            OPTIONAL {{
-                                        ?URI <http://purl.org/dc/terms/title> ?title .
-                                    }}
-                            OPTIONAL {{
-                                        ?journal vivo:publicationVenueFor ?URI .
-                                        ?journal rdfs:label ?journalTitle .
-                                    }}
-                            OPTIONAL {{
-                                        ?journal vivo:publicationVenueFor ?URI .
-                                        ?journal bibo:issn ?ISSN .
-                                    }}
-                            OPTIONAL {{
-                                        ?journal vivo:publicationVenueFor ?URI .
-                                        ?journal bibo:isbn ?ISBN .
-                                    }}
-                            OPTIONAL {{
-                                        ?publisherURI vivo:publisherOf ?journal .
-                                        ?publisherURI rdfs:label ?publisher .
-                                     }}
-                            OPTIONAL {{
-                                        ?subjectURI vivo:subjectAreaOf ?URI .
-                                        ?subjectURI rdfs:label ?subject .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI vivo:freetextKeyword ?keyword .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI vitro:mostSpecificType ?typeURI .
-                                        ?typeURI rdfs:label ?type .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI bibo:abstract ?abstract .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI bibo:doi ?doi .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI bibo:volume ?volume .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI bibo:number ?number .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI bibo:pageStart ?startPage .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI bibo:pageEnd ?endPage .
-                                    }}
-                            OPTIONAL {{
-                                        ?URI bibo:pmid ?PMID .
-                                   }}
-                            OPTIONAL {{
-                                        ?authorship a vivo:Authorship .
-                                        ?authorship vivo:relates ?URI .
-                                        ?authorURI vivo:relatedBy ?authorship .
-                                        ?authorURI a foaf:Person .
-                                        ?authorURI rdfs:label ?authorName .
-                                        BIND ( CONCAT(str(?authorURI), ";" , ?authorName) AS ?author) .
-                                    }}
+                            ?uri vivo:dateTimeValue ?dateUri .
+                            ?dateUri vivo:dateTime ?date .
                             FILTER (strdt(?date, xsd:date) >= "{}"^^xsd:date && strdt(?date, xsd:date) <= "{}"^^xsd:date)
-                        }} GROUP BY ?URI ?doi ?abstract ?title ?journalTitle ?date ?publisher ?volume ?number ?startPage ?endPage ?PMID ?ISSN ?ISBN LIMIT {} OFFSET {}
+                        }}  LIMIT {} OFFSET {}
                     """
         query_str = query_str.format(start_date.isoformat(), end_date.isoformat(), limit, offset)
         self.sparql_wrapper.setQuery(query_str)
         results = self.sparql_wrapper.query()
         results = results.convert()
-        return results['results']['bindings']
+        for result in results['results']['bindings']:
+            uris.append(result['uri']['value'])
+        return uris
+
+    def complete_authors(self, authors):
+        for author in authors:
+            pattern = """ <{}> obo:ARG_2000028 ?vcard .
+                          ?vcard vcard:hasEmail ?emailUri .
+                          ?emailUri vcard:email ?email ."""
+            email = self.get_string(author['sameAs'], {'name': 'email', 'pattern': pattern, 'type': 'string'})
+            if email:
+                author['email'] = email
+
+            pattern = """?role obo:RO_0000052 <{}> .
+                         ?role vivo:roleContributesTo ?organization .
+                         ?organization rdfs:label ?name .
+                         ?role vivo:dateTimeInterval ?interval .
+                        FILTER NOT EXISTS {{ ?interval vivo:end ?end }}"""
+            affiliation = self.get_dict(author['sameAs'], {'name': 'affiliation', 'fields': ['name'], 'pattern': pattern, 'type': 'dict'})
+            if affiliation:
+                author['affiliation'] = affiliation
+
+            pattern = """<{}> vivo:orcidId ?orcidId .
+                        FILTER isURI(?orcidId)"""
+            orcidId = self.get_string(author['sameAs'], {'name': 'orcidId', 'pattern': pattern, 'type': 'string'})
+            author['sameAs'] = [author['sameAs']]
+            if orcidId:
+                author['sameAs'].append(orcidId)
+        return authors
 
     @property
     def schema(self):
         return {
             'title': ('/title', lambda x: x if x else ''),
-            'description': ('/subtitle', lambda x: x[0] if (isinstance(x, list) and x) else x or ''),
             'providerUpdatedDateTime': ('/date', lambda x: datetime.strptime(x, "%Y-%m-%d").strftime("%Y-%m-%dT%H:%M:%S%Z") + '+00:00'),
             'uris': {
-                'canonicalUri': '/URI',
-                'providerUris': ['/URI'],
-                'objectUris': ('/objectUris', lambda x: process_object_uris(x) if x else [])
+                'canonicalUri': '/uri',
+                'providerUris': ['/uri'],
+                'objectUris': ('/pmid', '/doi', lambda x, y: process_object_uris(x, y))
             },
-            'contributors': ('/authors', lambda x: process_contributors(x) if x else []),
-            'subjects': ('/subjects', lambda x: x.split('|') if x else []),
-            'tags': ('/keywords', lambda x: x.split('|') if x else []),
-            'publisher': ('/publisher', lambda x: process_publisher(x) if x else {}),
+            'contributors': ('/authors', lambda x: self.complete_authors(x)),
+            'subjects': '/subjects',
+            'tags': '/keywords',
+            'publisher': ('/publisher', lambda x: {'name': x} if x else ''),
             'otherProperties': build_properties(
                 ('journalTitle', '/journalTitle'),
                 ('abstract', ('/abstract', lambda x: x if x else '')),
-                ('issue', '/issue'),
-                ('types', ('/types', lambda x: x.split('|') if x else [])),
-                ('ISSN', ('/ISSN', lambda x: x if x else '')),
+                ('type', '/types'),
+                ('ISSN', ('/issn', lambda x: x if x else '')),
                 ('number', '/number'),
-                ('ISBN', '/ISBN'),
+                ('ISBN', '/isbn'),
                 ('startPage', '/startPage'),
                 ('endPage', '/endPage'),
-                ('issue', '/issue'),
                 ('volume', '/volume'),
             )
         }
@@ -213,20 +215,21 @@ class VIVOHarvester(JSONHarvester):
         end_date = end_date or date.today()
 
         total = self.get_total(start_date, end_date)
-        logger.info('{} documents to be harvested'.format(total))
+        logger.info(' {} documents to be harvested'.format(total))
 
         doc_list = []
         for i in xrange(0, total, 1000):
-            records = self.sparql_query(start_date, end_date, 1000, i)
+            uris = self.get_uris(start_date, end_date, 1000, i)
+            records = self.get_records(uris)
             logger.info('Harvested {} documents'.format(i + len(records)))
 
             for record in records:
                 if 'doi' in record:
-                    doc_id = record['doi']['value']
+                    doc_id = record['doi']
                 else:
-                    doc_id = record['URI']['value']
+                    doc_id = record['uri']
                 doc_list.append(RawDocument({
-                    'doc': json.dumps(process_result(record)),
+                    'doc': json.dumps(record),
                     'source': self.short_name,
                     'docID': doc_id,
                     'filetype': 'json'
