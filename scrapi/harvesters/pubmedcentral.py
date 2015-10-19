f82fc06504b1e8df3a2e3c1ea7b6bc7d1f72704c
erinspace <erin.braswell@gmail.com>
2015-10-19 16:57:16 -0400

    Gets rid of extra line
    closes [#SHARE-116]

--- scrapi/harvesters/pubmedcentral.py
+++ scrapi/harvesters/pubmedcentral.py
@@ -4,7 +4,6 @@ Harvester of PubMed Central for the SHARE notification service
 Example API call: http://www.pubmedcentral.nih.gov/oai/oai.cgi?verb=ListRecords&metadataPrefix=oai_dc&from=2015-04-13&until=2015-04-14
 """
 
-
 from __future__ import unicode_literals
 
 from scrapi.base import helpers
