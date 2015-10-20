fdf4e8eaca3393d1273a634b0b30f665e334b57a
Fabian von Feilitzsch <fabian@fabianism.us>
2015-10-20 10:29:11 -0400

    add robots.txt to the routes

--- api/webview/urls.py
+++ api/webview/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from api.webview import views
 
 urlpatterns = [
     url(r'^documents/$', views.DocumentList.as_view()),
     url(r'^documents/(?P<source>\w+)/$', views.DocumentsFromSource.as_view(), name='source'),
-    url(r'^documents/(?P<source>[a-z]+)/(?P<docID>(.*))/$', views.document_detail, name='document_detail')
+    url(r'^documents/(?P<source>[a-z]+)/(?P<docID>(.*))/$', views.document_detail, name='document_detail'),
+    url(r'^robots\.txt$', include('robots.urls')),
 ]
