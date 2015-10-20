082c6ebb51220e293c9715c732afbd63161ce2e4
erinspace <erin.braswell@gmail.com>
2015-10-20 14:00:14 -0400

    Add harvester for UKY
    closes [#SHARE-113]

---
+++ scrapi/harvesters/uky.py
@@ -0,0 +1,18 @@
+'''
+Harvester for the UKnowledge for the SHARE project
+
+Example API call: http://uknowledge.uky.edu/do/oai/?verb=ListRecords&metadataPrefix=oai_dc
+'''
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+class UkyHarvester(OAIHarvester):
+    short_name = 'uky'
+    long_name = 'UKnowledge'
+    url = 'http://uknowledge.uky.edu'
+
+    base_url = 'http://uknowledge.uky.edu/do/oai/'
+    property_list = ['date', 'source', 'identifier', 'type', 'format', 'setSpec']
+    timezone_granularity = True
