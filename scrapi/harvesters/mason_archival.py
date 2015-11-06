727c1c930dbf3c5d1d61999737ca377d2d00c4b2
Zobair Alijan <zobair@Zobairs-MacBook-Air.local>
2015-11-06 12:48:51 -0500

    added mason

---
+++ scrapi/harvesters/mason_archival.py
@@ -0,0 +1,36 @@
+"""
+Harvester for Mason Archival Repository Service for the SHARE NS
+"""
+
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+class MasonArchival(OAIHarvester):
+    short_name = 'mason'
+    long_name = 'Mason Archival Repository Service'
+    url = 'http://mars.gmu.edu/'
+
+    base_url = 'http://mars.gmu.edu/oai/request'
+    timezone_granularity = True
+
+    property_list = [
+        'type', 'source', 'setSpec',
+        'format', 'identifier'
+    ]
+    approved_sets = [
+        u'col_1920_6102',
+        u'col_1920_6039',
+        u'com_1920_262',
+        u'com_1920_466',
+        u'com_1920_1320',
+        u'com_1920_2852',
+        u'com_1920_2869',
+        u'com_1920_2883',
+        u'com_1920_3011',
+        u'com_1920_7520',
+        u'com_1920_8132',
+        u'com_1920_8138',
+        u'col_1920_13'
+    ]
