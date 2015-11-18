5f414c782dd2713b929f014c4a921d42b2dcb28f
Henrique Moco <hmoco94@vt.edu>
2015-11-18 15:25:20 -0500

    Add route to grab inst data

--- api/webview/views.py
+++ api/webview/views.py
@@ -6,9 +6,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.views.decorators.clickjacking import xframe_options_exempt
 
+from elasticsearch import Elasticsearch
+
+from scrapi import settings
 from api.webview.models import Document
 from api.webview.serializers import DocumentSerializer
 
+es = Elasticsearch(settings.ELASTIC_URI)
 
 class DocumentList(generics.ListAPIView):
     """
@@ -64,3 +68,15 @@ def status(request):
     Show the status of the API
     """
     return HttpResponse(json.dumps({'status': 'ok'}), content_type='application/json', status=200)
+
+@api_view(['POST'])
+def institution_detail(request):
+    if not es:
+        return HttpResponse('No connection to elastic search', status=500)
+    query = request.data.get('query')
+    #validate query
+    res = es.search(index=settings.ELASTIC_INST_INDEX, body=query or {})
+    #to be changed dependent on the osf use case or how general this needs to be
+    res = [val['_source']['name'] for val in res.get('hits').get('hits')]
+    return Response(json.dumps(res), status=200)
+
