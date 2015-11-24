db6626c0802d8f3da5eafb95a1326412a9fdb931
erinspace <erin.braswell@gmail.com>
2015-11-24 15:42:52 -0500

    Make json API default for GET route

--- api/api/settings/defaults.py
+++ api/api/settings/defaults.py
@@ -115,6 +115,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     )
 }
 
