ffe01e02d55d776acedce099a333d18cfa5bba79
Katherine Schinkel <kms6bn@virginia.edu>
2015-12-15 15:03:31 -0500

    add AAU harvester, test, and favicon

---
+++ scrapi/harvesters/aau.py
@@ -0,0 +1,18 @@
+'''
+Harvester for the Addis Ababa University Institutional Repository for the SHARE project
+
+Example API call: http://etd.aau.edu.et/oai/request?verb=ListRecords&metadataPrefix=oai_dc
+'''
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+class AauHarvester(OAIHarvester):
+    short_name = 'aau'
+    long_name = 'Addis Ababa University Institutional Repository'
+    url = 'http://etd.aau.edu.et'
+
+    base_url = 'http://etd.aau.edu.et/oai/request'
+    property_list = ['date', 'type', 'identifier', 'setSpec']
+    timezone_granularity = True
