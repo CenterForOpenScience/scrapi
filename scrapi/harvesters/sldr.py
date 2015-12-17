00dd161fe441b579173b168407044340f82f846a
Katherine Schinkel <kms6bn@virginia.edu>
2015-12-17 11:25:53 -0500

    add sldr scraper and test

--- scrapi/harvesters/sldr.py
+++ scrapi/harvesters/sldr.py
@@ -14,24 +14,26 @@ class SldrHarvester(OAIHarvester):
     url = 'http://sldr.org'
 
     base_url = 'http://sldr.org/oai-pmh.php'
-    property_list = ['source', 'type', 'rights', '{http://purl.org/dc/terms/}modified', '{http://purl.org/dc/terms/}temporal', '{http://purl.org/dc/terms/}extent', '{http://purl.org/dc/terms/}spatial', 'identifier', '{http://purl.org/dc/terms/}abstract', 'date', '{http://purl.org/dc/terms/}created', '{http://purl.org/dc/terms/}license', '{http://purl.org/dc/terms/}bibliographicCitation', '{http://purl.org/dc/terms/}isPartOf', '{http://purl.org/dc/terms/}tableOfContents', '{http://purl.org/dc/terms/}accessRights', 'setSpec']
-    approved_sets =  ['publisher',
+    property_list = ['source', 'type', 'rights', 'modified', 'temporal', 'extent', 'spatial', 'identifier', 'abstract', 'date', 'created', 'license', 'bibliographicCitation', 'isPartOf', 'tableOfContents', 'accessRights', 'setSpec']
+    approved_sets = ['publisher',
                       'date',
                       'language',
-                      'rights',
-                      'license',
-                      'format',
-                      'isPartOf',
-                      'created',
-                      'accessRights',
-                      'temporal',
-                      'source',
-                      'bibliographicCitation',
-                      'modified',
-                      'spatial',
-                      'requires',
-                      'identifier',
-                      'type',
-                      'tableOfContents',
+                     'rights',
+                     'license',
+                     'format',
+                     'isPartOf',
+                     'created',
+                     'accessRights',
+                     'temporal',
+                     'source',
+                     'bibliographicCitation',
+                     'modified',
+                     'spatial',
+                     'requires',
+                     'identifier',
+                     'type',
+                     'tableOfContents',
+                     'ortolang',
+                     'archive:long-term'
     ]
     timezone_granularity = False
