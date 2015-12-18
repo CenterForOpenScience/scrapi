e0f3ae37224e48f59a70bac6a1f940937a86b733
Katherine Schinkel <kms6bn@virginia.edu>
2015-12-18 11:34:16 -0500

    update canonical and provider uris

--- scrapi/harvesters/addis_ababa.py
+++ scrapi/harvesters/addis_ababa.py
@@ -6,12 +6,37 @@ Example API call: http://etd.aau.edu.et/oai/request?verb=ListRecords&metadataPre
 from __future__ import unicode_literals
 
 from scrapi.base import OAIHarvester
+from scrapi.base import helpers
 
+def format_uris_addis_ababa(*args):
+    identifiers = helpers.gather_identifiers(args)
+    provider_uris, object_uris = helpers.seperate_provider_object_uris(identifiers)
+
+    for arg in args:
+        if arg and 'http://hdl.handle.net/123456789/' in arg[0]:
+            DOC_ID = arg[0].replace('http://hdl.handle.net/123456789/', '')
+            canonical_uri = 'http://etd.aau.edu.et/handle/123456789/' + DOC_ID
+            provider_uris = 'http://etd.aau.edu.et/handle/123456789/' + DOC_ID
+
+    if not canonical_uri:
+        raise ValueError('No Canonical URI was returned for this record.')
+
+    return {
+        'canonicalUri': canonical_uri,
+        'objectUris': object_uris,
+        'providerUris': provider_uris
+    }
 
 class AauHarvester(OAIHarvester):
     short_name = 'addis_ababa'
     long_name = 'Addis Ababa University Institutional Repository'
     url = 'http://etd.aau.edu.et'
+
+    @property
+    def schema(self):
+        return helpers.updated_schema(self._schema, {
+            "uris": ('//ns0:header/ns0:identifier/node()', '//dc:identifier/node()', format_uris_addis_ababa)
+        })
 
     base_url = 'http://etd.aau.edu.et/oai/request'
     property_list = ['date', 'type', 'identifier', 'setSpec']
