6eecc8e9ee98e5eec4dc28f756e84b085efc873e
Katherine Schinkel <kms6bn@virginia.edu>
2015-12-18 13:22:57 -0500

    update uris

--- scrapi/harvesters/addis_ababa.py
+++ scrapi/harvesters/addis_ababa.py
@@ -9,17 +9,22 @@ from scrapi.base import OAIHarvester
 from scrapi.base import helpers
 
 
-def format_uris_addis_ababa(*args):
+def oai_process_uris_addis_ababa(*args):
+
     identifiers = helpers.gather_identifiers(args)
     provider_uris, object_uris = helpers.seperate_provider_object_uris(identifiers)
 
-    for arg in args:
-        if arg and 'http://hdl.handle.net/123456789/' in arg[0]:
-            doc_id = arg[0].replace('http://hdl.handle.net/123456789/', '')
-            canonical_uri = 'http://etd.aau.edu.et/handle/123456789/' + doc_id
-            provider_uris = ['http://etd.aau.edu.et/handle/123456789/' + doc_id]
+    potential_uris = (provider_uris + object_uris)
 
-    if not canonical_uri:
+    for uri in potential_uris:
+        if 'http://hdl.handle.net/123456789/' in uri:
+            ind = potential_uris.index(uri)
+            doc_id = potential_uris[ind].replace('http://hdl.handle.net/123456789/', '')
+            potential_uris[ind] = 'http://etd.aau.edu.et/handle/123456789/' + doc_id
+
+    try:
+        canonical_uri = potential_uris[0]
+    except IndexError:
         raise ValueError('No Canonical URI was returned for this record.')
 
     return {
@@ -37,7 +42,7 @@ class AauHarvester(OAIHarvester):
     @property
     def schema(self):
         return helpers.updated_schema(self._schema, {
-            "uris": ('//ns0:header/ns0:identifier/node()', '//dc:identifier/node()', format_uris_addis_ababa)
+            "uris": ('//ns0:header/ns0:identifier/node()', '//dc:identifier/node()', oai_process_uris_addis_ababa)
         })
 
     base_url = 'http://etd.aau.edu.et/oai/request'
