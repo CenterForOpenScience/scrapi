152a3330db363d5518829e825defa4debab14f6c
Chelsea Chandler <ckc6cz@virginia.edu>
2016-02-09 09:42:51 -0500

    recommit to see if travis will cooperate

--- scrapi/harvesters/uncg.py
+++ scrapi/harvesters/uncg.py
@@ -1,5 +1,5 @@
 """
-Harvester for the Walter Clinton Jackson Library at the University of North Carolina
+This is a harvester for the Walter Clinton Jackson Library at the University of North Carolina
 at Greensboro for the SHARE project
 
 Example API call: http://libres.uncg.edu/ir/oai/oai.aspx?verb=listrecords&set=uncg&metadataprefix=oai_dc
@@ -16,4 +16,4 @@ class UNCGHarvester(OAIHarvester):
     long_name = 'UNC-Greensboro'
     url = 'http://libres.uncg.edu/ir'
     base_url = 'http://libres.uncg.edu/ir/oai/oai.aspx'
-    approved_sets = [u'UNCG']
+    approved_sets = ['UNCG']
