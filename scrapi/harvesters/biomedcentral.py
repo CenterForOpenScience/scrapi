46a69a57ad6b905b89e4119dc5f03af44e4a2e7c
MerlinZhang <mz6ws@virginia.edu>
2016-02-16 11:18:04 -0500

    [Biomed Central] Updated biomed central to use springer api

--- scrapi/harvesters/biomedcentral.py
+++ scrapi/harvesters/biomedcentral.py
@@ -6,134 +6,128 @@ not make a seperate request for additional metadata for each record.
 Example API query: http://www.biomedcentral.com/search/results?terms=*&format=json&drpAddedInLast=1&itemsPerPage=250#2015-04-23
 """
 
+
 from __future__ import unicode_literals
 
 import json
 import logging
-from datetime import date, timedelta, datetime
-
-from nameparser import HumanName
+from furl import furl
+from datetime import date, timedelta
 
 from scrapi import requests
 from scrapi import settings
 from scrapi.base import JSONHarvester
 from scrapi.linter.document import RawDocument
-from scrapi.base.helpers import build_properties, datetime_formatter
+from scrapi.base.helpers import build_properties, datetime_formatter, compose, default_name_parser
 
 logger = logging.getLogger(__name__)
 
 
-def process_contributors(authors):
+def process_urls(urls):
+    all_uris = [url['value'] for url in urls]
 
-    authors = authors.strip().replace('<span class="author-names">', '').replace('</span>', '')
-    authors = authors.split(',')
+    processed_uris = {
+        'canonicalUri': all_uris[0],
+        'objectUris': [],
+        'providerUris': all_uris
+    }
 
-    if ' and ' in authors[-1] or ' <em>et al.</em>' in authors[-1]:
-        split_name = authors.pop(-1).replace(' <em>et al.</em>', '').split(' and ')
-        authors.extend(split_name)
+    for url in all_uris:
+        if 'dx.doi.org' in url:
+            processed_uris['objectUris'].append(url)
 
-    contributor_list = []
-    for person in authors:
-        name = HumanName(person)
-        contributor = {
-            'name': person,
-            'givenName': name.first,
-            'additionalName': name.middle,
-            'familyName': name.last,
-            'sameAs': [],
-        }
-        contributor_list.append(contributor)
-
-    return contributor_list
+    return processed_uris
 
 
 class BiomedCentralHarvester(JSONHarvester):
     short_name = 'biomedcentral'
     long_name = 'BioMed Central'
-    url = 'http://www.biomedcentral.com/'
+    url = 'http://www.springer.com/us/'
     count = 0
 
-    URL = 'http://www.biomedcentral.com/search/results?terms=*&format=json&drpAddedInLast={}&itemsPerPage=250'
+    base_url = 'http://api.springer.com/meta/v1/json'
+    URL = furl(base_url)
+    URL.args['api_key'] = settings.SPRINGER_API_KEY
+    URL.args['p'] = 100
 
     @property
     def schema(self):
         return {
-            'contributors': ('/authorNames', process_contributors),
-            'uris': {
-                'canonicalUri': '/articleFullUrl',
-                'objectUris': ('/doi', 'http://dx.doi.org/{}'.format),
-                'providerUris': ('/articleFullUrl', '/abstractPath', lambda x, y: [x, y])
-            },
-            'title': ('/bibliographyTitle', '/blurbTitle', lambda x, y: x or y),
-            'providerUpdatedDateTime': ('/published Date', datetime_formatter),
-            'description': '/blurbText',
+            'contributors': (
+                '/creators',
+                compose(
+                    default_name_parser,
+                    lambda authors: [author['creator'] for author in authors]
+                )
+            ),
+            'uris': ('/url', process_urls),
+            'title': '/title',
+            'providerUpdatedDateTime': ('/publicationDate', datetime_formatter),
+            'description': '/abstract',
             'freeToRead': {
-                'startDate': ('/is_free', '/published Date', lambda x, y: y if x else None)
+                'startDate': ('/openaccess', '/publicationDate', lambda x, y: y if x == 'true' else None)
             },
+            'publisher': {
+                'name': '/publisher'
+            },
+            'subjects': ('/genre', lambda x: [x] if x else []),
             'otherProperties': build_properties(
-                ('imageURL', '/imageUrl', {'description': 'a image url'}),
-                ('type', '/type'),
-                ('isOpenAccess', '/isOpenAccess'),
-                ('articleUrl', '/articleUrl'),
-                ('articleFullUrl', '/articleFullUrl'),
-                ('isFree', '/isFree'),
-                ('isHighlyAccessed', '/isHighlyAccessed'),
-                ('status', '/status'),
-                ('abstractPath', '/abstractPath'),
-                ('journal Id', '/journal Id'),
-                ('article_host', '/article_host'),
-                ('longCitation', '/longCitation'),
-                ('is_subscription', '/is_subscription')
+                ('url', '/url'),
+                ('doi', '/doi'),
+                ('isbn', '/isbn'),
+                ('printIsbn', '/printIsbn'),
+                ('electronicIsbn', '/electronicIsbn'),
+                ('volume', '/volume'),
+                ('number', '/number'),
+                ('startingPage', '/startingPage'),
+                ('copyright', '/copyright'),
+                ('identifier', '/identifier')
             )
         }
 
     def harvest(self, start_date=None, end_date=None):
 
         start_date = start_date or date.today() - timedelta(settings.DAYS_BACK)
+        end_date = end_date or date.today()
 
-        # Biomed central can only have a start date
-        end_date = date.today()
-        date_number = end_date - start_date
+        delta = end_date - start_date
 
-        search_url = self.URL.format(date_number.days)
-        records = self.get_records(search_url)
+        date_strings = []
+        for i in range(delta.days + 1):
+            date_strings.append(start_date + timedelta(days=i))
 
-        record_list = []
+        search_urls = []
+        for adate in date_strings:
+            self.URL.args['q'] = 'date:{}'.format(adate)
+
+            search_urls.append(self.URL.url)
+
+        records = self.get_records(search_urls)
+        records_list = []
         for record in records:
-            doc_id = record['arxId']
+            format_type = record['publisher']
+            if format_type.lower() == "biomed central":
+                records_list.append(RawDocument({
+                    'doc': json.dumps(record),
+                    'source': self.short_name,
+                    'docID': record['identifier'],
+                    'filetype': 'json'
+                }))
+        return records_list
 
-            record_list.append(
-                RawDocument(
-                    {
-                        'doc': json.dumps(record),
-                        'source': self.short_name,
-                        'docID': doc_id,
-                        'filetype': 'json'
-                    }
-                )
-            )
+    def get_records(self, search_urls):
+        all_records_from_all_days = []
+        for search_url in search_urls:
+            records = requests.get(search_url).json()
+            index = 1
+            total_records = int(records['result'][0]['total'])
 
-        return record_list
+            all_records = []
+            while len(all_records) < total_records:
+                record_list = records['records']
+                all_records += record_list
+                index += 100
+                records = requests.get(search_url + '&s={}'.format(str(index), throttle=10)).json()
+            all_records_from_all_days = all_records_from_all_days + all_records
 
-    def get_records(self, search_url):
-        now = datetime.now()
-        records = requests.get(search_url + "#{}".format(date.today()))
-        page = 1
-
-        all_records = []
-        current_records = len(records.json()['entries'])
-        while current_records > 0:
-            record_list = records.json()['entries']
-
-            for record in record_list:
-                # Check to see if this is from the future, don't grab if so
-                published_date = record.get('published Date')
-                if datetime.strptime(published_date, '%Y-%m-%d') >= now:
-                    continue
-                all_records.append(record)
-
-            page += 1
-            records = requests.get(search_url + '&page={}#{}'.format(str(page), date.today()), throttle=10)
-            current_records = len(records.json()['entries'])
-
-        return all_records
+        return all_records_from_all_days
