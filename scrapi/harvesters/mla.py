a091a438ffa9c232c26066babf8894a36a9d9397
erinspace <erin.braswell@gmail.com>
2016-05-27 09:31:00 -0400

    MLA harvester

---
+++ scrapi/harvesters/mla.py
@@ -0,0 +1,18 @@
+'''
+Harvester for the MLA commons for the SHARE project
+
+Example API call: https://commons.mla.org/deposits/oai/?verb=ListRecords&metadataPrefix=oai_dc
+'''
+from __future__ import unicode_literals
+
+from scrapi.base import OAIHarvester
+
+
+class MlaHarvester(OAIHarvester):
+    short_name = 'mla'
+    long_name = 'MLA commons'
+    url = 'https://commons.mla.org'
+
+    base_url = 'https://commons.mla.org/deposits/oai/'
+    property_list = ['date', 'identifier', 'setSpec']
+    timezone_granularity = True
